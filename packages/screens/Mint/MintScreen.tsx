13e890b7455f1b3ba8c19e1c976be83ff0bc63b6
dubiella <diogodubiella@gmail.com>
2022-08-18 13:37:38 -0300

    Mint Screen Initial Commint Fork

--- packages/screens/Mint/MintScreen.tsx
+++ packages/screens/Mint/MintScreen.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { ScreenContainer } from "../../components/ScreenContainer";
+import { BrandText } from "../../components/BrandText";
 
 export const MintScreen: React.FC = () => {
   return (
     <ScreenContainer>
-      <h1>Mint Screen</h1>
+      <BrandText>Mint Screen Push</BrandText>
     </ScreenContainer>
   );
 };
