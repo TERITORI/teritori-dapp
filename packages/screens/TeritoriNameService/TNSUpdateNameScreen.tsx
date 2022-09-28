b00c4fd978c34ef4822472f002ee26a3d7a97354
YoLoADR <yohann.developer@gmail.com>
2022-09-28 20:14:22 +0530

    fix(staking): fixed form error in rest files

--- packages/screens/TeritoriNameService/TNSUpdateNameScreen.tsx
+++ packages/screens/TeritoriNameService/TNSUpdateNameScreen.tsx
@@ -4,7 +4,7 @@ import { View } from "react-native";
 
 import { ScreenContainer } from "../../components/ScreenContainer";
 import { BackTo } from "../../components/navigation/BackTo";
-import { NameDataFormType } from "../../components/teritoriNameService/NameDataForm";
+import { NameDataForm } from "../../components/teritoriNameService/NameDataForm";
 import { NameNFT } from "../../components/teritoriNameService/NameNFT";
 import { useFeedbacks } from "../../context/FeedbacksProvider";
 import { useTNS } from "../../context/TNSProvider";
