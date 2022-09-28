25222daf050c9a80c4152df54bcb85458b2cb2de
YoLoADR <yohann.developer@gmail.com>
2022-09-28 20:30:35 +0530

    fix(staking): gradient lint fix

--- packages/components/gradientText/GradientText.tsx
+++ packages/components/gradientText/GradientText.tsx
@@ -1,7 +1,7 @@
-import { TextStyle } from "react-native";
+import { StyleProp, TextStyle } from "react-native";
 
 import { BrandText } from "../BrandText";
 
-export const GradientText: React.FC<{ style: TextStyle }> = ({ children }) => (
-  <BrandText>{children}</BrandText>
-);
+export const GradientText: React.FC<{ style: StyleProp<TextStyle> }> = ({
+  children,
+}) => <BrandText>{children}</BrandText>;
