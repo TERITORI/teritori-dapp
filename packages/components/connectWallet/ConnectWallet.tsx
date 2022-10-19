badb5bbab627b4a5b5ed523b3affbc0da2a6e8a0
YoLoADR <yohann.developer@gmail.com>
2022-10-19 17:58:56 +0530

    feat(connect-wallet): added design

---
+++ packages/components/connectWallet/ConnectWallet.tsx
@@ -0,0 +1,88 @@
+// libraries
+import React from "react";
+
+// misc
+import { StyleSheet, View } from "react-native";
+
+import adenaSVG from "../../../assets/icons/adena.svg";
+import keplrSVG from "../../../assets/icons/keplr.svg";
+import metamaskSVG from "../../../assets/icons/metamask.svg";
+import walletConnectSVG from "../../../assets/icons/wallet-connect.svg";
+import { neutral77, secondaryColor } from "../../utils/style/colors";
+import { fontSemibold14 } from "../../utils/style/fonts";
+import { layout } from "../../utils/style/layout";
+import { BrandText } from "../BrandText";
+import { SeparatorGradient } from "../SeparatorGradient";
+import { TertiaryButton } from "../buttons/TertiaryButton";
+import ModalBase from "../modals/ModalBase";
+import { SpacerColumn } from "../spacer";
+import { ConnectWalletButton } from "./components/ConnectWalletButton";
+
+type ConnectWalletProps = {
+  onClose: () => void;
+  visible: boolean;
+};
+
+export const ConnectWallet: React.FC<ConnectWalletProps> = ({
+  onClose,
+  visible,
+}) => {
+  // returns
+  return (
+    <ModalBase
+      label="Connect Wallet"
+      description="Choose Blockchain of the wallet that you want to add to your Multi-wallet profile"
+      onClose={onClose}
+      visible={visible}
+      hideMainSeparator
+      width={457}
+    >
+      <ConnectWalletButton text="Metamask" isComingSoon icon={metamaskSVG} />
+      <SpacerColumn size={1.5} />
+      <ConnectWalletButton text="Keplr Wallet" icon={keplrSVG} />
+      <SpacerColumn size={1.5} />
+      <ConnectWalletButton text="Adena Wallet" isComingSoon icon={adenaSVG} />
+      <SpacerColumn size={1.5} />
+      <ConnectWalletButton
+        text="Wallet Connect"
+        isComingSoon
+        icon={walletConnectSVG}
+      />
+      <View style={styles.footer}>
+        <SeparatorGradient />
+        <SpacerColumn size={4} />
+        <BrandText style={styles.footerText}>
+          By connecting a wallet, you acknowledge that you have read and
+          understand the{" "}
+          <BrandText style={styles.footerTextHighlight}>
+            Teritori Protocol Disclaimer.
+          </BrandText>
+        </BrandText>
+        <SpacerColumn size={4} />
+        <TertiaryButton
+          size="M"
+          fullWidth
+          disabled
+          text="Read the full Disclaimer & Privacy Policy"
+        />
+      </View>
+    </ModalBase>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: { padding: layout.padding_x4, paddingBottom: layout.padding_x2_5 },
+  footerText: StyleSheet.flatten([
+    fontSemibold14,
+    {
+      color: neutral77,
+      lineHeight: 20,
+    },
+  ]),
+  footerTextHighlight: StyleSheet.flatten([
+    fontSemibold14,
+    {
+      color: secondaryColor,
+    },
+  ]),
+});
