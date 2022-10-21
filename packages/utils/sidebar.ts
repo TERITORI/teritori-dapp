9a2cd27aff7e05c51f04e503e25388a26a7b2309
YoLoADR <yohann.developer@gmail.com>
2022-10-21 17:02:32 +0530

    feat(coming-soon-hover): rioters footer added

--- packages/utils/sidebar.ts
+++ packages/utils/sidebar.ts
@@ -64,7 +64,7 @@ export const SIDEBAR_LIST: SidebarType = {
   },
   riotersFooter: {
     title: "Rioters Footer",
-    route: "ComingSoon",
+    route: "RiotersFooter",
     icon: rioterFooterSVG,
   },
 };
