8270c6ee53ad0f61e2494c654911b6e9d80cd861
Yohann Loïc Ravino <yohann.developer@gmail.com>
2022-10-27 17:51:35 +0200

    fix(collection-page): transfer modal + minor fixes (#200)
    * fix(collection-fixes): some of the collection fixes from hackmd
    
    * fix(collection-fixes): lint fix

---
+++ packages/components/DropdownOption.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { SvgProps } from "react-native-svg";
+
+import { secondaryColor } from "../utils/style/colors";
+import { fontSemibold14 } from "../utils/style/fonts";
+import { layout } from "../utils/style/layout";
+import { BrandText } from "./BrandText";
+import { SVG } from "./SVG";
+import { CustomPressable } from "./buttons/CustomPressable";
+import { SpacerRow } from "./spacer";
+
+interface DropdownOptionProps {
+  icon: React.FC<SvgProps>;
+  label: string;
+  onPress?: () => void;
+  isComingSoon?: boolean;
+}
+
+export const DropdownOption: React.FC<DropdownOptionProps> = ({
+  icon,
+  label,
+  onPress,
+  isComingSoon,
+}) => {
+  return (
+    <CustomPressable
+      style={[styles.option, isComingSoon && styles.comingSoon]}
+      onPress={onPress}
+    >
+      {({ hovered }) => (
+        <>
+          <SVG source={icon} width={20} height={20} color={secondaryColor} />
+          <SpacerRow size={1.5} />
+          <BrandText style={fontSemibold14}>
+            {hovered && isComingSoon ? "Coming Soon" : label}
+          </BrandText>
+        </>
+      )}
+    </CustomPressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  option: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+    padding: layout.padding_x1,
+  },
+  comingSoon: { opacity: 0.5 },
+});
