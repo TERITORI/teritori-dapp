a4e43abb6cc479d4ff9d6c7a0e1f56d7436059cf
Loïc Thomas <wadadidou@gmail.com>
2022-10-28 19:36:15 +0200

    feat: fix after rollback footers and rebase

--- packages/utils/style/layout.ts
+++ packages/utils/style/layout.ts
@@ -1,6 +1,7 @@
 export const helpAreaWidth = 188;
 export const screenContainerContentMarginHorizontal = 140; // Corresponding to the space H between the Sidebar and the page content
 export const headerHeight = 80;
+export const footerHeight = 82;
 export const screenContentMaxWidth = 1092;
 export const screenContentMaxWidthLarge = 1290;
 export const headerMarginHorizontal = 22;
