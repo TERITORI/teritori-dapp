0126fb21284673f8c55398f416c9812d1967bd4e
Loïc Thomas <wadadidou@gmail.com>
2022-10-30 15:42:58 +0100

    fix: LegalFooter

--- packages/components/footers/LegalFooter.tsx
+++ packages/components/footers/LegalFooter.tsx
@@ -14,6 +14,7 @@ export const LegalFooter: React.FC = ({ children }) => {
     <View
       style={{
         flexDirection: "row",
+        flexWrap: "wrap",
         alignItems: "center",
         justifyContent: "space-between",
         height: legalFooterHeight,
@@ -27,8 +28,6 @@ export const LegalFooter: React.FC = ({ children }) => {
           flexDirection: "row",
           alignItems: "center",
           marginLeft: layout.padding_x4,
-          width: "100%",
-          maxWidth: 800 - layout.padding_x4,
         }}
       >
         <SVG source={logoSVG} width={32} height={32} />
@@ -47,10 +46,10 @@ export const LegalFooter: React.FC = ({ children }) => {
       <View
         style={{
           flexDirection: "row",
+          flexWrap: "wrap",
           alignItems: "center",
           justifyContent: "space-evenly",
-          width: "100%",
-          maxWidth: 800,
+          width: "40%",
         }}
       >
         {/*TODO: Gradient text for these 3 ExternalLink ["#676767", "#B7B7B7"]*/}
@@ -58,7 +57,7 @@ export const LegalFooter: React.FC = ({ children }) => {
           externalUrl="TODO"
           style={[
             fontSemibold14,
-            { color: "#676767", textDecorationLine: "none" },
+            { color: "#676767", textDecorationLine: "none", marginRight: layout.padding_x1 },
           ]}
           numberOfLines={1}
         >
@@ -69,7 +68,7 @@ export const LegalFooter: React.FC = ({ children }) => {
           externalUrl="TODO"
           style={[
             fontSemibold14,
-            { color: "#676767", textDecorationLine: "none" },
+            { color: "#676767", textDecorationLine: "none", marginRight: layout.padding_x1 },
           ]}
           numberOfLines={1}
         >
