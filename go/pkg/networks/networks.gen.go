b3d25e29c5298bbadac9f81c101795c1372a4706
Norman Meier <norman@berty.tech>
2022-11-04 19:59:02 +0100

    fix: regen go networks
    Signed-off-by: Norman Meier <norman@berty.tech>

--- go/pkg/networks/networks.gen.go
+++ go/pkg/networks/networks.gen.go
@@ -12,9 +12,19 @@ var AllNetworks = []*Network{
 				Decimals:    6,
 			},
 			&IBCCurrency{
-				Denom:         "ibc/a670d9568b3e399316eede40c1181b7aa4bd0695f0b37513ce9b95b977dfc12e",
+				Denom:         "ibc/A670D9568B3E399316EEDE40C1181B7AA4BD0695F0B37513CE9B95B977DFC12E",
 				SourceNetwork: "cosmos-hub",
 				SourceDenom:   "uatom",
+			},
+		},
+	},
+	{
+		ID: "cosmos-hub",
+		Currencies: []Currency{
+			&NativeCurrency{
+				Denom:       "uatom",
+				CoinGeckoID: "cosmos",
+				Decimals:    6,
 			},
 		},
 	},
