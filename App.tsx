79a932b0da45b02be27a7bab665ee13912a4b6ae
WaDadidou <loic_thomas@outlook.com>
2022-11-04 19:52:49 +0100

    fix: show message if mobile

--- App.tsx
+++ App.tsx
@@ -9,10 +9,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
-import { Platform } from "react-native";
+import { Platform, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as ReduxProvider } from "react-redux";
 
+import { BrandText } from "./packages/components/BrandText";
 import { Navigator } from "./packages/components/navigation/Navigator";
 import { DropdownsContextProvider } from "./packages/context/DropdownsProvider";
 import { FeedbacksContextProvider } from "./packages/context/FeedbacksProvider";
@@ -22,6 +23,7 @@ import { TransactionModalsProvider } from "./packages/context/TransactionModalsP
 import { WalletsProvider } from "./packages/context/WalletsProvider";
 import { store } from "./packages/store/store";
 import { linking } from "./packages/utils/navigation";
+import { neutral00 } from "./packages/utils/style/colors";
 
 const queryClient = new QueryClient();
 
@@ -41,6 +43,22 @@ export default function App() {
   // FIXME: Fonts don't load on electron
   if (Platform.OS !== "web" && !fontsLoaded) {
     return null;
+  }
+
+  if (Platform.OS !== "web") {
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: neutral00,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <BrandText>⛩️ Not available on mobile for now ⛩️</BrandText>
+      </View>
+    );
   }
 
   return (
