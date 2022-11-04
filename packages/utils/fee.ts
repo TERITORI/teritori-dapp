4be328238b99b843dc31194101f6450d91d469f5
Norman Meier <norman@berty.tech>
2022-11-04 22:15:40 +0100

    fix: tns pricing
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/utils/fee.ts
+++ packages/utils/fee.ts
@@ -1,4 +1,4 @@
-import { coins, StdFee, Coin } from "@cosmjs/stargate";
+import { coins, StdFee } from "@cosmjs/stargate";
 
 export const defaultExecuteFee: StdFee = {
   amount: coins(5000, process.env.PUBLIC_STAKING_DENOM!),
@@ -8,16 +8,4 @@ export const defaultExecuteFee: StdFee = {
 export const defaultMintFee: StdFee = {
   amount: coins(5000, process.env.PUBLIC_STAKING_DENOM!),
   gas: "1000000",
-};
-
-// todo - should parse and validate these more elegantly
-export const getMintCost = (username: string): Coin[] => {
-  const surchargeOwed =
-    username.length < parseInt(process.env.PUBLIC_SURCHARGE_MAX_CHARACTERS!, 10)
-      ? parseInt(process.env.PUBLIC_SURCHARGE_FEE!, 10)
-      : 0;
-
-  const baseFee = parseInt(process.env.PUBLIC_BASE_MINT_FEE!, 10);
-  const totalFee = baseFee + surchargeOwed;
-  return coins(totalFee, process.env.PUBLIC_STAKING_DENOM!);
 };
