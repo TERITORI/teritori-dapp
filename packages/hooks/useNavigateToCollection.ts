a5d0796199e1fe46107e7d96d883f81ceab53433
Norman Meier <norman@berty.tech>
2022-11-16 17:02:28 +0100

    chore: add gorilla to noSecondaryDuringMintList
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/hooks/useNavigateToCollection.ts
+++ packages/hooks/useNavigateToCollection.ts
@@ -1,10 +1,17 @@
 import { useAppNavigation } from "../utils/navigation";
 import { useMintEnded } from "./useMintEnded";
 
+// FIXME: use contract parameter instead of hardcoded list
+
+const noSecondaryDuringMintList = [
+  "tori-tori1999u8suptza3rtxwk7lspve02m406xe7l622erg3np3aq05gawxsrh9g0p",
+  `tori-${process.env.THE_RIOT_COLLECTION_ADDRESS}`,
+];
+
 export const useNavigateToCollection = (id: string) => {
   const navigation = useAppNavigation();
   const mintEnded = useMintEnded(id);
-  if (id !== `tori-${process.env.THE_RIOT_COLLECTION_ADDRESS}`) {
+  if (!noSecondaryDuringMintList.includes(id)) {
     return () => navigation.navigate("Collection", { id });
   }
   if (mintEnded === undefined) {
