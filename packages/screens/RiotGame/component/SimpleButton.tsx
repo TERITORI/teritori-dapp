3cf5e3adb53b4309ae81772977cdf2f46aa93422
hthieu1110 <tronghieu.ha@gmail.com>
2022-11-19 01:07:56 +0700

    WIP: fix responsible view

---
+++ packages/screens/RiotGame/component/SimpleButton.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+import { BrandText } from "../../../components/BrandText";
+import { primaryTextColor, yellowDefault } from "../../../utils/style/colors";
+import { fontSemibold14 } from "../../../utils/style/fonts";
+
+interface SimpleButtonProps {
+  title: string;
+  size?: "medium" | "small";
+  color?: string;
+  bgColor?: string;
+  onPress?(): void;
+}
+
+export const SimpleButton: React.FC<SimpleButtonProps> = ({
+  title,
+  size = "medium",
+  color = primaryTextColor,
+  bgColor = yellowDefault,
+}) => {
+  let padH: number;
+  let padV: number;
+
+  switch (size) {
+    case "medium":
+      padH = 22;
+      padV = 16;
+      break;
+    case "small":
+      padH = 16;
+      padV = 12;
+      break;
+  }
+
+  return (
+    <TouchableOpacity>
+      <BrandText
+        style={[
+          styles.btnStyle,
+          {
+            color,
+            backgroundColor: bgColor,
+            paddingHorizontal: padH,
+            paddingVertical: padV,
+          },
+        ]}
+      >
+        {title}
+      </BrandText>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  btnStyle: {
+    marginTop: 40,
+    alignSelf: "center",
+    borderRadius: 12,
+    ...(fontSemibold14 as object),
+  },
+});
