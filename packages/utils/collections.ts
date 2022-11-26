fc10b15493f6261151a48de8a0288d4232443e81
Norman Meier <norman@berty.tech>
2022-11-26 13:58:41 +0100

    chore: open gorilla secondary
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/utils/collections.ts
+++ packages/utils/collections.ts
@@ -1,7 +1,6 @@
 // FIXME: use contract parameter instead of hardcoded list
 
 export const noSecondaryDuringMintList = [
-  "tori-tori1999u8suptza3rtxwk7lspve02m406xe7l622erg3np3aq05gawxsrh9g0p", // gorilla
   `tori-${process.env.THE_RIOT_COLLECTION_ADDRESS}`,
   "tori-tori1x22q8lfhz7qcvtzs0dakhgx2th64l79kepjujhhxk5x804taeqlqmhm4sz", // sucubi
   "tori-tori1r8raaqul4j05qtn0t05603mgquxfl8e9p7kcf7smwzcv2hc5rrlq0vket0", // pets
