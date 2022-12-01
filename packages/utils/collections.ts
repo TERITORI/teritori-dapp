ede260a360d3339e485968eac0a71cd4ea9a284a
Norman Meier <norman@berty.tech>
2022-12-01 15:44:42 +0100

    chore: the others nsdm
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/utils/collections.ts
+++ packages/utils/collections.ts
@@ -5,4 +5,5 @@ export const noSecondaryDuringMintList = [
   "tori-tori1x22q8lfhz7qcvtzs0dakhgx2th64l79kepjujhhxk5x804taeqlqmhm4sz", // sucubi
   "tori-tori1r8raaqul4j05qtn0t05603mgquxfl8e9p7kcf7smwzcv2hc5rrlq0vket0", // pets
   "tori-tori1rcmvfsn77pd6m04ctqj3wcu66pvrw9p265cdl72w4zarfup2rv7qnjklmn", // neo-animalia
+  "tori-tori1r0krq4hfttfuyd4tvcqnjk887xqeq0xae3u4qtya35qsfqy2trlqrdeqq0", // the others
 ];
