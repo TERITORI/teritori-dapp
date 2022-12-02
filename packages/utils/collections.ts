728688500d5d259c531e81473bc5eae3873e32b7
n0izn0iz <n0izn0iz@users.noreply.github.com>
2022-12-02 19:45:05 +0100

    chore: brain ai nsdm (#363)
    Signed-off-by: Norman Meier <norman@berty.tech>
    
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/utils/collections.ts
+++ packages/utils/collections.ts
@@ -6,4 +6,5 @@ export const noSecondaryDuringMintList = [
   "tori-tori1r8raaqul4j05qtn0t05603mgquxfl8e9p7kcf7smwzcv2hc5rrlq0vket0", // pets
   "tori-tori1rcmvfsn77pd6m04ctqj3wcu66pvrw9p265cdl72w4zarfup2rv7qnjklmn", // neo-animalia
   "tori-tori1r0krq4hfttfuyd4tvcqnjk887xqeq0xae3u4qtya35qsfqy2trlqrdeqq0", // the others
+  "tori-tori167xst2jy9n6u92t3n8hf762adtpe3cs6acsgn0w5n2xlz9hv3xgs4ksc6t", // brain ai
 ];
