58300a6e4ec4a631eb0a26c3d8d34fafa452302a
n0izn0iz <n0izn0iz@users.noreply.github.com>
2022-12-07 18:54:21 +0100

    fix: enable tns secondary (#377)
    Signed-off-by: Norman Meier <norman@berty.tech>
    
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/utils/collections.ts
+++ packages/utils/collections.ts
@@ -1,5 +1,6 @@
 // FIXME: use contract parameter instead of hardcoded list
 
 export const secondaryDuringMintList = [
+  `tori-${process.env.TERITORI_NAME_SERVICE_CONTRACT_ADDRESS}`, // TNS
   "tori-tori1r8raaqul4j05qtn0t05603mgquxfl8e9p7kcf7smwzcv2hc5rrlq0vket0", // Teritori's pets
 ];
