afd3c2a778de51c033b3dea8ea138549520b7eb9
yo1110 <tronghieu.ha@gmail.com>
2022-12-17 12:46:15 +0700

    Finish claiming

--- packages/screens/RiotGame/component/FightStatsSection.tsx
+++ packages/screens/RiotGame/component/FightStatsSection.tsx
@@ -3,10 +3,15 @@ import { useEffect, useMemo, useState } from "react";
 import { StyleSheet, View, ViewStyle } from "react-native";
 
 import { ButtonOutline } from "../../../components/buttons/ButtonOutline";
-import { TeritoriDistributorQueryClient } from "../../../contracts-clients/teritori-distributor/TeritoriDistributor.client";
+import { useFeedbacks } from "../../../context/FeedbacksProvider";
+import {
+  TeritoriDistributorClient,
+  TeritoriDistributorQueryClient,
+} from "../../../contracts-clients/teritori-distributor/TeritoriDistributor.client";
 import { useContractClients } from "../../../hooks/useContractClients";
 import useSelectedWallet from "../../../hooks/useSelectedWallet";
 import { p2eBackendClient } from "../../../utils/backend";
+import { decimalFromAtomics } from "../../../utils/coins";
 import { yellowDefault } from "../../../utils/style/colors";
 import { spacing } from "../../../utils/style/spacing";
 import {
@@ -27,15 +32,38 @@ export const FightStatsSection: React.FC<FightStatsSectionProps> = ({
   const [totalFighters, setTotalFighters] = useState(0);
   const [userRank, setUserRank] = useState(0);
   const [claimableAmount, setClaimableAmount] = useState(0);
+  const { setToastError, setToastSuccess } = useFeedbacks();
+  const [isClaiming, setIsClaiming] = useState(false);
 
   const {
     queryClient: distributorQueryClient,
+    client: distributorClient,
     isReady: isContractClientReady,
-  }: { queryClient: TeritoriDistributorQueryClient; isReady: boolean } =
-    useContractClients(
-      TERITORI_DISTRIBUTOR_CONTRACT_ADDRESS,
-      "teritori-distributor"
-    );
+  }: {
+    queryClient: TeritoriDistributorQueryClient;
+    client: TeritoriDistributorClient;
+    isReady: boolean;
+  } = useContractClients(
+    TERITORI_DISTRIBUTOR_CONTRACT_ADDRESS,
+    "teritori-distributor"
+  );
+
+  const claimRewards = async () => {
+    setIsClaiming(true);
+
+    try {
+      await distributorClient.claim();
+      setToastSuccess({
+        title: "Success",
+        message: "Your rewards have been sent to your wallet",
+      });
+      setClaimableAmount(0);
+    } catch (e: any) {
+      setToastError({ title: "Error", message: e.message });
+    } finally {
+      setIsClaiming(false);
+    }
+  };
 
   const prizePool = useMemo(() => {
     const month: string = moment.utc().format("YYYY-MM");
@@ -107,14 +135,24 @@ export const FightStatsSection: React.FC<FightStatsSectionProps> = ({
         width={120}
       />
 
-      <ButtonOutline
-        disabled={claimableAmount === 0}
-        color={yellowDefault}
-        size="M"
-        text="Claim available rewards"
-        style={spacing.ml_1}
-        noBrokenCorners
-      />
+      {claimableAmount !== 0 && (
+        <ButtonOutline
+          disabled={claimableAmount === 0 || isClaiming}
+          color={yellowDefault}
+          size="M"
+          text={
+            isClaiming
+              ? "Claiming..."
+              : `Claim available rewards: ${decimalFromAtomics(
+                  "" + claimableAmount,
+                  "utori"
+                )} TORI`
+          }
+          style={spacing.ml_1}
+          onPress={claimRewards}
+          noBrokenCorners
+        />
+      )}
     </View>
   );
 };
