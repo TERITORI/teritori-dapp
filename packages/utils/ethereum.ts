1d3c86cab74c0c3902fba6cc1909f1fd8745f454
yo1110 <tronghieu.ha@gmail.com>
2023-01-03 18:47:58 +0700

    Fix wei denom

--- packages/utils/ethereum.ts
+++ packages/utils/ethereum.ts
@@ -70,10 +70,6 @@ export const addNftMetadatas = async (nfts: NFT[]) => {
       console.error(e);
     }
 
-    if (+nft.denom === 0) {
-      nft.denom = "wei";
-    }
-
     nftWithMetadatas.push(nft);
   }
 
