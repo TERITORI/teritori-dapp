33cb355c5ec8a658d4ea6fbead40b871189079d2
YoLoADR <yohann.developer@gmail.com>
2023-01-05 23:27:20 +0530

    feat/social-feed: some fixes

--- packages/hooks/useTNSMetadata.ts
+++ packages/hooks/useTNSMetadata.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { useTNSMetaDataList } from "../context/TNSMetaDataListProvider";
 import { TeritoriNameServiceQueryClient } from "../contracts-clients/teritori-name-service/TeritoriNameService.client";
 import { PrimaryAliasResponse } from "../contracts-clients/teritori-name-service/TeritoriNameService.types";
 import { getNonSigningCosmWasmClient } from "../utils/keplr";
@@ -11,13 +12,19 @@ import { useSelectedNetworkInfo } from "./useSelectedNetwork";
 export const useTNSMetadata = (address?: string) => {
   const selectedNetworkInfo = useSelectedNetworkInfo();
 
+  const { metaDataList, addMetaData } = useTNSMetaDataList();
+
   const { data, isLoading, isError } = useQuery(
     ["tns-metadata", address, selectedNetworkInfo?.network],
     async () => {
       if (!address || !selectedNetworkInfo?.network) {
         return null;
       }
-      let aliasResponse: PrimaryAliasResponse = { username: address };
+      const storedMetadata = metaDataList[address];
+      if (storedMetadata) return storedMetadata;
+
+      try {
+        let aliasResponse: PrimaryAliasResponse = { username: address };
 
       // TODO: Do not support NS for Ethereum for now
       if (selectedNetworkInfo.network === Network.Ethereum) {
@@ -32,26 +39,43 @@ export const useTNSMetadata = (address?: string) => {
         process.env.TERITORI_NAME_SERVICE_CONTRACT_ADDRESS || "";
       // We just want to read, so we use a non-signing client
       const cosmWasmClient = await getNonSigningCosmWasmClient();
+        const contractAddress =
+          process.env.TERITORI_NAME_SERVICE_CONTRACT_ADDRESS || "";
+        // We just want to read, so we use a non-signing client
+        const cosmWasmClient = await getNonSigningCosmWasmClient();
 
-      const tnsClient = new TeritoriNameServiceQueryClient(
-        cosmWasmClient,
-        contractAddress
-      );
+        const tnsClient = new TeritoriNameServiceQueryClient(
+          cosmWasmClient,
+          contractAddress
+        );
 
-      if (!address.includes(".tori")) {
-        aliasResponse = await tnsClient.primaryAlias({
-          address,
+        if (!address.includes(".tori")) {
+          aliasResponse = await tnsClient.primaryAlias({
+            address,
+          });
+        }
+
+        // ======== Getting NFT info
+        const nftInfo = await tnsClient.nftInfo({
+          tokenId: aliasResponse.username,
         });
+        console.log(address, {
+          tokenId: aliasResponse.username,
+          ...nftInfo.extension,
+        });
+
+        addMetaData(address, {
+          tokenId: aliasResponse.username,
+          ...nftInfo.extension,
+        });
+
+        return { tokenId: aliasResponse.username, ...nftInfo.extension };
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (_err) {
+        return null;
       }
-
-      // ======== Getting NFT info
-      const nftInfo = await tnsClient.nftInfo({
-        tokenId: aliasResponse.username,
-      });
-
-      return { tokenId: aliasResponse.username, ...nftInfo.extension };
     },
-    { staleTime: Infinity }
+    { staleTime: Infinity, refetchOnWindowFocus: false }
   );
 
   return { loading: isLoading, metadata: data, notFound: isError };
