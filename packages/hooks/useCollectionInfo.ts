e6e5e872e48436f25e7d58e70b1a0acac51e751a
yo1110 <tronghieu.ha@gmail.com>
2023-01-06 03:51:36 +0700

    fix: eth mint whitelist max per address (#430)
    * optimise code
    
    * adjust minMax

--- packages/hooks/useCollectionInfo.ts
+++ packages/hooks/useCollectionInfo.ts
@@ -234,23 +234,20 @@ const getEthereumTeritoriBunkerCollectionInfo = async (mintAddress: string) => {
     state = "ended";
   }
 
-  let maxPerAddress = minterConfig.publicMintMax.toString() || undefined;
+  const maxPerAddress = minterConfig.publicMintMax.toString() || undefined;
   let whitelistSize = 0;
   let unitPrice = minterConfig.publicMintPrice.toString();
+  let whitelistMaxPerAddress = undefined;
 
   if (state === "whitelist" && currentPhase !== undefined) {
     const phaseData = whitelistPhases[currentPhase];
 
-    maxPerAddress = phaseData.mintMax.toString();
     whitelistSize = (
       await minterClient.callStatic.whitelistSize(BigNumber.from(currentPhase))
     ).toNumber();
     unitPrice = phaseData.mintPrice;
+    whitelistMaxPerAddress = phaseData.mintMax.toString();
   }
-
-  const whitelistMaxPerAddress = currentPhase
-    ? String(whitelistPhases[currentPhase].mintMax)
-    : undefined;
 
   const info: CollectionInfo = {
     name,
