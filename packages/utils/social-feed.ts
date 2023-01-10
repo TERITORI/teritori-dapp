ffa4575e4f705598f4b5f163fc156f4f01753012
YoLoADR <yohann.developer@gmail.com>
2023-01-10 13:58:12 +0530

    feat/social-feed: added emojis in comment section

--- packages/utils/social-feed.ts
+++ packages/utils/social-feed.ts
@@ -1,2 +1,24 @@
+import { cloneDeep } from "lodash";
+
+import { PostResult } from "../contracts-clients/teritori-social-feed/TeritoriSocialFeed.types";
+
 export const DEFAULT_NAME = "Anon";
 export const DEFAULT_USERNAME = "anonymous";
+
+export const getUpdatedReactions = (postResult: PostResult, icon: string) => {
+  const copyLocalComment = cloneDeep(postResult);
+  const hasIcon = copyLocalComment.reactions.find((r) => r.icon === icon);
+  let reactions = [...copyLocalComment.reactions];
+  if (hasIcon) {
+    reactions = reactions.map((rect) => {
+      if (rect.icon === icon) {
+        return { icon, count: ++rect.count };
+      }
+      return rect;
+    });
+  } else {
+    reactions = [...reactions, { icon, count: 1 }];
+  }
+
+  return reactions;
+};
