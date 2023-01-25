4f0108ccbe2b5152f2d70f8518ae772478cc0c84
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-01-24 23:58:04 -0700

    code update

--- packages/components/SocialThread/SocialThreadContent.tsx
+++ packages/components/SocialThread/SocialThreadContent.tsx
@@ -1,30 +1,51 @@
 import React from "react";
 import { View } from "react-native";
 
+import { layout } from "../../utils/style/layout";
+import { AudioPreview } from "../FilePreview/AudioPreview";
+import { ImagePreview } from "../FilePreview/UploadedFilePreview/ImagePreview";
+import { VideoPreview } from "../FilePreview/UploadedFilePreview/VideoPreview";
 import { SocialFeedMetadata, PostCategory } from "../NewsFeed/NewsFeed.type";
 import { ArticlePreview } from "./ArticlePreview";
-import { AudioPreviewThread } from "./AudioPreview";
-import { ImageThread } from "./ImageThread";
-
+import { TextContent } from "./TextContent";
 interface Props {
   metadata: SocialFeedMetadata;
   type: PostCategory;
 }
 
 export const SocialThreadContent: React.FC<Props> = ({ metadata, type }) => {
-  if (metadata.files?.[0]?.fileType === "image") {
-    return <ImageThread metadata={metadata} />;
-  }
-  if (metadata.files?.[0]?.fileType === "audio") {
-    return <AudioPreviewThread metadata={metadata} />;
-  }
+  if (type === PostCategory.Article) {
+    return (
+      <View>
+        {type === PostCategory.Article ||
+          (metadata?.message.length > 2500 && (
+            <ArticlePreview metadata={metadata} />
+          ))}
+      </View>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          paddingBottom: layout.padding_x1,
+        }}
+      >
+        <TextContent metadata={metadata} />
+        {metadata.files?.[0]?.fileType === "video" && (
+          <VideoPreview file={metadata.files[0]} onDelete={() => null} />
+        )}
 
-  return (
-    <View>
-      {type === PostCategory.Article ||
-        (metadata?.message.length > 2500 && (
-          <ArticlePreview metadata={metadata} />
-        ))}
-    </View>
-  );
+        {metadata.files?.[0]?.fileType === "image" && (
+          <ImagePreview
+            files={metadata.files || []}
+            isEditable={false}
+            onDelete={() => null}
+          />
+        )}
+        {metadata.files?.[0]?.fileType === "audio" && (
+          <AudioPreview file={metadata.files?.[0]} />
+        )}
+      </View>
+    );
+  }
 };
