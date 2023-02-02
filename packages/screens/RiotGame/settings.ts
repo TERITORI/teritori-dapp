32e368c25ae5ed06091d77a009371dd1336beb2c
yo1110 <tronghieu.ha@gmail.com>
2023-02-03 02:26:13 +0700

    change suqad contract to v2

--- packages/screens/RiotGame/settings.ts
+++ packages/screens/RiotGame/settings.ts
@@ -2,7 +2,7 @@ export const SQUAD_STAKE_COEF = 0.125; // Duration (in hours) = 0.125 * stamin
 export const DURATION_TO_XP_COEF = 100; // XP = 100 * duration (in hours)
 
 export const THE_RIOT_SQUAD_STAKING_CONTRACT_ADDRESS =
-  process.env.THE_RIOT_SQUAD_STAKING_CONTRACT_ADDRESS || "";
+  process.env.THE_RIOT_SQUAD_STAKING_CONTRACT_ADDRESS_V2 || "";
 
 export const THE_RIOT_COLLECTION_ADDRESS =
   process.env.THE_RIOT_COLLECTION_ADDRESS || "";
