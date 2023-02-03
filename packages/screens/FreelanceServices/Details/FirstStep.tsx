8d47bf56ad3c119ec83b10d349e6828d78b75e6e
Eng. Juan Combetto <omniwired@gmail.com>
2023-02-03 12:30:35 +0900

    Freelance service
    First Step screen:
    - Better match the comps
    - Deduplicate (first pass)

--- packages/screens/FreelanceServices/Details/FirstStep.tsx
+++ packages/screens/FreelanceServices/Details/FirstStep.tsx
@@ -1,13 +1,13 @@
 import Checkbox from "expo-checkbox";
 import React, { useState } from "react";
-import { Image, ImageBackground, View } from "react-native";
+import { FlatList, Image, ImageBackground, View } from "react-native";
 
 import backgroundPic from "../../../../assets/banners/freelance-service/background-pic.png";
 import profilePic from "../../../../assets/banners/freelance-service/profile-pic.png";
 import checkIcon from "../../../../assets/icons/blue-check.svg";
-import star from "../../../../assets/icons/yellow-star.svg";
 import { BrandText } from "../../../components/BrandText";
 import { SVG } from "../../../components/SVG";
+import { StarRating } from "../../../components/freelanceServices/StarRating";
 import {
   neutral00,
   neutral33,
@@ -22,77 +22,136 @@ import {
   fontSemibold20,
   fontSemibold28,
 } from "../../../utils/style/fonts";
+import { layout } from "../../../utils/style/layout";
+import { TopRatedSeller } from "../LogoDesign/TopRatedSeller";
 
 const title = [
-  "Extra Fast 2 Days Delivery",
-  "Additional revision",
-  "Include social media kit",
-  "Stationery designs",
-  "Additional logo",
-  "Full Vector Package",
-  "Favicon Design",
-  "E Mail Signature",
-  "Full Color Codes",
-  "Branding Design",
-  "Logo Guidelines",
+  {
+    text: "Extra Fast 2 Days Delivery",
+    subtext: "",
+    price: {
+      value: 1500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Additional revision",
+    subtext:
+      "Add an additional revision your seller will provide after the delivery.",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Include social media kit",
+    subtext:
+      "You'll get graphics showing your logo that you can use on social media platforms. Ex. Facebook and Instagram.",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Stationery designs",
+    subtext:
+      "You'll get a template with your logo to use for stationary—letterhead, envelopes, business cards, etc.",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Additional logo",
+    subtext: "Add another (1) logo concept.",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Full Vector Package",
+    subtext:
+      "I will provide full HQ resolution logo files in 5000 X 5000 Vector AI EPS PDF JPG PNG on transparent BG",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Favicon Design",
+    subtext: "I will design favicon for your Website",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "E Mail Signature",
+    subtext: "I will design E-Mail signature for digital branding",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Full Color Codes",
+    subtext:
+      "I will provide color codes in HEX RGB and CMYK for the final logo",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Branding Design",
+    subtext:
+      "I will design double sided business card and letterhead design to build your brand",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
+  {
+    text: "Logo Guidelines",
+    subtext: "I will design an outstanding Logo Guidelines",
+    price: {
+      value: 500,
+      currency: "TORI",
+    },
+  },
 ];
 
-const text = [
-  "",
-  "Add an additional revision your seller will provide after the delivery.",
-  "You'll get graphics showing your logo that you can use on social media platforms. Ex. Facebook and Instagram.",
-  "You'll get a template with your logo to use for stationary—letterhead, envelopes, business cards, etc.",
-  "Add another (1) logo concept.",
-  "I will provide full HQ resolution logo files in 5000 X 5000 Vector AI EPS PDF JPG PNG on transparent BG",
-  "I will design favicon for your Website",
-  "I will design E-Mail signature for digital branding",
-  "I will provide color codes in HEX RGB and CMYK for the final logo",
-  "I will design double sided business card and letterhead design to build your brand",
-  "I will design an outstanding Logo Guidelines",
-];
+function SelectedExtra(props: { item: any }) {
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: 220,
+        marginBottom: layout.padding_x1,
+      }}
+    >
+      <SVG
+        source={checkIcon}
+        width={16}
+        height={16}
+        style={{ marginRight: 12 }}
+      />
+      <BrandText
+        style={[fontSemibold14, { color: neutral77, marginRight: 24 }]}
+      >
+        {props.item.text}
+      </BrandText>
+    </View>
+  );
+}
 
 export const FirstStep: React.FC = () => {
-  const [checked_1, setChecked_1] = useState<boolean>();
-  const [checked_2, setChecked_2] = useState<boolean>();
-  const [checked_3, setChecked_3] = useState<boolean>();
-  const [checked_4, setChecked_4] = useState<boolean>();
-  const [checked_5, setChecked_5] = useState<boolean>();
-  const [checked_6, setChecked_6] = useState<boolean>();
-  const [checked_7, setChecked_7] = useState<boolean>();
-  const [checked_8, setChecked_8] = useState<boolean>();
-  const [checked_9, setChecked_9] = useState<boolean>();
-  const [checked_10, setChecked_10] = useState<boolean>();
-  const [checked_11, setChecked_11] = useState<boolean>();
-
-  const itemCheckedFunction: any = [
-    setChecked_1,
-    setChecked_2,
-    setChecked_3,
-    setChecked_4,
-    setChecked_5,
-    setChecked_6,
-    setChecked_7,
-    setChecked_8,
-    setChecked_9,
-    setChecked_10,
-    setChecked_11,
-  ];
-  const itemCheckedValue: any = [
-    checked_1,
-    checked_2,
-    checked_3,
-    checked_4,
-    checked_5,
-    checked_6,
-    checked_7,
-    checked_8,
-    checked_9,
-    checked_10,
-    checked_11,
-  ];
+  const [selected, setSelected] = useState<Set<number>>(new Set());
 
   return (
-    <View>
+    <View style={{ width: 840 }}>
       <View style={{ flexDirection: "row", marginTop: 24, display: "flex" }}>
         <ImageBackground
           source={backgroundPic}
@@ -120,9 +179,7 @@ export const FirstStep: React.FC = () => {
             <BrandText style={[fontMedium14, { marginRight: 12 }]}>
               @username
             </BrandText>
-            <BrandText style={[{ color: yellowDefault }, fontMedium14]}>
-              Top Rated Seller
-            </BrandText>
+            <TopRatedSeller rating={4.9} />
             <View
               style={{
                 width: 24,
@@ -131,11 +188,7 @@ export const FirstStep: React.FC = () => {
                 transform: [{ rotate: "90deg" }],
               }}
             />
-            <SVG source={star} width={24} height={24} />
-            <SVG source={star} width={24} height={24} />
-            <SVG source={star} width={24} height={24} />
-            <SVG source={star} width={24} height={24} />
-            <SVG source={star} width={24} height={24} />
+            <StarRating rating={4.9} />
             <BrandText
               style={[{ color: yellowDefault, marginRight: 12 }, fontMedium14]}
             >
@@ -174,30 +227,14 @@ export const FirstStep: React.FC = () => {
           marginTop: 12,
         }}
       >
-        {title.map((item, index) => (
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-            }}
-          >
-            {itemCheckedValue[index] && (
-              <SVG
-                source={checkIcon}
-                width={16}
-                height={16}
-                style={{ marginRight: 12 }}
-              />
-            )}
-            {itemCheckedValue[index] && (
-              <BrandText
-                style={[fontSemibold14, { color: neutral77, marginRight: 24 }]}
-              >
-                {item}
-              </BrandText>
-            )}
-          </View>
-        ))}
+        <FlatList
+          data={title.filter((element, index) => {
+            return selected.has(index);
+          })}
+          numColumns={4}
+          renderItem={({ item }) => <SelectedExtra item={item} />}
+          keyExtractor={(item) => item.text}
+        />
       </View>
       <View
         style={{
@@ -211,7 +248,7 @@ export const FirstStep: React.FC = () => {
       <BrandText style={[fontSemibold20]}>
         Upgrade your order with extras
       </BrandText>
-      {title.map((item: string, index: number) => (
+      {title.map((item, index: number) => (
         <View>
           <View
             style={{
@@ -231,17 +268,26 @@ export const FirstStep: React.FC = () => {
                   height: 24,
                   marginRight: 24,
                 }}
-                color={itemCheckedValue[index] ? primaryColor : neutral00}
-                value={itemCheckedValue[index]}
-                onValueChange={itemCheckedFunction[index]}
+                color={selected.has(index) ? primaryColor : neutral00}
+                value={selected.has(index)}
+                onValueChange={(value) => {
+                  if (value) {
+                    selected.add(index);
+                  } else {
+                    selected.delete(index);
+                  }
+                  setSelected(new Set([...selected]));
+                }}
               />
               <BrandText
                 style={[fontSemibold16, { height: 24, lineHeight: 24 }]}
               >
-                {item}
+                {item.text}
               </BrandText>
             </View>
-            <BrandText style={[fontSemibold16]}>500 TORI</BrandText>
+            <BrandText style={[fontSemibold16]}>
+              {item.price.value} {item.price.currency}
+            </BrandText>
           </View>
           <BrandText
             style={[
@@ -249,7 +295,7 @@ export const FirstStep: React.FC = () => {
               { color: neutral77, marginTop: 6, marginLeft: 48 },
             ]}
           >
-            {text[index]}
+            {item.subtext}
           </BrandText>
         </View>
       ))}
