6c37f806fddc40abea2612ca45297a4328242be2
3mp8r3 <3mp8r3@gmail.com>
2023-03-13 16:45:18 -0400

    remove lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,6 @@ module.exports = {
   root: true,
   extends: ["universe/native"],
   plugins: ["react-hooks"],
-  settings: {
-    "import/ignore": ["react-native"],
-  },
   rules: {
     "@typescript-eslint/no-unused-vars": "error",
     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
