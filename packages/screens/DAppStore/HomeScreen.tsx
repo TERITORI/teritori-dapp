805160d20f31f52ff441d6d758cd7961713599ed
Eng. Juan Combetto <omniwired@gmail.com>
2023-03-28 13:56:52 +0900

    Re use <Separator> element

--- packages/screens/DAppStore/HomeScreen.tsx
+++ packages/screens/DAppStore/HomeScreen.tsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { View } from "react-native";
 
 import { BrandText } from "../../components/BrandText";
-import { FullHeightSeparator } from "../../components/FullHeightSeparator";
 import { FullWidthSeparator } from "../../components/FullWidthSeparator";
 import { ScreenContainer } from "../../components/ScreenContainer";
+import { Separator } from "../../components/Separator";
 import { ScreenFC } from "../../utils/navigation";
 import { Header } from "./components/Header";
 import { LeftRail } from "./components/LeftRail";
@@ -25,7 +25,7 @@ export const DAppStore: ScreenFC<"DAppStore"> = () => {
       <View style={{ flexDirection: "row", minHeight: "inherit" }}>
         <LeftRail />
 
-        <FullHeightSeparator />
+        <Separator horizontal />
 
         <RightRail searchInput={searchInput} />
       </View>
