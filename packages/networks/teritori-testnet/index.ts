6da290dcea4931e5bbdb25ac31746f110eb08c8c
Eng. Juan Combetto <omniwired@gmail.com>
2023-03-29 18:50:13 +0900

    fix networks testnet file to lint to teritori backend

--- packages/networks/teritori-testnet/index.ts
+++ packages/networks/teritori-testnet/index.ts
@@ -21,7 +21,7 @@ export const teritoriTestnetNetwork: NetworkInfo = {
     "https://explorer.teritori.com/teritori-testnet/account/$address",
   idPrefix: "testori",
   testnet: true,
-  backendEndpoint: "https://www.backend-dev1.omniwired.com/",
+  backendEndpoint: "https://dapp-backend.testnet.teritori.com",
   addressPrefix: "tori",
   restEndpoint: "https://rest.testnet.teritori.com",
   rpcEndpoint: "https://rpc.testnet.teritori.com",
