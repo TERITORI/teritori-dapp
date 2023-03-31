8da57717afdea17233650d252c0d86fbf50bfc9f
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-03-31 18:43:07 +0545

    UI design of Cat header

---
+++ packages/screens/Message/AddFriend.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AddFriend = () => {
+  return (
+    <View style={{ backgroundColor: "#000" }}>
+      <Text style={{ color: "red" }}>Add Friend </Text>
+    </View>
+  );
+};
+export default AddFriend;
---
+++ packages/screens/Message/data.tsx
@@ -0,0 +1,24 @@
+import chat from "../../../assets/icons/add-chat.svg";
+import friend from "../../../assets/icons/friend.svg";
+import group from "../../../assets/icons/group.svg";
+import space from "../../../assets/icons/space.svg";
+const data = [
+  {
+    title: "Create a conversation",
+    icon: chat,
+  },
+  {
+    title: "Create a group",
+    icon: group,
+  },
+  {
+    title: "Add a friend",
+    icon: friend,
+  },
+  {
+    title: "Create a Teritori space",
+    icon: space,
+    subtitle: "coming soon",
+  },
+];
+export default data;
