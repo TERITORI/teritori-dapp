7c137969230aed7df0dcf70c7a171cd7322a9480
WaDadidou <wadadidou@gmail.com>
2023-04-05 23:04:51 +0200

    fix: select the right network in useForceUnselectNetworks (when leaving swap screen)

--- packages/hooks/useForceUnselectNetworks.ts
+++ packages/hooks/useForceUnselectNetworks.ts
@@ -1,19 +1,25 @@
 import { useFocusEffect, useRoute } from "@react-navigation/native";
 import { useCallback } from "react";
 
+import { useSelectedNetworkId } from "./useSelectedNetwork";
 import { teritoriNetwork } from "../networks/teritori";
+import { teritoriTestnetNetwork } from "../networks/teritori-testnet";
 import { setSelectedNetworkId } from "../store/slices/settings";
 import { useAppDispatch } from "../store/store";
 import { RouteName } from "../utils/navigation";
 
-// Automatically unselect the current network to another, depending on the RouteName (Depending on the current feature)
 export const useForceUnselectNetworks = () => {
   const dispatch = useAppDispatch();
   const { name: currentRouteName } = useRoute();
+  const selectedNetworkId = useSelectedNetworkId();
   const effect = useCallback(() => {
-    if ((currentRouteName as RouteName) !== "Swap") {
+    if (
+      (currentRouteName as RouteName) !== "Swap" &&
+      selectedNetworkId !== teritoriTestnetNetwork.id &&
+      selectedNetworkId !== teritoriNetwork.id
+    ) {
       dispatch(setSelectedNetworkId(teritoriNetwork.id));
     }
-  }, [dispatch, currentRouteName]);
+  }, [dispatch, currentRouteName, selectedNetworkId]);
   useFocusEffect(effect);
 };
