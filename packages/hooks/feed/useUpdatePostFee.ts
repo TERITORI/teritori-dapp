647adf3503b6488e8567d5241e924e2b7bc1122b
n0izn0iz <n0izn0iz@users.noreply.github.com>
2023-04-06 12:57:11 +0200

    fix: memoize social feed query hooks (#505)
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/hooks/feed/useUpdatePostFee.ts
+++ packages/hooks/feed/useUpdatePostFee.ts
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import { PostCategory } from "../../components/socialFeed/NewsFeed/NewsFeed.type";
 import { getPostFee } from "../../components/socialFeed/NewsFeed/NewsFeedQueries";
 
+// FIXME: use react-query
 export const useUpdatePostFee = () => {
   const [postFee, setPostFee] = useState(0);
-  const updatePostFee = async (
-    networkId: string,
-    postCategory: PostCategory
-  ) => {
-    const fee = await getPostFee({
-      networkId,
-      postCategory,
-    });
-    setPostFee(fee || 0);
-  };
+  const updatePostFee = useCallback(
+    async (networkId: string, postCategory: PostCategory) => {
+      const fee = await getPostFee({
+        networkId,
+        postCategory,
+      });
+      setPostFee(fee || 0);
+    },
+    []
+  );
   return { postFee, updatePostFee };
 };
