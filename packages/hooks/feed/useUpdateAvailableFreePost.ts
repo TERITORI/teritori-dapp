d49cc425a618d0072f40fc2b64965d93f8ec1303
Norman Meier <norman@berty.tech>
2023-04-06 12:47:33 +0200

    fix: memoize social feed query funcs
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/hooks/feed/useUpdateAvailableFreePost.ts
+++ packages/hooks/feed/useUpdateAvailableFreePost.ts
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import { PostCategory } from "../../components/socialFeed/NewsFeed/NewsFeed.type";
 import { getAvailableFreePost } from "../../components/socialFeed/NewsFeed/NewsFeedQueries";
 import { Wallet } from "../../context/WalletsProvider";
 
+// FIXME: use react-query
 export const useUpdateAvailableFreePost = () => {
   const [freePostCount, setFreePostCount] = useState(0);
-  const updateAvailableFreePost = async (
-    networkId: string,
-    postCategory: PostCategory,
-    wallet?: Wallet
-  ) => {
-    const freePost = await getAvailableFreePost({
-      networkId,
-      wallet,
-    });
-    setFreePostCount(freePost || 0);
-  };
+  const updateAvailableFreePost = useCallback(
+    async (networkId: string, postCategory: PostCategory, wallet?: Wallet) => {
+      const freePost = await getAvailableFreePost({
+        networkId,
+        wallet,
+      });
+      setFreePostCount(freePost || 0);
+    },
+    []
+  );
   return { freePostCount, updateAvailableFreePost };
 };
