735b0a36aa8565fbc1cb742dbf85b4f103b97a17
WaDadidou <wadadidou@gmail.com>
2023-04-07 20:10:41 +0200

    fix: useIsMobile not includes platform os

--- packages/hooks/useIsMobile.ts
+++ packages/hooks/useIsMobile.ts
@@ -1,10 +1,10 @@
-import { Platform, useWindowDimensions } from "react-native";
+import { useWindowDimensions } from "react-native";
 
 import { MOBILE_MAX_WIDTH } from "../utils/style/layout";
 
 export const useIsMobile = () => {
   const { width: windowWidth } = useWindowDimensions();
   return (
-    ["android", "ios"].includes(Platform.OS) || windowWidth <= MOBILE_MAX_WIDTH
+    windowWidth <= MOBILE_MAX_WIDTH
   );
 };
