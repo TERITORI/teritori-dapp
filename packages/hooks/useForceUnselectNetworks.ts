c4b42e59ecaafb2e0917a7971517f43c8f7570a8
Norman Meier <norman@berty.tech>
2023-04-17 17:09:40 +0200

    chore: run lint-fix
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/hooks/useForceUnselectNetworks.ts
+++ packages/hooks/useForceUnselectNetworks.ts
@@ -2,12 +2,12 @@ import { useFocusEffect, useRoute } from "@react-navigation/native";
 import { useCallback } from "react";
 
 import { useSelectedNetworkId } from "./useSelectedNetwork";
+import { osmosisNetwork } from "../networks/osmosis";
+import { osmosisTestnetNetwork } from "../networks/osmosis-testnet";
 import { teritoriNetwork } from "../networks/teritori";
 import { setSelectedNetworkId } from "../store/slices/settings";
 import { useAppDispatch } from "../store/store";
 import { RouteName } from "../utils/navigation";
-import { osmosisNetwork } from "../networks/osmosis";
-import { osmosisTestnetNetwork } from "../networks/osmosis-testnet";
 
 export const useForceUnselectNetworks = () => {
   const dispatch = useAppDispatch();
@@ -15,8 +15,8 @@ export const useForceUnselectNetworks = () => {
   const selectedNetworkId = useSelectedNetworkId();
   const effect = useCallback(() => {
     if (
-      (currentRouteName as RouteName) !== "Swap" &&
-      selectedNetworkId === osmosisNetwork.id ||
+      ((currentRouteName as RouteName) !== "Swap" &&
+        selectedNetworkId === osmosisNetwork.id) ||
       selectedNetworkId === osmosisTestnetNetwork.id
     ) {
       dispatch(setSelectedNetworkId(teritoriNetwork.id));
