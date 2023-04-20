1b0d811fb0e515f2a45fe224355834bd7c16cbee
WaDadidou <50441633+WaDadidou@users.noreply.github.com>
2023-04-20 14:51:18 +0200

    fix: adjust automatically the TopMenu height (#570)
    * fix: adjust TopMenu height to prevent overflow issue on small height screens
    
    * fix: top menu mobile height

--- packages/components/TopMenu/TopMenu.tsx
+++ packages/components/TopMenu/TopMenu.tsx
@@ -7,9 +7,17 @@ import chevronDownSVG from "../../../assets/icons/chevron-down.svg";
 import chevronUpSVG from "../../../assets/icons/chevron-up.svg";
 import { useDropdowns } from "../../context/DropdownsProvider";
 import useSelectedWallet from "../../hooks/useSelectedWallet";
-import { neutral00, neutral33, secondaryColor } from "../../utils/style/colors";
+import {
+  neutral00,
+  neutral33,
+  purpleLight,
+  secondaryColor,
+} from "../../utils/style/colors";
+import { fontSemibold14 } from "../../utils/style/fonts";
 import { SVG } from "../SVG";
 import { TertiaryBox } from "../boxes/TertiaryBox";
+
+export const TOP_MENU_BUTTON_HEIGHT = 40;
 
 export const TopMenu: FC = () => {
   const selectedWallet = useSelectedWallet();
@@ -29,7 +37,7 @@ export const TopMenu: FC = () => {
                 : neutral00,
             },
           ]}
-          height={40}
+          height={TOP_MENU_BUTTON_HEIGHT}
         >
           <WalletView wallet={selectedWallet} style={styles.walletView} />
           <SVG
@@ -65,5 +73,9 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 46,
     right: 0,
+  },
+  settingsText: {
+    ...(fontSemibold14 as object),
+    color: purpleLight,
   },
 });
--- packages/components/TopMenu/TopMenuBox.tsx
+++ packages/components/TopMenu/TopMenuBox.tsx
@@ -1,6 +1,13 @@
 import { FC } from "react";
-import { StyleProp, StyleSheet, ViewStyle } from "react-native";
+import {
+  ScrollView,
+  StyleProp,
+  StyleSheet,
+  useWindowDimensions,
+  ViewStyle,
+} from "react-native";
 
+import { TOP_MENU_BUTTON_HEIGHT } from "./TopMenu";
 import { TopMenuAccount } from "./TopMenuAccount";
 import { TopMenuHighlightedNews } from "./TopMenuHighlightedNews";
 import { TopMenuLiveMint } from "./TopMenuLiveMint";
@@ -8,7 +15,7 @@ import { TopMenuMyTeritories } from "./TopMenuMyTeritories";
 import { TopMenuMyWallets } from "./TopMenuMyWallets";
 import { purpleLight } from "../../utils/style/colors";
 import { fontSemibold14 } from "../../utils/style/fonts";
-import { layout, topMenuWidth } from "../../utils/style/layout";
+import { headerHeight, layout, topMenuWidth } from "../../utils/style/layout";
 import { BrandText } from "../BrandText";
 import FlexCol from "../FlexCol";
 import { OmniLink } from "../OmniLink";
@@ -19,30 +26,46 @@ export const TopMenuBox: FC<{
   style?: StyleProp<ViewStyle>;
   mainContainerStyle?: StyleProp<ViewStyle>;
 }> = ({ style, mainContainerStyle }) => {
+  const { height: windowHeight } = useWindowDimensions();
+
   return (
     <TertiaryBox
       width={topMenuWidth}
       noBrokenCorners
-      style={style}
+      style={[style, { borderRadius: 8 }]}
       mainContainerStyle={mainContainerStyle}
     >
-      <TopMenuAccount />
-      <TopMenuMyWallets />
-      <TopMenuMyTeritories />
-      <TopMenuHighlightedNews />
-      <TopMenuLiveMint />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={[
+          styles.scrollView,
+          {
+            maxHeight: windowHeight - headerHeight / 2 - TOP_MENU_BUTTON_HEIGHT,
+          },
+        ]}
+      >
+        <TopMenuAccount />
+        <TopMenuMyWallets />
+        <TopMenuMyTeritories />
+        <TopMenuHighlightedNews />
+        <TopMenuLiveMint />
 
-      <Separator />
-      <OmniLink to={{ screen: "Settings" }}>
-        <FlexCol style={{ paddingVertical: layout.padding_x1_5 }}>
-          <BrandText style={styles.settingsText}>Settings</BrandText>
-        </FlexCol>
-      </OmniLink>
+        <Separator />
+        <OmniLink to={{ screen: "Settings" }}>
+          <FlexCol style={{ paddingVertical: layout.padding_x1_5 }}>
+            <BrandText style={styles.settingsText}>Settings</BrandText>
+          </FlexCol>
+        </OmniLink>
+      </ScrollView>
     </TertiaryBox>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    height: "100%",
+    width: topMenuWidth - 2,
+  },
   settingsText: {
     ...(fontSemibold14 as object),
     color: purpleLight,
