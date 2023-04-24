a7e86fc586ccd0ce68c82bea393c72f85ecd6b90
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-04-24 11:39:23 -0600

    env fix

--- packages/components/NetworkSelector/NetworkSelectorMenu.tsx
+++ packages/components/NetworkSelector/NetworkSelectorMenu.tsx
@@ -19,7 +19,6 @@ import {
   setSelectedWalletId,
 } from "../../store/slices/settings";
 import { useAppDispatch } from "../../store/store";
-import { env } from "../../utils/env";
 import { neutral17 } from "../../utils/style/colors";
 import { fontSemibold12 } from "../../utils/style/fonts";
 import { layout } from "../../utils/style/layout";
@@ -90,7 +89,7 @@ export const NetworkSelectorMenu: FC<{
         borderBottomRightRadius: 0,
       }}
     >
-      {(env.DISPLAYED_NETWORKS_IDS || "")
+      {(process.env.DISPLAYED_NETWORKS_IDS || "")
         .split(",")
         .map((s) => s.trim())
         .map(getNetwork)
--- packages/utils/tns.ts
+++ packages/utils/tns.ts
@@ -11,4 +11,4 @@ export const isNSPath = (str: string) => str.includes("::");
 export const isNSToken = (str: string) => !isNSPath(str);
 
 export const isTokenOwnedByUser = (tokensArray: string[], name: string) =>
-  tokensArray.includes(name + env.TLD);
+  tokensArray.includes(name + process.env.TLD);
