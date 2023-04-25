ba92a2422aef8d41224c4a0d8f3c20df6a4e255b
WaDadidou <50441633+WaDadidou@users.noreply.github.com>
2023-04-25 21:33:24 +0200

    fix: responsive SocialCardHeader (#594)

--- packages/components/socialFeed/SocialThread/DateTime.tsx
+++ packages/components/socialFeed/SocialThread/DateTime.tsx
@@ -1,14 +1,15 @@
 import moment from "moment";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { StyleProp, TextStyle, TouchableOpacity } from "react-native";
 
 import { fontSemibold12 } from "../../../utils/style/fonts";
 import { BrandText } from "../../BrandText";
 
 interface Props {
   date: string;
+  textStyle?: StyleProp<TextStyle>;
 }
-export const DateTime = ({ date }: Props) => {
+export const DateTime = ({ date, textStyle }: Props) => {
   const [isFullDate, setIsFullDate] = useState(false);
 
   return (
@@ -16,7 +17,7 @@ export const DateTime = ({ date }: Props) => {
       activeOpacity={0.8}
       onPress={() => setIsFullDate((prev) => !prev)}
     >
-      <BrandText style={fontSemibold12}>
+      <BrandText style={[fontSemibold12, textStyle]}>
         {isFullDate
           ? moment(date).local().format("MMM D, YYYY [at] hh:mm a")
           : moment(date).local().fromNow()}
