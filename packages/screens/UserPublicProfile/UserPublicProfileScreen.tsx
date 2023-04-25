d741d08670565558440a3ff1bbf172979978d89f
n0izn0iz <n0izn0iz@users.noreply.github.com>
2023-04-25 16:34:03 +0200

    fix: reset upp and collection screens state on id change (#591)
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/screens/UserPublicProfile/UserPublicProfileScreen.tsx
+++ packages/screens/UserPublicProfile/UserPublicProfileScreen.tsx
@@ -134,6 +134,7 @@ export const UserPublicProfileScreen: ScreenFC<"UserPublicProfile"> = ({
 
   return (
     <ScreenContainer
+      key={`UserPublicProfile ${id}`} // this key is to reset the screen state when the id changes
       forceNetworkId={network?.id}
       responsive
       fullWidth
