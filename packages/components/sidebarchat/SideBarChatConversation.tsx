22eb2c81056d96cf8a66736bc48d15ea01169101
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-04-26 17:45:49 +0545

    clean code

--- packages/components/sidebarchat/SideBarChatConversation.tsx
+++ packages/components/sidebarchat/SideBarChatConversation.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Avatar, Badge } from "react-native-paper";
 
 import doublecheck from "../../../assets/icons/doublecheck.svg";
 import { Separator } from "../../components/Separator";
+import { neutral22, neutralA3, secondaryColor } from "../../utils/style/colors";
+import {
+  fontMedium10,
+  fontSemibold11,
+  fontSemibold13,
+} from "../../utils/style/fonts";
+import { BrandText } from "../BrandText";
+import FlexRow from "../FlexRow";
 import { SVG } from "../SVG";
+import { SpacerColumn, SpacerRow } from "../spacer";
 
 type Props = {
   name: string;
@@ -25,94 +34,53 @@ const SideBarChatConversation: React.FC<Props> = ({
   const onlineStatusBadgeColor = isOnline ? "green" : "yellow";
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <View style={{ flexDirection: "row" }}>
-          <Avatar.Image size={35} source={avatar} />
-          <Badge
-            style={{
-              position: "absolute",
-              top: 20,
-              left: 25,
+      <FlexRow justifyContent="space-between" style={{ marginTop: 10 }}>
+        <View>
+          <FlexRow>
+            <Avatar.Image size={35} source={avatar} />
+            <Badge
+              style={{
+                position: "absolute",
+                top: 20,
+                left: 25,
 
-              backgroundColor: onlineStatusBadgeColor,
-            }}
-            size={12}
-          />
-          <View style={{ marginLeft: 12 }}>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.online}>{chat}</Text>
-          </View>
+                backgroundColor: onlineStatusBadgeColor,
+              }}
+              size={12}
+            />
+            <SpacerRow size={2} />
+            <View>
+              <FlexRow>
+                <BrandText style={[fontSemibold13, { color: secondaryColor }]}>
+                  {name}
+                </BrandText>
+              </FlexRow>
+              <SpacerColumn size={0.2} />
+              <BrandText style={[fontSemibold11, { color: neutralA3 }]}>
+                {chat}
+              </BrandText>
+            </View>
+          </FlexRow>
         </View>
-        <View style={{ flexDirection: "row" }}>
-          <Text
-            style={{
-              color: "#FFFFFF",
-              fontWeight: "500",
-              fontSize: 10,
-              lineHeight: 12,
-            }}
-          >
-            {time}
-          </Text>
-          <SVG
-            width={24}
-            height={24}
-            source={doublecheck}
-            style={{ marginRight: 2, marginLeft: 4 }}
-          />
+        <View style={{ marginTop: -15 }}>
+          <FlexRow>
+            <BrandText style={[fontMedium10, { color: secondaryColor }]}>
+              {time}
+            </BrandText>
+            <SpacerRow size={0.6} />
+            <SVG width={16} height={16} source={doublecheck} />
+          </FlexRow>
         </View>
-      </View>
-      <View style={{ marginTop: 10 }}>
-        <Separator color="#222222" />
-      </View>
+      </FlexRow>
+      <SpacerColumn size={1.5} />
+      <Separator color={neutral22} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#000",
-  },
-  row: {
-    flexDirection: "row",
-    marginTop: 10,
-    justifyContent: "space-between",
-  },
-  chatboxicon: { flexDirection: "row" },
-  icon: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-  },
-  online: {
-    fontSize: 11,
-    fontWeight: "500",
-    color: "#A3A3A3",
-    lineHeight: 18,
-  },
-  badge: {
-    backgroundColor: "green",
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    position: "absolute",
-    bottom: -5,
-    right: -5,
-  },
-  name: {
-    fontSize: 13,
-    fontWeight: "600",
-    color: "#FFFFFF",
-    lineHeight: 18,
-  },
-  chatIcon: {
-    width: 24,
-    height: 24,
-    marginRight: 8,
-  },
-  dotsIcon: {
-    width: 24,
-    height: 24,
+    backgroundColor: "#000000",
   },
 });
 
--- packages/screens/Message/MessengerGroupChat/MessagePopup.tsx
+++ packages/screens/Message/MessengerGroupChat/MessagePopup.tsx
@@ -1,20 +1,56 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
+import avatar from "../../../../assets/icons/avatar.svg";
 import copy from "../../../../assets/icons/copy.svg";
 import deleteicon from "../../../../assets/icons/delete.svg";
 import farward from "../../../../assets/icons/farward.svg";
 import farwardto from "../../../../assets/icons/farwardto.svg";
 import reply from "../../../../assets/icons/reply.svg";
+import searchSVG from "../../../../assets/icons/search.svg";
 import select from "../../../../assets/icons/select.svg";
+import { BrandText } from "../../../components/BrandText";
 import FlexRow from "../../../components/FlexRow";
 import { SVG } from "../../../components/SVG";
 import { Separator } from "../../../components/Separator";
 import { SpacerColumn, SpacerRow } from "../../../components/spacer";
 import { neutralA3 } from "../../../utils/style/colors";
 import { fontSemibold13 } from "../../../utils/style/fonts";
-
+const data = [
+  {
+    icon: avatar,
+    name: "John  Robert ",
+  },
+  {
+    icon: avatar,
+    name: "John  Robert",
+  },
+  {
+    icon: avatar,
+    name: "John  Robert",
+  },
+  {
+    icon: avatar,
+    name: "John  Robert",
+  },
+  {
+    icon: avatar,
+    name: "John  Robert",
+  },
+];
 const MessagePopup = () => {
+  const [isForwarding, setIsForwarding] = useState(false);
+  const handleForwardClick = () => {
+    setIsForwarding(true);
+  };
+  const ITEM_HEIGHT = 50;
   return (
     <View>
       <TouchableOpacity>
@@ -27,7 +63,7 @@ const MessagePopup = () => {
 
       <SpacerColumn size={1} />
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleForwardClick}>
         <FlexRow>
           <SVG source={farwardto} height={16} width={16} color={neutralA3} />
           <SpacerRow size={1} />
@@ -42,7 +78,49 @@ const MessagePopup = () => {
           />
         </FlexRow>
       </TouchableOpacity>
+      {isForwarding && (
+        <View style={[styles.forwardContainer, { maxHeight: 4 * ITEM_HEIGHT }]}>
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <SVG source={searchSVG} width={16} height={16} />
+            <TextInput
+              placeholder="Search"
+              // value={value}
+              // onChangeText={handleChangeText}
+              // onKeyPress={(event) =>
+              //   handleKeyPress({ event, onPressEnter })
+              // }
 
+              placeholderTextColor="#999999"
+              style={{
+                fontSize: 14,
+                color: "white",
+                fontFamily: "Exo_600SemiBold",
+                marginLeft: 11,
+              }}
+            />
+          </View>
+          <SpacerColumn size={2} />
+          <Separator />
+          <SpacerColumn size={2} />
+
+          <ScrollView showsVerticalScrollIndicator={false}>
+            {data.map((item, index) => (
+              <View style={{ maxHeight: ITEM_HEIGHT }}>
+                <FlexRow key={index}>
+                  <SVG source={item.icon} height={24} width={24} />
+                  <SpacerRow size={2} />
+                  <BrandText style={fontSemibold13}>{item.name}</BrandText>
+                </FlexRow>
+                <SpacerColumn size={2} />
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+      )}
       <SpacerColumn size={1} />
       <Separator />
       <SpacerColumn size={1} />
@@ -75,3 +153,23 @@ const MessagePopup = () => {
   );
 };
 export default MessagePopup;
+const styles = StyleSheet.create({
+  forwardContainer: {
+    backgroundColor: "rgba(41, 41, 41, 0.8)",
+    padding: 20,
+
+    borderRadius: 16,
+    // width: "auto",
+    zIndex: 11111,
+    position: "absolute",
+    right: -225,
+  },
+  forwardText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  receiverText: {
+    fontSize: 14,
+  },
+});
