6bfb23e1d76fb25c0b8dc1d2bf29f73a99ad25ca
Norman Meier <norman@berty.tech>
2023-04-29 23:06:11 +0200

    chore: clean
    Signed-off-by: Norman Meier <norman@berty.tech>

--- App.tsx
+++ App.tsx
@@ -1,9 +1,3 @@
-// eslint-disable-next-line import/order
-import React from "react";
-// import './expo-crypto-shim.js' --> Only for Expo SDK 48+
-//import "@walletconnect/react-native-compat";
-//import "@ethersproject/shims";
-
 import {
   useFonts,
   Exo_600SemiBold,
@@ -14,6 +8,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import { MetaMaskProvider } from "metamask-react";
+import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
 import { Platform } from "react-native";
 import { MenuProvider } from "react-native-popup-menu";
@@ -32,65 +27,12 @@ import { WalletsProvider } from "./packages/context/WalletsProvider";
 import { store } from "./packages/store/store";
 import { linking } from "./packages/utils/navigation";
 
-/*
-
-const sleep = (durationms: number) =>
-  new Promise((resolve) => setTimeout(resolve, durationms));
-
-let queue: string[] = [];
-
-(async () => {
-  //let index = 0;
-  while (true) {
-    if (!queue.length) {
-      await sleep(100);
-      continue;
-    }
-    const msg = queue[0];
-    try {
-      await axios.post(
-        "http://192.168.1.164:4242/push",
-        msg
-      );
-    } catch {
-      // axios.post("http://192.168.1.164:4242/push", `${JSON.stringify(err)}\n`);
-      // FIXME: find a way to show error to user
-    }
-    queue = queue.slice(1);
-    //index++;
-  }
-})();
-
-const remoteLog = (...args: any[]) => {
-  queue.push(JSON.stringify(args) + "\n");
-};
-
-const nativeLog = console.log;
-console.log = (...args) => {
-  remoteLog(...args);
-  nativeLog(...args);
-};
-const nativeError = console.error;
-console.error = (...args) => {
-  remoteLog(...args);
-  nativeError(...args);
-};
-const nativeWarn = console.warn;
-console.warn = (...args) => {
-  remoteLog(...args);
-  nativeWarn(...args);
-};
-
-*/
-
 const queryClient = new QueryClient();
 
 // it's here just to fix a TS2589 error
 type DefaultForm = {
   novalue: string;
 };
-
-// Web3Modal no work
 
 export default function App() {
   const methods = useForm<DefaultForm>();
