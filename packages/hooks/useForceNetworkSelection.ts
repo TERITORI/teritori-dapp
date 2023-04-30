bee617e4902bf50d8a8d8d67a2cd037a51e3596d
Norman Meier <norman@berty.tech>
2023-04-30 21:56:09 +0200

    feat: select testnet on force network when previously selected
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/hooks/useForceNetworkSelection.ts
+++ packages/hooks/useForceNetworkSelection.ts
@@ -2,6 +2,7 @@ import { useFocusEffect } from "@react-navigation/native";
 import { useCallback } from "react";
 
 import { useSelectedNetworkId } from "./useSelectedNetwork";
+import { getNetwork } from "../networks";
 import { setSelectedNetworkId } from "../store/slices/settings";
 import { useAppDispatch } from "../store/store";
 
@@ -9,8 +10,13 @@ export const useForceNetworkSelection = (networkIds: string[] | undefined) => {
   const selectedNetworkId = useSelectedNetworkId();
   const dispatch = useAppDispatch();
   const effect = useCallback(() => {
-    if (networkIds && !networkIds.includes(selectedNetworkId)) {
-      dispatch(setSelectedNetworkId(networkIds[0]));
+    if (networkIds?.length && !networkIds.includes(selectedNetworkId)) {
+      // use testnet if previous is testnet
+      const target = networkIds.find(
+        (id) =>
+          getNetwork(id)?.testnet === getNetwork(selectedNetworkId)?.testnet
+      );
+      dispatch(setSelectedNetworkId(target || networkIds[0]));
     }
   }, [dispatch, networkIds, selectedNetworkId]);
   useFocusEffect(effect);
