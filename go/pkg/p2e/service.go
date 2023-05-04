15f37f79a9f915994ebd1e42862537002596f8f9
yo1110 <tronghieu.ha@gmail.com>
2023-05-04 18:23:53 +0700

    fix: optimize leaderboard reset time to avoid corrupted staking situation

--- go/pkg/p2e/service.go
+++ go/pkg/p2e/service.go
@@ -2,6 +2,7 @@ package p2e
 
 import (
 	"context"
+	"time"
 
 	"github.com/TERITORI/teritori-dapp/go/internal/indexerdb"
 	"github.com/TERITORI/teritori-dapp/go/pkg/p2epb"
@@ -125,7 +126,8 @@ func (s *P2eService) Leaderboard(req *p2epb.LeaderboardRequest, srv p2epb.P2ESer
 }
 
 func (s *P2eService) CurrentSeason(ctx context.Context, req *p2epb.CurrentSeasonRequest) (*p2epb.CurrentSeasonResponse, error) {
-	currentSeason, remainingHp, err := GetCurrentSeason()
+	currentTime := time.Now().UTC()
+	currentSeason, remainingHp, err := GetSeasonByTime(currentTime)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get current season")
 	}
