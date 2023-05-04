5fade263df1238f8845db013c3e0e9eabac0f410
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-05-04 17:48:34 +0545

    Add friends list screen

--- packages/screens/Message/AddFriend.tsx
+++ packages/screens/Message/AddFriend.tsx
@@ -3,92 +3,87 @@ import { View, Text, Image } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 import FlexRow from "../../components/FlexRow";
+import { ScreenContainer } from "../../components/ScreenContainer";
 import { Separator } from "../../components/Separator";
 import AddFriendList from "../../components/addfriend/AddFriendList";
 import data from "../../components/addfriend/data";
 import { TertiaryBox } from "../../components/boxes/TertiaryBox";
-import { TextInputCustomBorder } from "../../components/inputs/TextInputCustomBorder";
+import { SearchInput } from "../../components/sorts/SearchInput";
 import { SpacerColumn, SpacerRow } from "../../components/spacer";
+import {
+  neutral33,
+  neutralA3,
+  primaryColor,
+  primaryTextColor,
+  secondaryColor,
+} from "../../utils/style/colors";
+import {
+  fontSemibold13,
+  fontSemibold14,
+  fontSemibold16,
+} from "../../utils/style/fonts";
 const AddFriend = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "#000" }}>
-      <Separator horizontal={false} />
-      <SpacerColumn size={2} />
+    <ScreenContainer>
+      <View style={{ backgroundColor: "#000" }}>
+        <Separator horizontal={false} />
+        <SpacerColumn size={2} />
 
-      <Text
-        style={{
-          color: "#FFFFFF",
-          fontWeight: "600",
-          fontSize: 15,
-          lineHeight: 18,
-        }}
-      >
-        Add a friend
-      </Text>
-      <Text
-        style={{
-          color: "#A3A3A3",
-          fontWeight: "600",
-          fontSize: 12,
-          lineHeight: 18,
-        }}
-      >
-        You can find a friend using .tori directory service.
-      </Text>
-      <SpacerColumn size={2} />
-      <View>
-        <FlexRow>
-          <TextInputCustomBorder
-            placeHolder="Search..."
-            style={{ backgroundColor: "#000" }}
-          />
-
-          <SpacerRow size={2} />
-          <TouchableOpacity>
-            <TertiaryBox
-              height={40}
-              mainContainerStyle={{
-                backgroundColor: "#16BBFF",
-                padding: 12,
-
-                alignItems: "center",
-              }}
-            >
-              <Text
-                style={{ color: "#2B0945", fontSize: 14, fontWeight: "600" }}
+        <Text style={[fontSemibold16, { color: secondaryColor }]}>
+          Add a friend
+        </Text>
+        <Text style={[fontSemibold13, { color: neutralA3 }]}>
+          You can find a friend using .tori directory service.
+        </Text>
+        <SpacerColumn size={2} />
+        <FlexRow justifyContent="space-between">
+          <View style={{ width: "70%" }}>
+            <SearchInput />
+          </View>
+          <SpacerRow size={1} />
+          <View style={{ width: "30%" }}>
+            <TouchableOpacity>
+              <TertiaryBox
+                height={36}
+                mainContainerStyle={{
+                  backgroundColor: primaryColor,
+                  padding: 8,
+                }}
               >
-                Find a friend
-              </Text>
-            </TertiaryBox>
-          </TouchableOpacity>
+                <Text style={[fontSemibold14, { color: primaryTextColor }]}>
+                  Find a friend
+                </Text>
+              </TertiaryBox>
+            </TouchableOpacity>
+          </View>
         </FlexRow>
-      </View>
-      <SpacerColumn size={2} />
-      <Separator horizontal={false} />
-      <SpacerColumn size={2} />
-      {data.length > 0 ? (
-        data.map((item) => (
-          <AddFriendList
-            key={item.id}
-            avatar={item.avatar}
-            name={item.name}
-            isOnline={item.isOnline}
+        <SpacerColumn size={2} />
+        <Separator horizontal={false} color={neutral33} />
+        <SpacerColumn size={1} />
+        {data.length > 0 ? (
+          data.map((item) => (
+            <AddFriendList
+              key={item.id}
+              avatar={item.avatar}
+              name={item.name}
+              isOnline={item.isOnline}
+            />
+          ))
+        ) : (
+          <Image
+            source={require("../../../assets/icons/illustration.png")}
+            style={{
+              height: 300,
+              width: 300,
+              alignSelf: "center",
+              marginTop: 40,
+            }}
           />
-        ))
-      ) : (
-        <Image
-          source={require("../../../assets/icons/illustration.png")}
-          style={{
-            height: 300,
-            width: 300,
-            alignSelf: "center",
-            marginTop: 40,
-          }}
-        />
-      )}
+        )}
 
-      <SpacerColumn size={2} />
-    </View>
+        <SpacerColumn size={2} />
+      </View>
+    </ScreenContainer>
   );
 };
 export default AddFriend;
