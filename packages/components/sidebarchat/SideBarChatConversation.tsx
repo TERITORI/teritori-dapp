018ba18af83ec914a040c4110aa493b46bd35f36
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-05-11 17:45:47 +0545

    Changes UI of Conversation

--- packages/components/sidebarchat/SideBarChatConversation.tsx
+++ packages/components/sidebarchat/SideBarChatConversation.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Avatar, Badge } from "react-native-paper";
 
-import doublecheck from "../../../assets/icons/doublecheck.svg";
+import Doublecheck from "../../../assets/icons/doublecheck.svg";
 import { Separator } from "../../components/Separator";
 import { neutral22, neutralA3, secondaryColor } from "../../utils/style/colors";
 import {
@@ -22,6 +22,7 @@ type Props = {
 
   chat: string;
   time: string;
+  iconCheck: any;
 };
 
 const SideBarChatConversation: React.FC<Props> = ({
@@ -30,50 +31,52 @@ const SideBarChatConversation: React.FC<Props> = ({
   isOnline,
   chat,
   time,
+  iconCheck,
 }) => {
   const onlineStatusBadgeColor = isOnline ? "green" : "yellow";
   return (
     <View style={styles.container}>
-      <FlexRow justifyContent="space-between" style={{ marginTop: 10 }}>
+      <FlexRow justifyContent="space-between">
         <View>
           <FlexRow>
-            <Avatar.Image size={35} source={avatar} />
+            <Avatar.Image size={40} source={avatar} />
             <Badge
               style={{
                 position: "absolute",
-                top: 20,
-                left: 25,
+                top: 30,
+                left: 30,
 
                 backgroundColor: onlineStatusBadgeColor,
               }}
               size={12}
             />
-            <SpacerRow size={2} />
+            <SpacerRow size={1.5} />
             <View>
               <FlexRow>
                 <BrandText style={[fontSemibold13, { color: secondaryColor }]}>
                   {name}
                 </BrandText>
               </FlexRow>
-              <SpacerColumn size={0.2} />
+              <SpacerColumn size={0.5} />
               <BrandText style={[fontSemibold11, { color: neutralA3 }]}>
                 {chat}
               </BrandText>
             </View>
           </FlexRow>
         </View>
-        <View style={{ marginTop: -15 }}>
+        <View>
           <FlexRow>
             <BrandText style={[fontMedium10, { color: secondaryColor }]}>
               {time}
             </BrandText>
             <SpacerRow size={0.6} />
-            <SVG width={16} height={16} source={doublecheck} />
+            <SVG width={16} height={16} source={iconCheck} />
           </FlexRow>
         </View>
       </FlexRow>
       <SpacerColumn size={1.5} />
       <Separator color={neutral22} />
+      <SpacerColumn size={1.5} />
     </View>
   );
 };
