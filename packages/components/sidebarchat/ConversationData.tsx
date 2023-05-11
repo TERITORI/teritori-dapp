407d3c2f64aea260719867a06153b622c97db3af
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-05-11 17:45:47 +0545

    Changes UI of Conversation

--- packages/components/sidebarchat/ConversationData.tsx
+++ packages/components/sidebarchat/ConversationData.tsx
@@ -1,3 +1,7 @@
+import Doublecheck from "../../../assets/icons/doublecheck.svg";
+import singleCheck from "../../../assets/icons/singlecheck.svg";
+import warning from "../../../assets/icons/warning.svg";
+
 const ConversationData = [
   {
     name: "Eleanor Pena",
@@ -5,6 +9,7 @@ const ConversationData = [
     avatar: require("../../../assets/icons/ava.png"),
     chat: "Hi, I know we haven’t talked …",
     time: "9:30",
+    icon: Doublecheck,
   },
   {
     name: "John",
@@ -12,6 +17,7 @@ const ConversationData = [
     avatar: require("../../../assets/icons/ava.png"),
     chat: " Hi, I know we haven’tt …",
     time: "9:00",
+    icon: warning,
   },
   {
     name: "David",
@@ -19,6 +25,7 @@ const ConversationData = [
     avatar: require("../../../assets/icons/ava.png"),
     chat: "Hi, I know we haven’t  …",
     time: "3:30",
+    icon: singleCheck,
   },
   {
     name: "William",
@@ -26,6 +33,7 @@ const ConversationData = [
     avatar: require("../../../assets/icons/ava.png"),
     chat: "Hi, I know we haven’t talked  …",
     time: "5:00",
+    icon: Doublecheck,
   },
   {
     name: "Charles",
@@ -33,6 +41,7 @@ const ConversationData = [
     avatar: require("../../../assets/icons/ava.png"),
     chat: "Hi, I know we haven’t talked …",
     time: "4:10",
+    icon: warning,
   },
   {
     name: "Robert",
@@ -40,6 +49,7 @@ const ConversationData = [
     avatar: require("../../../assets/icons/ava.png"),
     chat: "Hi, I know we haven’t talked  …",
     time: "2:30",
+    icon: singleCheck,
   },
 ];
 export default ConversationData;
