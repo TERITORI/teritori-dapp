095639488b44aede30623e453b378237cd3e973c
Norman Meier <norman@berty.tech>
2023-05-15 19:08:05 +0200

    feat: weshnet example
    Signed-off-by: Norman Meier <norman@berty.tech>

---
+++ packages/screens/Chat/ChatScreen.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import { BrandText } from "../../components/BrandText";
+import { ScreenContainer } from "../../components/ScreenContainer";
+import { PrimaryButton } from "../../components/buttons/PrimaryButton";
+import { ScreenFC } from "../../utils/navigation";
+import { createWeshClient } from "../../weshnet";
+
+/*
+  To test this:
+  - clone this branch https://github.com/TERITORI/weshnet/tree/first-contact
+  - run `go run ./cmd/weshd` at weshnet root
+  - navigate to `/chat` in teritori ui
+  - click `Test` button
+*/
+
+export const ChatScreen: ScreenFC<"Chat"> = () => {
+  const [text, setText] = useState("");
+  return (
+    <ScreenContainer>
+      <PrimaryButton
+        size="M"
+        text="Test"
+        loader
+        onPress={async () => {
+          try {
+            const weshClient = createWeshClient("http://localhost:4242");
+            const weshConfig = await weshClient.ServiceGetConfiguration({});
+            console.log("wesh test success", weshConfig);
+            setText(JSON.stringify(weshConfig));
+          } catch (err) {
+            console.error("wesh test error", err);
+            setText(`${err}`);
+          }
+        }}
+      />
+      <BrandText>{text}</BrandText>
+    </ScreenContainer>
+  );
+};
