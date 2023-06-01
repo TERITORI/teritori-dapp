2c0e1781ea336bc1dbff65a2dd3ebcccfd8204e9
Norman Meier <norman@berty.tech>
2023-06-01 16:24:56 +0200

    chore: remove useless webpack config
    Signed-off-by: Norman Meier <norman@berty.tech>

--- webpack.config.js
+++ webpack.config.js
@@ -27,13 +27,6 @@ module.exports = async function (env, argv) {
     },
   });
 
-  config.module.rules.push({
-    test: /.*rn-range-slider/,
-    use: {
-      loader: "babel-loader",
-    },
-  });
-
   config.module.rules.forEach((rule) => {
     if (rule.oneOf) {
       rule.oneOf.unshift({
