f9ad49d69917b6537eed01e162992372c5188e34
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-06-19 21:07:49 -0600

    file message

---
+++ packages/utils/file.ts
@@ -0,0 +1,12 @@
+export const convertFileToBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
