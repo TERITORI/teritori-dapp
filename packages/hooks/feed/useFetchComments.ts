e08c001ca64c5ecbd52d74b7344e3c72100e6e1c
yo1110 <tronghieu.ha@gmail.com>
2023-08-07 06:14:58 +0700

    feat: gno social feeds (#642)
    * wip: handle post for gno
    
    * wip: integrating gno boards
    
    * wip: working on get gno feed
    
    * feat: hanndle actions/comments/pagination for posts
    
    * chore: add missing files
    
    * feat: use network features to handle menu
    
    * chore: update networkjson
    
    * feat: add gno teritori
    
    * feat: add teritori gno network
    
    * chore: fix lint
    
    * chore: add update network
    
    * fix: fix lint
    
    * feat: update contract social_feeds_v3 + fix teritori test network
    
    * chore: remove default val

--- packages/hooks/feed/useFetchComments.ts
+++ packages/hooks/feed/useFetchComments.ts
@@ -1,8 +1,17 @@
+import { GnoJSONRPCProvider } from "@gnolang/gno-js-client";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
+import { Post } from "../../api/feed/v1/feed";
 import { nonSigningSocialFeedClient } from "../../client-creators/socialFeedClient";
+import {
+  GNO_SOCIAL_FEEDS_PKG_PATH,
+  TERITORI_FEED_ID,
+} from "../../components/socialFeed/const";
+import { decodeGnoPost } from "../../components/socialFeed/utils";
 import { PostResult } from "../../contracts-clients/teritori-social-feed/TeritoriSocialFeed.types";
-import { useSelectedNetworkId } from "../useSelectedNetwork";
+import { GnoNetworkInfo, NetworkKind } from "../../networks";
+import { extractGnoString } from "../../utils/gno";
+import { useSelectedNetworkInfo } from "../useSelectedNetwork";
 
 export type FetchCommentResponse = {
   list: PostResult[];
@@ -20,30 +29,66 @@ type ConfigType = {
   enabled?: boolean;
 };
 
+const fetchTeritoriComments = async (
+  networkId: string,
+  pageParam: number,
+  parentId?: string
+) => {
+  const client = await nonSigningSocialFeedClient({
+    networkId,
+  });
+
+  const subComment = await client.querySubPosts({
+    count: 5,
+    from: pageParam || 0,
+    sort: "desc",
+    identifier: parentId || "",
+  });
+
+  return { list: subComment };
+};
+
+const fetchGnoComments = async (
+  selectedNetwork: GnoNetworkInfo,
+  parentId: string
+) => {
+  const provider = new GnoJSONRPCProvider(selectedNetwork.endpoint);
+  const output = await provider.evaluateExpression(
+    GNO_SOCIAL_FEEDS_PKG_PATH,
+    `GetComments(${TERITORI_FEED_ID}, ${parentId})`
+  );
+
+  const posts: Post[] = [];
+
+  const outputStr = extractGnoString(output);
+  for (const postData of outputStr.split(",")) {
+    const post = decodeGnoPost(selectedNetwork.id, postData);
+    posts.push(post);
+  }
+  return { list: posts.sort((p1, p2) => p2.createdAt - p1.createdAt) };
+};
+
 export const useFetchComments = ({
   parentId,
   totalCount,
   enabled,
 }: ConfigType) => {
   // variable
-  const selectedNetworkId = useSelectedNetworkId();
+  const selectedNetwork = useSelectedNetworkInfo();
 
   // request
   const data = useInfiniteQuery<FetchCommentResponse>(
-    ["FetchComment", parentId],
+    ["FetchComment", parentId, selectedNetwork?.id],
     async ({ pageParam }) => {
-      const client = await nonSigningSocialFeedClient({
-        networkId: selectedNetworkId,
-      });
-
-      const subComment = await client.querySubPosts({
-        count: 5,
-        from: pageParam || 0,
-        sort: "desc",
-        identifier: parentId || "",
-      });
-
-      return { list: subComment };
+      if (selectedNetwork?.kind === NetworkKind.Gno) {
+        return await fetchGnoComments(selectedNetwork, parentId || "");
+      } else {
+        return await fetchTeritoriComments(
+          selectedNetwork?.id || "",
+          pageParam,
+          parentId
+        );
+      }
     },
     {
       getNextPageParam: (_, pages) => {
