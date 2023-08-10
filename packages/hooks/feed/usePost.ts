a21741f4f81fc524a38c05448f2151a9653339f3
yo1110 <tronghieu.ha@gmail.com>
2023-08-10 23:54:23 +0700

    chore(social feed): disambiguate created_by (#666)
    * fix: handle ns more properly
    
    * wip: standarize userId for social_feeds
    
    * wip: standarize userId vs userAddress
    
    * fix: stadardize userId vs userAddress
    
    * fix: change id for proto field author_id
    
    * fix: proto gen
    
    * fix: revert proto field id for author_id

--- packages/hooks/feed/usePost.ts
+++ packages/hooks/feed/usePost.ts
@@ -1,18 +1,18 @@
 import { GnoJSONRPCProvider } from "@gnolang/gno-js-client";
 import { useQuery } from "@tanstack/react-query";
 
+import { Post } from "../../api/feed/v1/feed";
 import { nonSigningSocialFeedClient } from "../../client-creators/socialFeedClient";
 import { GNO_SOCIAL_FEEDS_PKG_PATH } from "../../components/socialFeed/const";
 import { decodeGnoPost } from "../../components/socialFeed/utils";
-import { NetworkKind, getNetwork } from "../../networks";
+import { NetworkKind, getNetwork, getUserId } from "../../networks";
 import { extractGnoString } from "../../utils/gno";
 
 export const usePost = (id: string, networkId: string) => {
-  const { data, ...other } = useQuery(
+  const { data, ...other } = useQuery<Post>(
     ["social-post", id, networkId],
     async () => {
       const network = getNetwork(networkId);
-
       if (network?.kind === NetworkKind.Gno) {
         const provider = new GnoJSONRPCProvider(network.endpoint);
         const output = await provider.evaluateExpression(
@@ -21,24 +21,26 @@ export const usePost = (id: string, networkId: string) => {
         );
 
         const postData = extractGnoString(output);
-        const post = decodeGnoPost(network.id, postData, false);
-
-        return {
-          identifier: id,
-          parent_post_identifier: post.parentPostIdentifier, // identifier of linked post
-          category: post.category, // PostCategory
-          metadata: post.metadata,
-          reactions: post.reactions,
-          user_reactions: [], // TODO: What this for ?
-          post_by: post.createdBy,
-          deleted: post.isDeleted,
-          sub_post_length: 0, // TODO: handle this
-        };
+        return decodeGnoPost(networkId, postData);
       } else {
         const client = await nonSigningSocialFeedClient({
           networkId,
         });
-        return await client.queryPost({ identifier: id });
+        const res = await client.queryPost({ identifier: id });
+
+        const post: Post = {
+          identifier: id,
+          parentPostIdentifier: res.parent_post_identifier || "",
+          category: res.category,
+          metadata: res.metadata,
+          reactions: res.reactions,
+          authorId: getUserId(networkId, res.post_by),
+          isDeleted: res.deleted,
+          subPostLength: res.sub_post_length,
+          createdAt: res.created_at,
+          tipAmount: res.tip_amount,
+        };
+        return post;
       }
     }
   );
