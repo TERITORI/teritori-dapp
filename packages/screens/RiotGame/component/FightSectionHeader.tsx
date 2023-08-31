c3651ab77a3ce5caf2b03602527db03196748b10
hthieu1110 <tronghieu.ha@gmail.com>
2023-08-31 14:24:04 +0700

    chore: fix lint

--- packages/screens/RiotGame/component/FightSectionHeader.tsx
+++ packages/screens/RiotGame/component/FightSectionHeader.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-import { SimpleButton } from "../../../components/buttons/SimpleButton";
 import addCircleSVG from "../../../../assets/icons/add-circle.svg";
 import { BrandText } from "../../../components/BrandText";
 import FlexRow from "../../../components/FlexRow";
+import { SimpleButton } from "../../../components/buttons/SimpleButton";
 import { useAppNavigation } from "../../../utils/navigation";
 import { yellowDefault, neutral17 } from "../../../utils/style/colors";
 import { fontMedium48, fontSemibold28 } from "../../../utils/style/fonts";
