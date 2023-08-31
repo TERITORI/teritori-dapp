95cae91b60a76a8e39c0138329404c6c2010208c
Eng. Juan Combetto <omniwired@gmail.com>
2023-08-31 14:14:17 +0900

    feat(leap-wallet-integration): Add useIsLeapConnected hook
    Add a new hook called useIsLeapConnected that checks if the user's wallets contain a Leap wallet. This hook will be used to determine if the Leap wallet is connected.

---
+++ packages/hooks/useIsLeapConnected.ts
@@ -0,0 +1,10 @@
+import { useWallets } from "../context/WalletsProvider";
+import { WalletProvider } from "../utils/walletProvider";
+
+// Check if the user's wallets contain a Leap wallet
+export const useIsLeapConnected = () => {
+  const { wallets } = useWallets();
+  return !!wallets.find(
+    (wallet) => wallet.provider === WalletProvider.Leap && wallet.connected
+  );
+};
