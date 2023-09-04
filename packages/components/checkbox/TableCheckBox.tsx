e271b561104f85b29ec42f3f592d79fed0992935
Eng. Juan Combetto <omniwired@gmail.com>
2023-09-04 13:33:35 +0900

    feat(components): optimize TableCheckBox styles (feat/freelance-services)

--- packages/components/checkbox/TableCheckBox.tsx
+++ packages/components/checkbox/TableCheckBox.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Pressable, View, ViewStyle } from "react-native";
 
 import Tick from "../../../assets/icons/tick.svg";
 import {
@@ -24,37 +24,35 @@ export const TableCheckBox: React.FC<TableCheckBoxProps> = ({
 }) => {
   const unitHeight = 38;
 
-  const styles = StyleSheet.create({
-    unitContainer: {
-      width: "100%",
-      height: unitHeight,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: neutral00,
-    },
-    checked: {
-      width: zoom * layout.spacing_x2_5,
-      height: zoom * layout.spacing_x2_5,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: primaryColor,
-      borderRadius: 5,
-    },
-    unchecked: {
-      width: zoom * layout.spacing_x2_5,
-      height: zoom * layout.spacing_x2_5,
-      borderWidth: 1,
-      borderColor: neutralA3,
-      borderRadius: 5,
-      backgroundColor: neutral17,
-    },
-  });
+  const unitContainerStyles: ViewStyle = {
+    width: "100%",
+    height: unitHeight,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: neutral00,
+  };
+  const checkedStyles: ViewStyle = {
+    width: zoom * layout.spacing_x2_5,
+    height: zoom * layout.spacing_x2_5,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: primaryColor,
+    borderRadius: 5,
+  };
+  const uncheckedStyles: ViewStyle = {
+    width: zoom * layout.spacing_x2_5,
+    height: zoom * layout.spacing_x2_5,
+    borderWidth: 1,
+    borderColor: neutralA3,
+    borderRadius: 5,
+    backgroundColor: neutral17,
+  };
 
   return (
-    <Pressable onPress={() => setValue(!value)} style={styles.unitContainer}>
-      <View style={value ? styles.checked : styles.unchecked}>
+    <Pressable onPress={() => setValue(!value)} style={unitContainerStyles}>
+      <View style={value ? checkedStyles : uncheckedStyles}>
         {value && <SVG source={Tick} height={zoom * 10} width={zoom * 10} />}
       </View>
     </Pressable>
