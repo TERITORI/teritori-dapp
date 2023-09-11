cccbe221fb3d20cc05af13ffa5b0e560fd7a14ab
Eng. Juan Combetto <omniwired@gmail.com>
2023-09-11 18:03:01 +0900

    feat(RiotGame): remove StyleSheet.create in CenterSection.tsx

--- packages/screens/RiotGame/component/CenterSection.tsx
+++ packages/screens/RiotGame/component/CenterSection.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { Animated, StyleSheet, View } from "react-native";
+import { Animated, View } from "react-native";
 
 import startButtonSVG from "../../../../assets/game/start-button.svg";
 import { BrandText } from "../../../components/BrandText";
@@ -46,23 +46,29 @@ export const CenterSection: React.FC<CenterSectionProps> = ({
   return (
     <>
       <View
-        style={[
-          styles.section,
-          {
-            width: cardWidth * 2,
-            height: cardHeight,
-            paddingHorizontal: layout.spacing_x2_5,
-          },
-        ]}
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,
+          margin: "auto",
+          justifyContent: "center",
+          alignItems: "center",
+
+          width: cardWidth * 2,
+          height: cardHeight,
+          paddingHorizontal: layout.spacing_x2_5,
+        }}
       >
         <View
-          style={[
-            styles.buttonContainer,
-            {
-              width: cardWidth * 2 - layout.spacing_x2_5 * 2,
-              height: cardHeight * 0.37,
-            },
-          ]}
+          style={{
+            position: "relative",
+            justifyContent: "center",
+            alignItems: "center",
+            width: cardWidth * 2 - layout.spacing_x2_5 * 2,
+            height: cardHeight * 0.37,
+          }}
         >
           <Animated.View
             style={[
@@ -82,7 +88,20 @@ export const CenterSection: React.FC<CenterSectionProps> = ({
               width={cardWidth * 2 - layout.spacing_x2_5 * 2}
               height={cardHeight * 0.37}
             />
-            <BrandText onPress={onPress} style={styles.startGameText}>
+            <BrandText
+              onPress={onPress}
+              style={[
+                fontBold16,
+                {
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                },
+              ]}
+            >
               Start the Game
             </BrandText>
           </Animated.View>
@@ -95,36 +114,3 @@ export const CenterSection: React.FC<CenterSectionProps> = ({
     </>
   );
 };
-// FIXME: remove StyleSheet.create
-// eslint-disable-next-line no-restricted-syntax
-const styles = StyleSheet.create({
-  section: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    margin: "auto",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonContainer: {
-    position: "relative",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  positionAbsolute: {
-    position: "absolute",
-  },
-  startGameText: StyleSheet.flatten([
-    fontBold16,
-    {
-      position: "absolute",
-      marginLeft: "auto",
-      marginRight: "auto",
-      left: 0,
-      right: 0,
-      textAlign: "center",
-    },
-  ]),
-});
