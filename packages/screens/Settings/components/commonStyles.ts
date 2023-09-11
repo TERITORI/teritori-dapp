6def855a25bee4f706c436dd1fdc33a0ef4abf27
WaDadidou <50441633+WaDadidou@users.noreply.github.com>
2023-09-11 12:33:14 +0200

    fix: settings page responsive (#696)

--- packages/screens/Settings/components/commonStyles.ts
+++ packages/screens/Settings/components/commonStyles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet, useWindowDimensions } from "react-native";
+import { TextStyle, useWindowDimensions, ViewStyle } from "react-native";
 
 import {
   neutral00,
@@ -14,72 +14,66 @@ import {
 import { layout } from "../../../utils/style/layout";
 
 const MOBILE_WIDTH = 768;
-const padWidth = 1024;
 
 export const useCommonStyles = () => {
   const { width } = useWindowDimensions();
 
-  // FIXME: remove StyleSheet.create
-  // eslint-disable-next-line no-restricted-syntax
-  return StyleSheet.create({
-    switchBox: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      margin: layout.spacing_x1,
-    },
-    bigTitle: StyleSheet.flatten([
-      fontSemibold20,
-      {
-        paddingTop: layout.spacing_x4,
-        paddingLeft: layout.spacing_x2,
-      },
-    ]),
-    cardSubtitle: StyleSheet.flatten([
-      fontSemibold14,
-      {
-        color: neutralA3,
-      },
-    ]),
-    cardContent: StyleSheet.flatten([
-      fontSemibold12,
-      {
-        color: neutralA3,
-        width:
-          width < MOBILE_WIDTH
-            ? (width - 75) * 0.9 - 2 * layout.spacing_x2 - 50
-            : undefined,
-        marginTop: layout.spacing_x0_5,
-      },
-    ]),
-    cardContainer: {
-      width: "100%",
-      borderRadius: layout.spacing_x1_5,
-      backgroundColor: neutral17,
-      padding: layout.spacing_x2,
-    },
-    cardTitle: StyleSheet.flatten([fontSemibold14]),
+  const switchBox: ViewStyle = {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: layout.spacing_x1,
+  };
+  const bigTitle: TextStyle = {
+    ...fontSemibold20,
 
-    pageContainer: {
-      width:
-        width < padWidth
-          ? width < MOBILE_WIDTH
-            ? (width - 75) * 0.9
-            : (width - 209) * 0.9
-          : 676,
-      margin: "auto",
-      paddingTop: layout.contentSpacing,
-    },
-    apiInput: {
-      fontSize: 14,
-      color: "white",
-      fontFamily: "Exo_600SemiBold",
-      padding: layout.spacing_x2,
-      borderColor: neutral33,
-      borderWidth: 1,
-      backgroundColor: neutral00,
-      borderRadius: layout.spacing_x1_5,
-    },
-  });
+    paddingTop: layout.spacing_x4,
+    paddingLeft: layout.spacing_x2,
+  };
+  const cardSubtitle: TextStyle = {
+    ...fontSemibold14,
+
+    color: neutralA3,
+  };
+  const cardContent: TextStyle = {
+    ...fontSemibold12,
+
+    color: neutralA3,
+    width:
+      width < MOBILE_WIDTH
+        ? (width - 75) * 0.9 - 2 * layout.spacing_x2 - 50
+        : undefined,
+    marginTop: layout.spacing_x0_5,
+  };
+  const cardContainer: ViewStyle = {
+    width: "100%",
+    borderRadius: layout.spacing_x1_5,
+    backgroundColor: neutral17,
+    padding: layout.spacing_x2,
+  };
+  const cardTitle: TextStyle = { ...fontSemibold14 };
+  const pageContainer: ViewStyle = {
+    paddingVertical: layout.contentSpacing,
+  };
+  const apiInput: TextStyle = {
+    ...fontSemibold14,
+    padding: layout.spacing_x2,
+    borderColor: neutral33,
+    borderWidth: 1,
+    backgroundColor: neutral00,
+    borderRadius: layout.spacing_x1_5,
+  };
+
+  return {
+    switchBox,
+    bigTitle,
+    cardSubtitle,
+    cardContent,
+    cardContainer,
+    cardTitle,
+    pageContainer,
+    apiInput,
+  };
 };
