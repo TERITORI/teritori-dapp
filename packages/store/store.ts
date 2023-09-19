212f7924575d87f2db5b6438604e1eb68c1ed63a
clegirar <clemntgirard@gmail.com>
2023-09-19 14:36:18 +0200

    fix: all unused exports from packages/store
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- packages/store/store.ts
+++ packages/store/store.ts
@@ -54,10 +54,8 @@ export const store = configureStore({
     getDefaultMiddleware({ serializableCheck: false }),
 });
 
-export const persistor = persistStore(store);
-
 export type RootState = ReturnType<typeof store.getState>;
 
-export type AppDispatch = typeof store.dispatch;
+type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
