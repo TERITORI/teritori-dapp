856b438d30ad12892111ad25a0cfe407f3a77087
Eng. Juan Combetto <omniwired@gmail.com>
2023-09-27 21:05:04 +0800

    refactor(Dropdown): remove unused import and callback functions

--- packages/components/Dropdown.tsx
+++ packages/components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   LayoutChangeEvent,
   TouchableOpacity,
