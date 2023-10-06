ab248abf3305a9c37f7278481fbb2438d9148729
Norman Meier <norman@berty.tech>
2023-10-06 16:53:42 +0200

    chore: make contract info useful for other people
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/scripts/contractInfo.ts
+++ packages/scripts/contractInfo.ts
@@ -1,63 +1,83 @@
 import { program } from "commander";
 
-import { mustGetNonSigningCosmWasmClient } from "../networks";
+import {
+  mustGetNonSigningCosmWasmClient,
+  parseNetworkObjectId,
+} from "../networks";
 
 const main = async () => {
-  program
-    .option("-n, --network <string>", "teritori")
-    .option("-c, --contract <string>");
+  program.argument("<contract-id>");
   program.parse();
   // FIXME: use typesafe commander
 
-  console.log("args:", program.opts());
+  const [contractId, ...additionalQueries] = program.args;
+  const [network, contractAddress] = parseNetworkObjectId(contractId);
+  if (!network || !contractAddress) {
+    throw new Error("Invalid contract id");
+  }
+  const client = await mustGetNonSigningCosmWasmClient(network?.id);
 
-  const options = program.opts();
-  const client = await mustGetNonSigningCosmWasmClient(options.network);
-  const contract = await client.getContract(options.contract);
-  console.log("contract:", contract);
-  const { data, ...wasm } = await client.getCodeDetails(contract.codeId);
-  console.log("wasm:", wasm);
-  const queries: string[] = [];
+  const all: { [key: string]: any } = {};
   try {
-    await client.queryContractSmart(options.contract, {
-      __this_should_not_exist: {},
-    });
-    console.log("Failed to query contract interface");
-  } catch (err) {
-    if (err instanceof Error) {
-      const message = err.message;
-      const match = message.match(/expected one of (.+?):/);
-      if (match?.length === 2) {
-        queries.push(
-          ...match[1].split(", ").map((s) => s.slice(1, s.length - 1))
-        );
-      } else if (!message.includes("no variants")) {
-        console.log(err);
-      }
-    } else {
-      console.log(err);
-    }
-  }
-  console.log("queries:", queries);
-  for (const test of [
-    "admin",
-    "info",
-    "config",
-    "hooks",
-    "proposal_modules",
-    "voting_module",
-    "group_contract",
-    "proposal_creation_policy",
-  ].filter((m) => queries.includes(m))) {
+    const contract = await client.getContract(contractAddress);
+    all["contract"] = contract;
+    const { data, ...wasm } = await client.getCodeDetails(contract.codeId);
+    all["wasm"] = wasm;
+    const queries: string[] = [];
     try {
-      const res = await client.queryContractSmart(options.contract, {
-        [test]: {},
+      await client.queryContractSmart(contractAddress, {
+        __this_should_not_exist: {},
       });
-      console.log(test + ":", res);
+      all["interfaceQueryError"] = new Error(
+        "query should have failed"
+      ).toString();
     } catch (err) {
-      console.log(err);
+      if (err instanceof Error) {
+        const message = err.message;
+        let match = message.match(/expected one of (.+?):/);
+        if (match?.length !== 2) {
+          match = message.match(/expected (.+?):/);
+        }
+        if (match?.length === 2) {
+          let parts;
+          if (match[1].includes(", ")) {
+            parts = match[1].split(", ");
+          } else if (match[1].includes(" or ")) {
+            parts = match[1].split(" or ");
+          }
+          queries.push(...(parts || []).map((s) => s.slice(1, s.length - 1)));
+        } else {
+          all["interfaceQueryError"] = new Error(
+            "unexpected message shape"
+          ).toString();
+        }
+      } else {
+        all["interfaceQueryError"] = err.toString();
+      }
     }
+    all["queries"] = queries;
+    all.results = {};
+    all.queriesErrors = [];
+    for (const test of [
+      "admin",
+      "admin_address",
+      "info",
+      "config",
+      ...additionalQueries,
+    ].filter((m) => queries.includes(m))) {
+      try {
+        const res = await client.queryContractSmart(contractAddress, {
+          [test]: {},
+        });
+        all.results[test] = res;
+      } catch (err) {
+        all.queriesErrors.push(err.toString());
+      }
+    }
+  } catch (err) {
+    all["error"] = err.toString();
   }
+  console.log(JSON.stringify(all, null, 4));
 };
 
 main();
