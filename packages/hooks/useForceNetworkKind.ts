f66f2383e5d3a1619316d3d4f25ba5bd60c3cf1b
Norman Meier <norman@berty.tech>
2023-10-06 11:01:56 +0200

    chore: factorize
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/hooks/useForceNetworkKind.ts
+++ packages/hooks/useForceNetworkKind.ts
@@ -1,28 +1,25 @@
 import { useFocusEffect } from "@react-navigation/native";
 import { useCallback } from "react";
-import { useSelector } from "react-redux";
 
+import { useEnabledNetworks } from "./useEnabledNetworks";
 import { useSelectedNetworkKind } from "./useSelectedNetwork";
-import { NetworkKind, allNetworks } from "../networks";
-import {
-  selectNetworksSettings,
-  setSelectedNetworkId,
-} from "../store/slices/settings";
+import { NetworkKind } from "../networks";
+import { setSelectedNetworkId } from "../store/slices/settings";
 import { useAppDispatch } from "../store/store";
 
 export const useForceNetworkKind = (networkKind: NetworkKind | undefined) => {
   const selectedNetworkKind = useSelectedNetworkKind();
-  const networksSettings = useSelector(selectNetworksSettings);
+  const enabledNetworks = useEnabledNetworks();
   const dispatch = useAppDispatch();
   const effect = useCallback(() => {
     if (networkKind && networkKind !== selectedNetworkKind) {
-      const newNetwork = allNetworks
-        .filter((n) => networksSettings[n.id]?.enabled)
-        .find((network) => network.kind === networkKind);
+      const newNetwork = enabledNetworks.find(
+        (network) => network.kind === networkKind
+      );
       if (newNetwork) {
         dispatch(setSelectedNetworkId(newNetwork.id));
       }
     }
-  }, [dispatch, networkKind, networksSettings, selectedNetworkKind]);
+  }, [dispatch, enabledNetworks, networkKind, selectedNetworkKind]);
   useFocusEffect(effect);
 };
