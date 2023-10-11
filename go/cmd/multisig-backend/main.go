37e577d7a9232bd7d1f494967f78da7ab5ebea23
Norman Meier <norman@berty.tech>
2023-10-11 13:56:50 +0200

    feat: allow to pass opts to multisig backend cli
    Signed-off-by: Norman Meier <norman@berty.tech>

--- go/cmd/multisig-backend/main.go
+++ go/cmd/multisig-backend/main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/TERITORI/teritori-dapp/go/pkg/multisig"
 	"github.com/TERITORI/teritori-dapp/go/pkg/multisigpb"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"github.com/peterbourgon/ff/v3"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	port := 9091
+	fs := flag.NewFlagSet("cosmos-multisig-backend", flag.ContinueOnError)
+	var (
+		portFlag   = fs.Int("port", 9091, "port for grpc-web server")
+		dbPathFlag = fs.String("db-path", "multisig.db", "path to database")
+	)
+	if err := ff.Parse(fs, os.Args[1:],
+		ff.WithEnvVars(),
+		ff.WithIgnoreUndefined(true),
+		ff.WithConfigFile(".env"),
+		ff.WithConfigFileParser(ff.EnvParser),
+		ff.WithAllowMissingConfigFile(true),
+	); err != nil {
+		panic(errors.Wrap(err, "failed to parse flags"))
+	}
+
+	port := *portFlag
+	dbPath := *dbPathFlag
 
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		panic(errors.Wrap(err, "failed to create logger"))
 	}
 
-	svc, err := multisig.NewMultisigService(multisig.MultisigServiceOpts{})
+	svc, err := multisig.NewMultisigService(multisig.MultisigServiceOpts{
+		DBPath: dbPath,
+		Logger: logger,
+	})
 	if err != nil {
 		panic(errors.Wrap(err, "failed to create multisig service"))
 	}
