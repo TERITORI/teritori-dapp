aa1cf65861027709e827fa365fccb8be24faf360
Eng. Juan Combetto <omniwired@gmail.com>
2023-10-23 14:05:31 +0900

    feat(Notifications): dismiss notifications

--- go/internal/indexerdb/notification.go
+++ go/internal/indexerdb/notification.go
@@ -1,9 +1,11 @@
 package indexerdb
 
-import "github.com/TERITORI/teritori-dapp/go/pkg/networks"
+import (
+	"github.com/TERITORI/teritori-dapp/go/pkg/networks"
+)
 
 type Notification struct {
-	id        int64           `gorm:"primaryKey"`
+	Id        int64           `gorm:"primaryKey"`
 	UserId    networks.UserID `gorm:"index"`
 	TriggerBy networks.UserID
 	Body      string
