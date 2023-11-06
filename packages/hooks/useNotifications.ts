765cb1274f991d94dedfb36142108c845e7d8504
Norman Meier <norman@berty.tech>
2023-11-06 03:23:45 +0100

    chore: don't spam errors if backend is not defined for network
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/hooks/useNotifications.ts
+++ packages/hooks/useNotifications.ts
@@ -2,20 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 
 import { NotificationsRequest } from "../api/notification/v1/notification";
 import { parseUserId } from "../networks";
-import { mustGetNotificationClient } from "../utils/backend";
+import { getNotificationClient } from "../utils/backend";
 
 export const useNotifications = (req: Partial<NotificationsRequest>) => {
   const { data } = useQuery(
     ["notifications", req],
     async () => {
       const [network] = parseUserId(req?.userId);
-
       if (!network) {
         return [];
       }
 
-      const notificationService = mustGetNotificationClient(network.id);
-      const { notifications } = await notificationService.Notifications(req);
+      const notificationClient = getNotificationClient(network.id);
+      if (!notificationClient) {
+        return [];
+      }
+
+      const { notifications } = await notificationClient.Notifications(req);
       return notifications;
     },
     { staleTime: Infinity }
