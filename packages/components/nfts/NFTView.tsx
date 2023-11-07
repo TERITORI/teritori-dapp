d8a2d5ef618d17a7d6ee08667ba734fe11529595
Norman Meier <norman@berty.tech>
2023-11-07 05:27:23 +0100

    chore: revert NFTView changes
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/components/nfts/NFTView.tsx
+++ packages/components/nfts/NFTView.tsx
@@ -19,12 +19,11 @@ import raffleSVG from "../../../assets/icons/raffle.svg";
 import sendSVG from "../../../assets/icons/send.svg";
 import { NFT } from "../../api/marketplace/v1/marketplace";
 import { useDropdowns } from "../../context/DropdownsProvider";
-import { useIsDAO } from "../../hooks/cosmwasm/useCosmWasmContractInfo";
 import { useIsMobile } from "../../hooks/useIsMobile";
 import { useMaxResolution } from "../../hooks/useMaxResolution";
 import { useNSUserInfo } from "../../hooks/useNSUserInfo";
 import useSelectedWallet from "../../hooks/useSelectedWallet";
-import { getNetwork, parseUserId } from "../../networks";
+import { getCosmosNetwork, parseUserId } from "../../networks";
 import {
   addSelected,
   removeSelected,
@@ -40,7 +39,6 @@ import {
   neutral77,
 } from "../../utils/style/colors";
 import { layout } from "../../utils/style/layout";
-import { nameServiceDefaultImage } from "../../utils/tns";
 import { BrandText } from "../BrandText";
 import { CurrencyIcon } from "../CurrencyIcon";
 import { DropdownOption } from "../DropdownOption";
@@ -64,8 +62,7 @@ export const NFTView: React.FC<{
   const flatStyle = StyleSheet.flatten(style);
   const selectedWallet = useSelectedWallet();
   const userInfo = useNSUserInfo(nft.ownerId);
-  const { isDAO } = useIsDAO(nft.ownerId);
-  const network = getNetwork(nft.networkId);
+  const cosmosNetwork = getCosmosNetwork(nft.networkId);
   const { onPressDropdownButton, isDropdownOpen, closeOpenedDropdown } =
     useDropdowns();
   const [isTransferNFTVisible, setIsTransferNFTVisible] =
@@ -163,7 +160,7 @@ export const NFTView: React.FC<{
                 >
                   <OptimizedImage
                     sourceURI={userInfo.metadata.image}
-                    fallbackURI={nameServiceDefaultImage(isDAO, network)}
+                    fallbackURI={cosmosNetwork?.nameServiceDefaultImage}
                     width={32}
                     height={32}
                     style={{
