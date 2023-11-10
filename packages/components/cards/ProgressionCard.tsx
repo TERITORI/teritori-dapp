52117e36107f8d0a320637bdd55badc46359129f
clegirar <clemntgirard@gmail.com>
2023-11-10 16:39:47 +0100

    fix: refracto ProgressionCard component
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- packages/components/cards/ProgressionCard.tsx
+++ packages/components/cards/ProgressionCard.tsx
@@ -1,23 +1,95 @@
 import React from "react";
 import { ViewStyle, View, StyleProp } from "react-native";
+import { useSelector } from "react-redux";
 
+import { selectIsLightTheme } from "../../store/slices/settings";
 import { neutral44, neutral77, primaryColor } from "../../utils/style/colors";
 import { fontSemibold12, fontSemibold28 } from "../../utils/style/fonts";
 import { BrandText } from "../BrandText";
 import { TertiaryBox } from "../boxes/TertiaryBox";
 import { GradientText } from "../gradientText";
 
-export const ProgressionCard: React.FC<{
-  style?: StyleProp<ViewStyle>;
+type ProgressionBaseProps = {
   label: string;
   valueCurrent: number;
   valueMax: number;
-}> = ({ style, label, valueCurrent, valueMax }) => {
-  const percent = Math.round((valueCurrent * 100) / valueMax);
+};
 
+const ProgressionBase: React.FC<ProgressionBaseProps> = ({
+  label,
+  valueCurrent,
+  valueMax,
+}) => {
+  const percent = Math.round((valueCurrent * 100) / valueMax);
+  const isLightTheme = useSelector(selectIsLightTheme);
+  return (
+    <View style={{ zIndex: 2, width: "100%" }}>
+      <BrandText style={[fontSemibold12, { marginBottom: 8 }]}>
+        {label}
+      </BrandText>
+
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "flex-end",
+          justifyContent: "space-between",
+          marginBottom: 10,
+        }}
+      >
+        <GradientText gradientType="blue" style={fontSemibold28}>
+          {percent}%
+        </GradientText>
+
+        <BrandText
+          style={[fontSemibold12, { color: neutral77, marginRight: 6 }]}
+        >
+          {valueCurrent.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}/
+          {valueMax.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </BrandText>
+      </View>
+
+      <View
+        style={{
+          borderRadius: 20,
+          height: 4,
+          backgroundColor: isLightTheme ? "#ECECEC" : neutral44,
+          width: "100%",
+        }}
+      >
+        <View
+          style={{
+            borderRadius: 20,
+            height: 4,
+            backgroundColor: isLightTheme ? "#3063D3" : primaryColor,
+            width: `${percent}%`,
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+type ProgressionProps = ProgressionBaseProps & {
+  style?: StyleProp<ViewStyle>;
+};
+
+export const ProgressionCardWithoutBox: React.FC<ProgressionProps> = (
+  props
+) => {
+  return (
+    <View style={props.style}>
+      <ProgressionBase {...props} />
+    </View>
+  );
+};
+
+export const ProgressionCard: React.FC<ProgressionProps> = (props) => {
   return (
     <TertiaryBox
-      style={style}
+      style={props.style}
       height={100}
       fullWidth
       mainContainerStyle={{
@@ -26,51 +98,7 @@ export const ProgressionCard: React.FC<{
         width: "100%",
       }}
     >
-      <View style={{ zIndex: 2, width: "100%" }}>
-        <BrandText style={[fontSemibold12, { marginBottom: 8 }]}>
-          {label}
-        </BrandText>
-
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "flex-end",
-            justifyContent: "space-between",
-            marginBottom: 10,
-          }}
-        >
-          <GradientText gradientType="blue" style={fontSemibold28}>
-            {percent}%
-          </GradientText>
-
-          <BrandText
-            style={[fontSemibold12, { color: neutral77, marginRight: 6 }]}
-          >
-            {valueCurrent}/{valueMax}
-          </BrandText>
-        </View>
-
-        <View
-          style={{
-            borderRadius: 20,
-            height: 4,
-            backgroundColor: neutral44,
-            width: "100%",
-          }}
-        >
-          <View
-            style={{
-              borderRadius: 20,
-              height: 4,
-              backgroundColor: primaryColor,
-              width: `${percent}%`,
-              position: "absolute",
-              top: 0,
-              left: 0,
-            }}
-          />
-        </View>
-      </View>
+      <ProgressionBase {...props} />
     </TertiaryBox>
   );
 };
