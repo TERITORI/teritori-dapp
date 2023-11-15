04b2aadc74e7ab8779561f9a710e8b4c4123510f
Norman Meier <norman@berty.tech>
2023-11-15 17:05:14 +0100

    chore: show my teritories instead of all daos
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/components/TopMenu/TopMenuMyTeritories.tsx
+++ packages/components/TopMenu/TopMenuMyTeritories.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Image, View } from "react-native";
+import { View } from "react-native";
 
 import { TopMenuSection } from "./TopMenuSection";
 import { DAO } from "../../api/dao/v1/dao";
 import { useDAOs } from "../../hooks/dao/useDAOs";
-import { useSelectedNetworkId } from "../../hooks/useSelectedNetwork";
-import { neutral77 } from "../../utils/style/colors";
-import { fontBold11, fontMedium10 } from "../../utils/style/fonts";
+import useSelectedWallet from "../../hooks/useSelectedWallet";
+import { fontBold11 } from "../../utils/style/fonts";
 import { layout, topMenuWidth } from "../../utils/style/layout";
 import { BrandText } from "../BrandText";
 import FlexCol from "../FlexCol";
@@ -14,6 +13,7 @@ import FlexRow from "../FlexRow";
 import { OmniLink } from "../OmniLink";
 import { TertiaryBox } from "../boxes/TertiaryBox";
 import { SmallCarousel } from "../carousels/SmallCarousel";
+import { UserAvatarWithFrame } from "../images/AvatarWithFrame";
 
 //TODO: Make SmallCarousel with items auto sized instead of fix widths
 const ORG_CARD_WIDTH = 164;
@@ -34,15 +34,11 @@ const OrgCard: React.FC<{ organization: DAO }> = ({ organization }) => {
         }}
       >
         <FlexRow alignItems="center">
-          <Image
-            source={{ uri: organization.imageUrl }}
+          <UserAvatarWithFrame
+            userId={organization.id}
+            size="XXS"
             style={{
-              borderRadius: 999,
               marginRight: layout.spacing_x1,
-              minHeight: 32,
-              minWidth: 32,
-              width: 32,
-              height: 32,
             }}
           />
           <FlexCol
@@ -50,19 +46,11 @@ const OrgCard: React.FC<{ organization: DAO }> = ({ organization }) => {
             width="auto"
             alignItems="flex-start"
           >
-            <BrandText style={[fontBold11]}>{organization.name}</BrandText>
             <BrandText
-              style={[
-                fontMedium10,
-                {
-                  color: neutral77,
-                  width: ORG_CARD_TAG_WIDTH,
-                },
-              ]}
-              ellipsizeMode="tail"
-              numberOfLines={1}
+              style={[fontBold11, { width: ORG_CARD_TAG_WIDTH }]}
+              numberOfLines={2}
             >
-              {organization.tokenName}
+              {organization.name}
             </BrandText>
           </FlexCol>
         </FlexRow>
@@ -72,11 +60,14 @@ const OrgCard: React.FC<{ organization: DAO }> = ({ organization }) => {
 };
 
 export const TopMenuMyTeritories: React.FC = () => {
-  const networkId = useSelectedNetworkId();
-
-  const { daos } = useDAOs({ networkId });
+  const selectedWallet = useSelectedWallet();
+  const { daos } = useDAOs({
+    networkId: selectedWallet?.networkId,
+    memberAddress: selectedWallet?.address,
+  });
+  if (!daos?.length) return null;
   return (
-    <TopMenuSection title="All DAOs" isCarousel>
+    <TopMenuSection title="My Teritories" isCarousel>
       {daos && (
         <SmallCarousel
           style={{
