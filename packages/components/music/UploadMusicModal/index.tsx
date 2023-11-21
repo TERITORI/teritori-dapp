4de410c8261bb6d18539655ff44a05e64aaf9309
Norman Meier <norman@berty.tech>
2023-11-21 17:54:02 +0100

    chore: remove dead code
    Signed-off-by: Norman Meier <norman@berty.tech>

--- packages/components/music/UploadMusicModal/index.tsx
+++ packages/components/music/UploadMusicModal/index.tsx
@@ -1,26 +1,20 @@
 import { FC, useState } from "react";
 
-import { UploadAlbum } from "./UploadAlbum";
 import { UploadTrack } from "./UploadTrack";
 import ModalBase from "../../modals/ModalBase";
 import { SpacerColumn } from "../../spacer";
-
-interface UploadAlbumModalProps {
-  onClose: () => void;
-  isVisible: boolean;
-}
 
 enum UploadMode {
   SINGLE_TRACK,
   ALBUM,
 }
 
-const UPLOAD_ALBUM_MODAL_WIDTH = 564;
+const UPLOAD_MUSIC_MODAL_WIDTH = 564;
 
-export const UploadMusicModal: FC<UploadAlbumModalProps> = ({
-  onClose,
-  isVisible,
-}) => {
+export const UploadMusicModal: FC<{
+  onClose: () => void;
+  isVisible: boolean;
+}> = ({ onClose, isVisible }) => {
   const [uploadMode] = useState(UploadMode.SINGLE_TRACK);
   return (
     <ModalBase
@@ -31,48 +25,14 @@ export const UploadMusicModal: FC<UploadAlbumModalProps> = ({
       }
       visible={isVisible}
       onClose={onClose}
-      width={UPLOAD_ALBUM_MODAL_WIDTH}
+      width={UPLOAD_MUSIC_MODAL_WIDTH}
     >
-      {/*TODO: Uncomment this after album stuff integration*/}
-      {/*<FlexRow justifyContent="flex-end">*/}
-      {/*  <TouchableOpacity*/}
-      {/*    style={[*/}
-      {/*      uploadModeButtonCStyle,*/}
-      {/*      uploadMode === UploadMode.SINGLE_TRACK && { opacity: 0.6 },*/}
-      {/*    ]}*/}
-      {/*    onPress={() => setUploadMode(UploadMode.SINGLE_TRACK)}*/}
-      {/*    disabled={uploadMode === UploadMode.SINGLE_TRACK}*/}
-      {/*  >*/}
-      {/*    <BrandText style={fontSemibold12}>Upload single track</BrandText>*/}
-      {/*  </TouchableOpacity>*/}
-      {/*  <SpacerRow size={1} />*/}
-      {/*  <TouchableOpacity*/}
-      {/*    style={[*/}
-      {/*      uploadModeButtonCStyle,*/}
-      {/*      uploadMode === UploadMode.ALBUM && { opacity: 0.6 },*/}
-      {/*    ]}*/}
-      {/*    onPress={() => setUploadMode(UploadMode.ALBUM)}*/}
-      {/*    disabled={uploadMode === UploadMode.ALBUM}*/}
-      {/*  >*/}
-      {/*    <BrandText style={fontSemibold12}>Upload album</BrandText>*/}
-      {/*  </TouchableOpacity>*/}
-      {/*</FlexRow>*/}
-
       <SpacerColumn size={2} />
       {uploadMode === UploadMode.SINGLE_TRACK ? (
         <UploadTrack onUploadDone={onClose} />
       ) : (
-        <UploadAlbum onUploadDone={onClose} />
+        <>{/*TODO*/}</>
       )}
     </ModalBase>
   );
 };
-
-// const uploadModeButtonCStyle: ViewStyle = {
-//   height: 24,
-//   alignItems: "center",
-//   justifyContent: "center",
-//   paddingHorizontal: layout.spacing_x1,
-//   borderRadius: 6,
-//   backgroundColor: neutral30,
-// };
