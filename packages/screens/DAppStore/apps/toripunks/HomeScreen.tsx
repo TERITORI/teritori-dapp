dc8674f91c6b6239de3e1d9ebbbda577a0874ea3
Norman Meier <norman@samourai.coop>
2023-11-25 19:43:31 +0100

    fix: don't call setState in render function
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- packages/screens/DAppStore/apps/toripunks/HomeScreen.tsx
+++ packages/screens/DAppStore/apps/toripunks/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { Content } from "./content/Content";
 import { ContentContextProvider } from "./context/ContentProvider";
@@ -9,9 +9,11 @@ import { ScreenFC, useAppNavigation } from "../../../../utils/navigation";
 export const ToriPunks: ScreenFC<"ToriPunks"> = ({ route }) => {
   const navigation = useAppNavigation();
   const screen = route.params ? route.params.route : "welcome";
-  if (!route.params) {
-    navigation.navigate("ToriPunks", { route: screen });
-  }
+  useEffect(() => {
+    if (!route.params) {
+      navigation.navigate("ToriPunks", { route: screen });
+    }
+  }, [navigation, route.params, screen]);
   return (
     <ScreenContainer
       fullWidth
