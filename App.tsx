b954d733c46671d80822d01f465bb02b192545d9
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-11-30 21:32:00 +0545

    fix: depcheck

--- App.tsx
+++ App.tsx
@@ -1,7 +1,3 @@
-import Constants from "expo-constants";
-
 import Root from "./Root";
-Object.entries(Constants.expoConfig?.extra?.env).forEach(([key, value]) => {
-  process.env[key] = `${value}`;
-});
+
 export default Root;
