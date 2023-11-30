0c35ecc19fcede3cfc4b7bdeb7530169d5335a14
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-11-30 21:10:20 +0545

    feat:app build

--- App.tsx
+++ App.tsx
@@ -1,3 +1,7 @@
-import Root from "./Root";
+import Constants from "expo-constants";
 
+import Root from "./Root";
+Object.entries(Constants.expoConfig?.extra?.env).forEach(([key, value]) => {
+  process.env[key] = `${value}`;
+});
 export default Root;
