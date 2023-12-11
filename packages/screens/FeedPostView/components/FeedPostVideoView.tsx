d3477612ab7a6fe51d21a2d851d461061e3f8554
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-12-11 15:55:11 +0545

    fix: social feed view crash

--- packages/screens/FeedPostView/components/FeedPostVideoView.tsx
+++ packages/screens/FeedPostView/components/FeedPostVideoView.tsx
@@ -5,6 +5,7 @@ import Animated, {
   useAnimatedRef,
   useAnimatedScrollHandler,
   useSharedValue,
+  runOnJS,
 } from "react-native-reanimated";
 
 import { VideoComment } from "./VideoComment";
@@ -220,7 +221,7 @@ export const FeedPostVideoView: FC<{
         } else if (flatListContentOffsetY < event.contentOffset.y) {
           isGoingUp.value = false;
         }
-        setFlatListContentOffsetY(event.contentOffset.y);
+        runOnJS(setFlatListContentOffsetY)(event.contentOffset.y);
       },
     },
     [post?.identifier],
