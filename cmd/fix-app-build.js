884b9093221f450923ca91a64a24734424bc79a6
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-12-21 12:58:02 +0545

    fix: cmd

--- cmd/fix-app-build.js
+++ cmd/fix-app-build.js
@@ -2,25 +2,20 @@ const fs = require("fs");
 
 const filePath = "./.gitignore";
 
-// Read the package.json file
 fs.readFile(filePath, "utf8", (err, data) => {
   if (err) {
     console.error("Error reading gitignore", err);
     return;
   }
 
-  // Define the regular expression to match the line you want to remove
-
-  // Remove the matched line using replace
   let updatedData = data.replace("/weshd/ios/Frameworks/", "");
-  updatedData = data.replace("/weshd/android/libs/", "");
+  updatedData = updatedData.replace("/weshd/android/libs/", "");
 
-  // Write the updated content back to package.json
   fs.writeFile(filePath, updatedData, "utf8", (err) => {
     if (err) {
       console.error("Error writing to package.json:", err);
     } else {
-      console.log("Package.json updated successfully!");
+      console.log("gitignore updated successfully!");
     }
   });
 });
