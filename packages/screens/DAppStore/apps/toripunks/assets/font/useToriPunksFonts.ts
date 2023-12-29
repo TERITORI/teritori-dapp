8ef93bc236d5af8d0b88b98d70898a0c80489406
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2023-12-29 09:19:32 +0545

    fix: tori punk font fix

--- packages/screens/DAppStore/apps/toripunks/assets/font/useToriPunksFonts.ts
+++ packages/screens/DAppStore/apps/toripunks/assets/font/useToriPunksFonts.ts
@@ -3,6 +3,6 @@ import { useFonts } from "expo-font";
 export const useToriPunksFonts = () => {
   return useFonts({
     "Bebas Neue": require("./Bebas_Neue/BebasNeue-Regular.ttf"),
-    "Dafter Harder Better Stronger": require("./Dafter_Harder_Better_Stronger/Dafter Harder Better Stronger.ttf"),
+    "Dafter Harder Better Stronger": require("./Dafter_Harder_Better_Stronger/Dafter-Harder-Better-Stronger.ttf"),
   });
 };
