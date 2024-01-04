92e6a2b1bdfeabae0c8a47b34bc88eefbdab8ba0
Sachin <sachin@intosoft.com>
2024-01-04 12:18:01 +0545

    added mini profile screen

---
+++ packages/screens/Mini/DAppStore/DAppStoreScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { ScreenFC } from "../../../utils/navigation";
+
+export const DAppStoreScreen: ScreenFC<"MiniDAppStore"> = (props) => {
+  return (
+    <View>
+      <Text>DAppStoreScreen</Text>
+    </View>
+  );
+};
