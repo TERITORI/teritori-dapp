a12a88accdf8ed120e3a42daeaaba3580d57265f
n0izn0iz <n0izn0iz@users.noreply.github.com>
2024-01-05 19:04:40 +0100

    feat: testnet 5 (#850)
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- packages/networks/teritori-testnet/currencies.ts
+++ packages/networks/teritori-testnet/currencies.ts
@@ -11,27 +11,4 @@ export const teritoriTestnetCurrencies: CurrencyInfo[] = [
     kind: "native",
     color: currencyTORIcolor,
   },
-  {
-    kind: "ibc",
-    denom:
-      "ibc/C9300DDD93DF3A3A668CAB02398A0AA081EF89EC005B2DB68832E363BAAABF85",
-    sourceNetwork: "cosmos-hub-theta",
-    sourceDenom: "uatom",
-    sourceChannelPort: "transfer",
-    sourceChannelId: "channel-685",
-    destinationChannelPort: "transfer",
-    destinationChannelId: "channel-30",
-    deprecated: true,
-  },
-  {
-    kind: "ibc",
-    denom:
-      "ibc/8D9734B53D56DC57A92E4CC788547699853F411190F6DAA70FA12B9BD062F7AE",
-    sourceNetwork: "cosmos-hub-theta",
-    sourceDenom: "uatom",
-    sourceChannelPort: "transfer",
-    sourceChannelId: "channel-701",
-    destinationChannelPort: "transfer",
-    destinationChannelId: "channel-33",
-  },
 ];
