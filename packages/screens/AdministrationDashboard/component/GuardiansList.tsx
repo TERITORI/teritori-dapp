a0394def104ed0fe82be74539e12bb63e987c2a8
Vishal Rakholiya <vishal.rakholiya@iviewlabs.com>
2024-01-11 16:59:33 +0530

    style: tooltip on Guardians

---
+++ packages/screens/AdministrationDashboard/component/GuardiansList.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Image, TouchableOpacity, ViewStyle } from "react-native";
+import Popover from "react-native-popover-view";
+
+import { TooltipContant } from "./TooltipContant";
+import avaPNG from "../../../../assets/default-images/ava.png";
+import checkBadgeSVG from "../../../../assets/icons/check-badge.svg";
+import { BrandText } from "../../../components/BrandText";
+import { SVG } from "../../../components/SVG";
+import { neutral44, neutral11 } from "../../../utils/style/colors";
+import { fontSemibold16 } from "../../../utils/style/fonts";
+import { layout } from "../../../utils/style/layout";
+
+export const GuardiansList = ({ index }: { index: number }) => {
+  const [viewPopover, setViewPopover] = useState<number>(-1);
+
+  const setViewPopoverHandler = (index: number) => {
+    setViewPopover((old) => (old === index ? -1 : index));
+  };
+
+  return (
+    <>
+      <Popover
+        isVisible={viewPopover === index + 1}
+        from={
+          <TouchableOpacity
+            onPress={() => setViewPopoverHandler(index + 1)}
+            style={listToggle}
+          >
+            <Image
+              style={{
+                width: 28,
+                height: 28,
+              }}
+              source={avaPNG}
+            />
+            <BrandText
+              style={[fontSemibold16, { marginLeft: 15, marginRight: 10 }]}
+            >
+              Meebits
+            </BrandText>
+            <SVG source={checkBadgeSVG} />
+          </TouchableOpacity>
+        }
+        onRequestClose={() => setViewPopover(-1)}
+        popoverStyle={popoverStyle}
+        backgroundStyle={{ opacity: 0 }}
+      >
+        <TooltipContant />
+      </Popover>
+    </>
+  );
+};
+
+const listToggle: ViewStyle = {
+  flexDirection: "row",
+  alignItems: "center",
+  marginVertical: 5,
+};
+
+const popoverStyle = {
+  backgroundColor: neutral11,
+  width: 425,
+  padding: layout.spacing_x2,
+  borderWidth: 2,
+  borderColor: neutral44,
+  borderRadius: 12,
+};
