b5d7922659aaaccec57128c88b9480d8737f10ef
WaDadidou <50441633+WaDadidou@users.noreply.github.com>
2024-01-15 18:30:29 +0100

    feat: feed post/upload loading bar (#845)

---
+++ packages/hooks/useIpfs.ts
@@ -0,0 +1,132 @@
+import axios from "axios";
+import { omit } from "lodash";
+import { CID } from "multiformats";
+import { useState } from "react";
+
+import { LocalFileData, RemoteFileData } from "../utils/types/files";
+
+interface PinataFileProps {
+  file: LocalFileData;
+  pinataJWTKey: string;
+}
+
+interface UploadPostFilesToPinataParams {
+  files: LocalFileData[];
+  pinataJWTKey: string;
+}
+
+interface IPFSUploadProgress {
+  fileUrl: string;
+  progress: number; // 0 to 1
+}
+
+export const useIpfs = () => {
+  const [ipfsUploadProgresses, setIpfsUploadProgresses] = useState<
+    IPFSUploadProgress[]
+  >([]);
+
+  const pinataPinFileToIPFS = async ({
+    file,
+    pinataJWTKey,
+  }: PinataFileProps): Promise<string | undefined> => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file.file);
+
+      const responseFile = await axios({
+        onUploadProgress: (progressEvent) => {
+          // Map the different progresses for each file to prevent a bad finalProgress
+          setIpfsUploadProgresses((ipfsUploadProgresses) =>
+            ipfsUploadProgresses.map((progressItem) => {
+              if (progressItem.fileUrl === file.url) {
+                return {
+                  ...progressItem,
+                  progress: progressEvent.progress || 0,
+                };
+              } else {
+                return progressItem;
+              }
+            }),
+          );
+        },
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data: formData,
+        headers: {
+          Authorization: "Bearer " + pinataJWTKey,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      return responseFile.data.IpfsHash;
+    } catch (err) {
+      console.error("Error pinning " + file.fileName + " to IPFS", err);
+    }
+  };
+
+  const uploadFilesToPinata = async ({
+    files,
+    pinataJWTKey,
+  }: UploadPostFilesToPinataParams): Promise<RemoteFileData[]> => {
+    setIpfsUploadProgresses([]);
+
+    const storedFile = async (file: LocalFileData): Promise<RemoteFileData> => {
+      const fileIpfsHash = await pinataPinFileToIPFS({
+        file,
+        pinataJWTKey,
+      });
+      const url = !fileIpfsHash
+        ? ""
+        : "ipfs://" + CID.parse(fileIpfsHash).toV1().toString();
+
+      if (file.thumbnailFileData) {
+        const thumbnailFileIpfsHash = await pinataPinFileToIPFS({
+          file: file.thumbnailFileData,
+          pinataJWTKey,
+        });
+        const thumbnailUrl = !thumbnailFileIpfsHash
+          ? ""
+          : "ipfs://" + CID.parse(thumbnailFileIpfsHash).toV1().toString();
+
+        return {
+          ...omit(file, "file"),
+          url,
+          thumbnailFileData: {
+            ...omit(file.thumbnailFileData, "file"),
+            url: thumbnailUrl,
+          },
+        };
+      } else {
+        return {
+          ...omit(file, "file"),
+          // Thumbnails cannot have a thumbnail, so we set isThumbnailImage here
+          isThumbnailImage: file.isThumbnailImage,
+          url,
+        };
+      }
+    };
+
+    const queries = [];
+    for (const file of files) {
+      setIpfsUploadProgresses((ipfsUploadProgresses) => [
+        ...ipfsUploadProgresses,
+        {
+          fileUrl: file.url,
+          progress: 0,
+        },
+      ]);
+
+      const storedFileQuery = storedFile(file);
+      queries.push(storedFileQuery);
+    }
+    return await Promise.all(queries);
+  };
+
+  const finalProgress = !ipfsUploadProgresses.length
+    ? 0
+    : ipfsUploadProgresses.reduce(
+        (acc, progressItem) => acc + progressItem.progress,
+        0,
+      ) / ipfsUploadProgresses.length;
+
+  return { uploadFilesToPinata, ipfsUploadProgress: finalProgress };
+};
