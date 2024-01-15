2244b4caf9858f8f3bb4da7f1167aa94d9a4b5c1
WaDadidou <50441633+WaDadidou@users.noreply.github.com>
2024-01-16 00:12:55 +0100

    feat: NFT Marketplace - Period filter (#860)
    * feat: add a period filter (only front)
    
    * feat(marketplace-period-filter): Add period filter to marketplace screen
    
    Add a period filter to the marketplace screen in order to allow users to filter collections based on a specific time period. The period filter will be implemented using a dropdown menu with options for different time periods, such as "Last 10 minutes", "Last 1 hour", "Last 6 hours", etc. The selected time period will be used to retrieve collections that were created within that period.
    
    Changes made in this commit:
    - Added a new optional field "period_in_minutes" to the "GetCollectionsRequest" message in the "marketplace.proto" file.
    - Added a new state variable "timePeriod" to the "UIStates" interface in the "marketplaceFilters.ts" file.
    - Added a new array "marketplacePeriodItems" to define the available time period options in the "marketplaceFilters.ts" file.
    - Added a new action "setTimePeriod" to update the selected time period in the
    
    * fix: remove console.log
    
    ---------
    
    Co-authored-by: Eng. Juan Combetto <omniwired@gmail.com>

---
+++ packages/screens/Marketplace/PeriodFilter.tsx
@@ -0,0 +1,100 @@
+import React, { FC, Fragment, useRef, useState } from "react";
+import { TouchableOpacity, useWindowDimensions, View } from "react-native";
+import { useSelector } from "react-redux";
+
+import chevronDownSVG from "../../../assets/icons/chevron-down.svg";
+import chevronUpSVG from "../../../assets/icons/chevron-up.svg";
+import sortSVG from "../../../assets/icons/sort.svg";
+import { BrandText } from "../../components/BrandText";
+import { SVG } from "../../components/SVG";
+import { SpacerColumn, SpacerRow } from "../../components/spacer";
+import { useDropdowns } from "../../context/DropdownsProvider";
+import {
+  marketplacePeriodItems,
+  PeriodItem,
+  selectTimePeriod,
+  setTimePeriod,
+} from "../../store/slices/marketplaceFilters";
+import { useAppDispatch } from "../../store/store";
+import { neutral33, secondaryColor } from "../../utils/style/colors";
+import { fontSemibold14 } from "../../utils/style/fonts";
+import { layout, RESPONSIVE_BREAKPOINT_S } from "../../utils/style/layout";
+
+export const PeriodFilter: FC = () => {
+  const { width } = useWindowDimensions();
+  const { onPressDropdownButton, isDropdownOpen, closeOpenedDropdown } =
+    useDropdowns();
+  const dropdownRef = useRef<View>(null);
+  const timePeriod = useSelector(selectTimePeriod);
+  const [selectedItem, setSelectedItem] = useState<PeriodItem>(timePeriod);
+  const dispatch = useAppDispatch();
+
+  const onPressPeriodItem = (periodItem: PeriodItem) => {
+    setSelectedItem(periodItem);
+    dispatch(setTimePeriod(periodItem));
+    closeOpenedDropdown();
+  };
+
+  return (
+    <View ref={dropdownRef}>
+      <TouchableOpacity
+        onPress={() => onPressDropdownButton(dropdownRef)}
+        style={{
+          height: 40,
+          paddingLeft: layout.spacing_x1,
+          paddingRight: layout.spacing_x1_5,
+          borderRadius: 8,
+          flexDirection: "row",
+          alignItems: "center",
+          backgroundColor: neutral33,
+        }}
+      >
+        <SVG source={sortSVG} width={16} height={16} />
+        <SpacerRow size={1} />
+
+        <BrandText style={fontSemibold14}>
+          {width < RESPONSIVE_BREAKPOINT_S
+            ? selectedItem.shortLabel
+            : selectedItem.label}
+        </BrandText>
+        <SpacerRow size={1} />
+
+        <SVG
+          source={isDropdownOpen(dropdownRef) ? chevronUpSVG : chevronDownSVG}
+          width={16}
+          height={16}
+          color={secondaryColor}
+        />
+      </TouchableOpacity>
+
+      {isDropdownOpen(dropdownRef) && (
+        <View
+          style={{
+            position: "absolute",
+            top: 44,
+            backgroundColor: neutral33,
+            paddingHorizontal: layout.spacing_x1_5,
+            paddingTop: layout.spacing_x1_5,
+            borderRadius: 8,
+            width: "100%",
+          }}
+        >
+          {marketplacePeriodItems
+            .filter((periodItem) => selectedItem !== periodItem)
+            .map((periodItem, index) => (
+              <Fragment key={index}>
+                <TouchableOpacity onPress={() => onPressPeriodItem(periodItem)}>
+                  <BrandText style={fontSemibold14}>
+                    {width < RESPONSIVE_BREAKPOINT_S
+                      ? periodItem.shortLabel
+                      : periodItem.label}
+                  </BrandText>
+                </TouchableOpacity>
+                <SpacerColumn size={1.5} />
+              </Fragment>
+            ))}
+        </View>
+      )}
+    </View>
+  );
+};
