63e2921599a3c2b3138a20b02c78ae030852c563
Sujal Lama <sujal@intosoft.com>
2024-01-19 17:04:32 +0545

    Modal and table component created

--- packages/screens/Mini/Wallet/SendingToriScreen.tsx
+++ packages/screens/Mini/Wallet/SendingToriScreen.tsx
@@ -1,19 +1,77 @@
+import { useState } from "react";
 import { View } from "react-native";
 
 import chevronDownSVG from "../../../../assets/icons/chevron-down-white.svg";
+import foxCircleSVG from "../../../../assets/icons/networks/fox-circle.svg";
+import teritoriSVG from "../../../../assets/icons/networks/teritori.svg";
 import teritoriCircleSVG from "../../../../assets/icons/tori-circle.svg";
+import { BrandText } from "../../../components/BrandText";
 import { Dropdown } from "../../../components/Dropdown";
 import { SVG } from "../../../components/SVG";
-import { SpacerColumn } from "../../../components/spacer";
-import { ScreenFC } from "../../../utils/navigation";
-import { secondaryColor } from "../../../utils/style/colors";
+import { TertiaryBadge } from "../../../components/badges/TertiaryBadge";
+import { SpacerColumn, SpacerRow } from "../../../components/spacer";
+import { ScreenFC, useAppNavigation } from "../../../utils/navigation";
+import {
+  neutral33,
+  neutralA3,
+  secondaryColor,
+  withAlpha,
+} from "../../../utils/style/colors";
 import { fontMedium16 } from "../../../utils/style/fonts";
 import { layout } from "../../../utils/style/layout";
 import { BlurScreenContainer } from "../components/BlurScreenContainer";
+import CircularImgOrIcon from "../components/CircularImgOrIcon";
 import { CustomButton } from "../components/CustomButton";
+import MobileModal from "../components/MobileModal";
 import RowDisplay from "../components/RowDisplay";
+import MiniTable from "../components/Table/MiniTable";
+import TitleBar from "../components/TitleBar";
+
+const toriData = [
+  {
+    label: "Token",
+    value: "0x89383938...A3b2",
+    icon: "link",
+    onPress: () => alert("Token"),
+  },
+  {
+    label: "Amount",
+    value: "8187278373838393837373",
+  },
+  {
+    label: "Expiration",
+    value: "157843252",
+  },
+  {
+    label: "Nonce",
+    value: "0",
+  },
+  {
+    label: "Network",
+    value: <TertiaryBadge iconSVG={teritoriSVG} label="Teritori" />,
+  },
+  {
+    label: (
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <CircularImgOrIcon
+          size={32}
+          enableFullIcon
+          style={{ alignItems: "center", justifyContent: "center" }}
+          icon={require("../../../../assets/default-images/profile.png")}
+        />
+        <SpacerRow size={1} />
+        <BrandText style={[fontMedium16, { color: neutralA3 }]}>
+          ninja.tori
+        </BrandText>
+      </View>
+    ),
+    value: "GxF34...3A31",
+  },
+];
 
 const SendingToriScreen: ScreenFC<"MiniSendingTori"> = ({ navigation }) => {
+  const [openModal, setOpenModal] = useState(false);
+
   const goBackTo = () =>
     navigation.replace("MiniSendTori", { back: "MiniSendingTori" });
 
@@ -59,10 +117,62 @@ const SendingToriScreen: ScreenFC<"MiniSendingTori"> = ({ navigation }) => {
             </View>
           </Dropdown>
         </View>
-        <CustomButton title="Send" onPress={() => navigation.goBack()} />
+
+        <SendingModal visible={openModal} onClose={() => setOpenModal(false)} />
+
+        <CustomButton title="Send" onPress={() => setOpenModal(true)} />
       </View>
     </BlurScreenContainer>
   );
 };
 
 export default SendingToriScreen;
+
+type SendingModalProps = {
+  visible: boolean;
+  onClose: () => void;
+};
+
+function SendingModal({ visible, onClose }: SendingModalProps) {
+  const navigation = useAppNavigation();
+
+  return (
+    <MobileModal visible={visible} onClose={onClose}>
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: layout.spacing_x2,
+          paddingVertical: layout.spacing_x3,
+          justifyContent: "space-between",
+        }}
+      >
+        <View>
+          <TitleBar
+            title="Signature request"
+            icon={foxCircleSVG}
+            subTitle="Be careful this message may transfer assets"
+          />
+          <SpacerColumn size={3} />
+          <MiniTable
+            items={toriData}
+            colorOptions={{ tableColor: withAlpha(neutral33, 0.8) }}
+          />
+        </View>
+
+        <View style={{ flexDirection: "row", gap: 10 }}>
+          <CustomButton
+            type="gray"
+            title="Cancel"
+            onPress={() => navigation.navigate("MiniTabs")}
+            style={{ flex: 1 }}
+          />
+          <CustomButton
+            title="Sign"
+            onPress={() => navigation.navigate("MiniTabs")}
+            style={{ flex: 1 }}
+          />
+        </View>
+      </View>
+    </MobileModal>
+  );
+}
---
+++ packages/screens/Mini/components/Table/MiniTableRow.tsx
@@ -0,0 +1,55 @@
+import React, { ReactNode } from "react";
+import { StyleProp, TextStyle, View, ViewStyle } from "react-native";
+
+import { BrandText } from "../../../../components/BrandText";
+import { neutral22, neutralA3 } from "../../../../utils/style/colors";
+import { fontBold16, fontMedium16 } from "../../../../utils/style/fonts";
+import { layout } from "../../../../utils/style/layout";
+
+type MiniTableRowProps = {
+  leftLabel?: string | ReactNode;
+  rightLabel?: string | ReactNode;
+  style?: StyleProp<ViewStyle>;
+  rightLabelStyle?: StyleProp<TextStyle>;
+  leftLabelStyle?: StyleProp<TextStyle>;
+};
+
+export default function MiniTableRow({
+  leftLabel,
+  rightLabel,
+  style,
+  leftLabelStyle,
+  rightLabelStyle,
+}: MiniTableRowProps) {
+  return (
+    <View
+      style={[
+        {
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: layout.spacing_x2,
+          borderRadius: 14,
+          backgroundColor: neutral22,
+        },
+        style,
+      ]}
+    >
+      {typeof leftLabel === "string" ? (
+        <BrandText style={[fontMedium16, { color: neutralA3 }, leftLabelStyle]}>
+          {leftLabel}
+        </BrandText>
+      ) : (
+        leftLabel
+      )}
+
+      {typeof rightLabel === "string" ? (
+        <BrandText style={[fontBold16, rightLabelStyle]}>
+          {rightLabel}
+        </BrandText>
+      ) : (
+        rightLabel
+      )}
+    </View>
+  );
+}
