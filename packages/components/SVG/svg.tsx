aa5ca47ee4e62d37a17c8ee5d22f9dc9437f2df0
Norman Meier <norman@samourai.coop>
2024-01-23 12:38:08 +0100

    fix: remove space text
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- packages/components/SVG/svg.tsx
+++ packages/components/SVG/svg.tsx
@@ -32,7 +32,6 @@ export const SVG: React.FC<
           width: svgProps.width || "auto",
         }}
       >
-        {" "}
         <SvgFromUri uri={source} {...svgProps} />
       </View>
     );
