9eac5787291e9fc796e4c0d7cc8c0204c8aaecee
Norman Meier <norman@samourai.coop>
2024-01-24 16:42:50 +0100

    chore: upgrade to expo 50
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ const { WebpackDeduplicationPlugin } = require("webpack-deduplication-plugin");
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // overrind lottie on web due to https://github.com/expo/expo/issues/6054
-  config.resolve.alias["lottie-react-native"] = "react-native-web-lottie";
-
   if (process.env.isElectron) {
     config.target = "electron-renderer";
     config.output.publicPath = "./";
