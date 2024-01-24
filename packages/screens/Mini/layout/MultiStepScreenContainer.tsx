c4230b58b5b7dac4a5b2ef55d23b7ffeaac8582e
Sujal Lama <sujal@intosoft.com>
2024-01-24 17:42:48 +0545

    success screen in wallet

--- packages/screens/Mini/layout/MultiStepScreenContainer.tsx
+++ packages/screens/Mini/layout/MultiStepScreenContainer.tsx
@@ -1,19 +1,29 @@
 import { PropsWithChildren } from "react";
 import { View, SafeAreaView } from "react-native";
 
+import chevronLeftSVG from "../../../../assets/icons/chevron-left.svg";
 import teritoriSVG from "../../../../assets/icons/teritori-white.svg";
 import { SVG } from "../../../components/SVG";
+import { CustomPressable } from "../../../components/buttons/CustomPressable";
 import { SpacerColumn } from "../../../components/spacer";
+import { useAppNavigation } from "../../../utils/navigation";
+import { layout } from "../../../utils/style/layout";
 import { ProgressLine2 } from "../components/ProgressLine2";
 
 interface MultiStepScreenProps extends PropsWithChildren {
   screenPercentage: number;
+  enableBack?: boolean;
 }
 
 export default function MultiStepScreenContainer({
   screenPercentage,
+  enableBack = false,
   children,
 }: MultiStepScreenProps) {
+  const navigation = useAppNavigation();
+
+  const navigateBack = () => navigation.canGoBack() && navigation.goBack();
+
   return (
     <SafeAreaView
       style={{
@@ -23,12 +33,23 @@ export default function MultiStepScreenContainer({
       }}
     >
       <View>
-        <SVG
-          source={teritoriSVG}
-          height={40}
-          width={40}
-          style={{ alignSelf: "center" }}
-        />
+        {enableBack ? (
+          <View style={{ paddingHorizontal: layout.spacing_x2 }}>
+            <CustomPressable
+              onPress={navigateBack}
+              style={{ height: 24, width: 24 }}
+            >
+              <SVG source={chevronLeftSVG} height={24} width={24} />
+            </CustomPressable>
+          </View>
+        ) : (
+          <SVG
+            source={teritoriSVG}
+            height={40}
+            width={40}
+            style={{ alignSelf: "center" }}
+          />
+        )}
         <SpacerColumn size={2} />
         <ProgressLine2 percent={screenPercentage} />
       </View>
--- packages/screens/Wallet/Screens/SucessScreen.tsx
+++ packages/screens/Wallet/Screens/SucessScreen.tsx
@@ -1,38 +1,13 @@
 import React from "react";
-import { View } from "react-native";
 
-import { BrandText } from "../../../components/BrandText";
 import { ScreenFC } from "../../../utils/navigation";
-import { neutral77 } from "../../../utils/style/colors";
-import { fontSemibold16, fontSemibold28 } from "../../../utils/style/fonts";
-import { layout } from "../../../utils/style/layout";
-import { Button } from "../components/Button";
-import { TopBarWithProgress } from "../layout/TopBarWithProgress";
-import { WalletContainer } from "../layout/WalletContainer";
+import { AllSet } from "../../Mini/ConnectLedger/components/AllSet";
+import MultiStepScreenContainer from "../../Mini/layout/MultiStepScreenContainer";
 
-export const SuccessScreen: ScreenFC<"SuccessScreen"> = () => {
+export const SuccessScreen: ScreenFC<"SuccessScreen"> = ({ navigation }) => {
   return (
-    <WalletContainer>
-      <TopBarWithProgress progress={100} />
-      <View
-        style={{
-          width: "100%",
-          flex: 7,
-          marginTop: layout.spacing_x3,
-          alignItems: "flex-start",
-        }}
-      >
-        <BrandText style={[fontSemibold28]}>You're All Set!</BrandText>
-        <BrandText
-          style={[
-            fontSemibold16,
-            { color: neutral77, marginTop: layout.spacing_x1 },
-          ]}
-        >
-          Click on the Start button to launch Teritori.
-        </BrandText>
-      </View>
-      <Button text="Start" navigateTo="MiniTabs" />
-    </WalletContainer>
+    <MultiStepScreenContainer screenPercentage={100}>
+      <AllSet onComplete={() => navigation.navigate("MiniTabs")} />
+    </MultiStepScreenContainer>
   );
 };
