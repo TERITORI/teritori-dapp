a57f3d2c985e6cb45f4f491dd9a9f258fcdeb687
Eng. Juan Combetto <omniwired@gmail.com>
2024-01-24 17:28:29 +0900

    feat(wallet): rename useNativeWallet into getNativeWallet

--- packages/screens/Wallet/hooks/useNativeSigner.ts
+++ packages/screens/Wallet/hooks/useNativeSigner.ts
@@ -1,16 +1,13 @@
 import { SigningStargateClient } from "@cosmjs/stargate";
 
-import { useNativeWallet } from "./useNativeWallet";
+import { getNativeWallet } from "./getNativeWallet";
 import { mustGetCosmosNetwork } from "../../../networks";
-import { addSelected } from "../../../store/slices/wallets";
-import { useAppDispatch } from "../../../store/store";
 
 export const useNativeSigner = async () => {
-  const dispatch = useAppDispatch();
   const networkId = "teritori";
   const network = mustGetCosmosNetwork(networkId);
 
-  const wallet = await useNativeWallet();
+  const wallet = await getNativeWallet();
   const rpcEndpoint = network.rpcEndpoint;
   const client = await SigningStargateClient.connectWithSigner(
     rpcEndpoint,
@@ -19,12 +16,6 @@ export const useNativeSigner = async () => {
   if (!client) {
     throw new Error("Native Wallet not ready");
   }
-  const [firstAccount] = await wallet.getAccounts();
-  dispatch(
-    addSelected({
-      publicKey: firstAccount.address,
-      network: network.kind,
-    }),
-  );
+
   return client;
 };
