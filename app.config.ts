d6d877437f0e7ca7d913a49fe5c9c148199fe2c6
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2024-01-30 16:11:50 +0545

    fix:rebase package.json issue

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,11 @@
-import { ExpoConfig } from "expo/config";
-
-const config: { expo: ExpoConfig } = {
+const config = {
   expo: {
-    name: "teritori-dapp",
-    slug: "teritori-dapp",
+    name: "Teritori",
+    slug: "teritori",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
+    owner: "teritori",
     userInterfaceStyle: "light",
     splash: {
       image: "./assets/splash.png",
@@ -19,12 +18,14 @@ const config: { expo: ExpoConfig } = {
     assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
+      bundleIdentifier: "com.teritori",
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/icon.png",
         backgroundColor: "#FFFFFF",
       },
+      package: "com.teritori",
     },
     web: {
       favicon: "./assets/favicon.png",
@@ -32,7 +33,11 @@ const config: { expo: ExpoConfig } = {
     },
     extra: {
       env: process.env,
+      eas: {
+        projectId: "9ce165de-0199-478c-b3bd-8688e5ce03eb",
+      },
     },
+    plugins: ["expo-font", "expo-secure-store"],
   },
 };
 
