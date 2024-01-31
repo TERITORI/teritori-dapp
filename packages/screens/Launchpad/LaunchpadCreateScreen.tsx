d467953c615cb0f45fa88a0e06168b21a7c6aec7
ChiragPansuriya-iView <134677881+ChiragPansuriya-iView@users.noreply.github.com>
2024-01-31 17:07:26 +0530

    fix: create a component for launchpad steper

--- packages/screens/Launchpad/LaunchpadCreateScreen.tsx
+++ packages/screens/Launchpad/LaunchpadCreateScreen.tsx
@@ -1,31 +1,21 @@
 import React, { useState } from "react";
-import { TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 
 import { LaunchpadAdditional } from "./components/LaunchpadAdditional";
 import { LaunchpadAssetsandMetadata } from "./components/LaunchpadAssetsandMetadata";
 import { LaunchpadBasic } from "./components/LaunchpadBasic";
 import { LaunchpadDetails } from "./components/LaunchpadDetails";
 import { LaunchpadMinting } from "./components/LaunchpadMinting";
+import { LaunchpadSteper } from "./components/LaunchpadSteper";
 import { LaunchpadTeamandInvestment } from "./components/LaunchpadTeamandInvestment";
-import ChevronRightSvg from "../../../assets/icons/chevron-right.svg";
 import { BrandText } from "../../components/BrandText";
-import { SVG } from "../../components/SVG";
 import { ScreenContainer } from "../../components/ScreenContainer";
-import { PrimaryBox } from "../../components/boxes/PrimaryBox";
 import { PrimaryButton } from "../../components/buttons/PrimaryButton";
 import { SecondaryButton } from "../../components/buttons/SecondaryButton";
 import { SpacerColumn } from "../../components/spacer";
 import { NetworkFeature } from "../../networks";
 import { ScreenFC, useAppNavigation } from "../../utils/navigation";
-import {
-  neutral17,
-  neutral22,
-  neutral33,
-  neutral77,
-  primaryColor,
-  primaryTextColor,
-} from "../../utils/style/colors";
-import { fontSemibold14 } from "../../utils/style/fonts";
+import { neutral33 } from "../../utils/style/colors";
 import { layout } from "../../utils/style/layout";
 
 const StepContent = ({ step }: { step: number }) => {
@@ -84,83 +74,11 @@ export const LaunchpadCreateScreen: ScreenFC<"LaunchpadCreate"> = () => {
           marginTop: layout.spacing_x3,
         }}
       >
-        <PrimaryBox
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            minHeight: 56,
-            minWidth: 1290,
-            backgroundColor: neutral17,
-            justifyContent: "center",
-            marginHorizontal: layout.spacing_x3,
-            borderWidth: 0,
-          }}
-        >
-          {stepOptions.map((item, index) => (
-            <TouchableOpacity
-              onPress={() => {
-                setSelectedStep(item.key);
-              }}
-              key={index}
-              style={[
-                {
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  paddingHorizontal: layout.spacing_x2,
-                  paddingVertical: layout.spacing_x1,
-                },
-              ]}
-            >
-              <View
-                style={{
-                  width: layout.iconButton,
-                  height: layout.iconButton,
-                  borderRadius: layout.iconButton / 2,
-                  backgroundColor:
-                    selectedStep === item.key ? primaryColor : neutral22,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  marginRight: layout.spacing_x0_75,
-                }}
-              >
-                <BrandText
-                  style={[
-                    fontSemibold14,
-                    {
-                      color:
-                        selectedStep === item.key
-                          ? primaryTextColor
-                          : neutral77,
-                    },
-                  ]}
-                >
-                  {item.key}
-                </BrandText>
-              </View>
-              <BrandText
-                style={[
-                  fontSemibold14,
-                  {
-                    marginLeft: 12,
-                    color: selectedStep === item.key ? primaryColor : neutral77,
-                    marginRight: layout.spacing_x2,
-                  },
-                ]}
-              >
-                {item.title}
-              </BrandText>
-              {stepOptions.length !== index + 1 && (
-                <SVG
-                  source={ChevronRightSvg}
-                  width={16}
-                  height={16}
-                  color={neutral77}
-                />
-              )}
-            </TouchableOpacity>
-          ))}
-        </PrimaryBox>
+        <LaunchpadSteper
+          stepOptions={stepOptions}
+          step={selectedStep}
+          onStepPress={setSelectedStep}
+        />
 
         <View
           style={{
