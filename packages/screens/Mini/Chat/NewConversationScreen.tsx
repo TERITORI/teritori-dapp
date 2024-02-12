170b3fb51866028294644f0a3c5a20d0fef4996c
sujal-into <155436948+sujal-into@users.noreply.github.com>
2024-02-12 18:11:40 +0545

    fix: don't crop text in mini mode inputs (#947)

--- packages/screens/Mini/Chat/NewConversationScreen.tsx
+++ packages/screens/Mini/Chat/NewConversationScreen.tsx
@@ -70,7 +70,7 @@ export const NewConversationScreen: ScreenFC<"MiniNewConversation"> = ({
           icon={searchSVG}
           placeholder="Search by nickname"
           style={{ paddingVertical: layout.spacing_x1 }}
-          inputStyle={[fontMedium14]}
+          inputStyle={[fontMedium14, { lineHeight: 0 }]}
         />
         <SpacerColumn size={2} />
         <NewConversationOrGroupSelector
