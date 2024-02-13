d9af23a9d2763c97e1358db140d08387df0f2482
Norman Meier <norman@samourai.coop>
2024-02-13 15:21:34 +0100

    fix: inject buffer on all platforms
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- App.native.tsx
+++ App.native.tsx
@@ -9,10 +9,6 @@ import Constants from "expo-constants";
 
 import Root from "./Root";
 
-if (!globalThis.Buffer) {
-  globalThis.Buffer = require("buffer").Buffer;
-}
-
 Object.entries(Constants.expoConfig?.extra?.env).forEach(([key, value]) => {
   process.env[key] = `${value}`;
 });
