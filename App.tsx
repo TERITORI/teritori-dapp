9bdb82ba3ac26954ab95bbfd24f80dacf622676e
Norman Meier <norman@samourai.coop>
2024-02-13 14:08:48 +0100

    chore: revert uneeded changes
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- App.tsx
+++ App.tsx
@@ -1,12 +1,7 @@
-import { Buffer as BufferImport } from "buffer";
 import Constants from "expo-constants";
 
 import "@expo/metro-runtime";
 import Root from "./Root";
-
-if (!globalThis.Buffer) {
-  globalThis.Buffer = BufferImport;
-}
 
 Object.entries(Constants.expoConfig?.extra?.env).forEach(([key, value]) => {
   process.env[key] = `${value}`;
