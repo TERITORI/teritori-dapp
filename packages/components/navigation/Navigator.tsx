90f524ecb4e20afa6f958ffe1b146e989d1407fa
Sachin <sachin@intosoft.com>
2024-02-19 12:18:26 +0545

    fixed unused exports

--- packages/components/navigation/Navigator.tsx
+++ packages/components/navigation/Navigator.tsx
@@ -6,12 +6,19 @@ import { getNormalModeScreens } from "./getNormalModeScreens";
 import { getNav } from "./util";
 
 import { useAppMode } from "@/hooks/useAppMode";
+import { useOnboardedStatus } from "@/hooks/useOnboardStatus";
 import { AppMode } from "@/utils/types/app-mode";
 
 export const Navigator: React.FC = () => {
   const [appMode] = useAppMode();
+  const [isLoading] = useOnboardedStatus();
 
   const { Nav, navigatorScreenOptions } = getNav(appMode as AppMode);
+
+  if (isLoading && appMode === "mini") {
+    return null;
+  }
+
   return (
     <Nav.Navigator
       initialRouteName={appMode === "mini" ? "MiniTabs" : "Home"}
