6f82ea1b7bce3658637a179d1fe1f99d72d2e066
Eng. Juan Combetto <omniwired@gmail.com>
2024-02-20 16:35:58 +0900

    chore(lint): no-restricted-imports

--- packages/utils/wallet/getNativeSigner.ts
+++ packages/utils/wallet/getNativeSigner.ts
@@ -3,12 +3,13 @@ import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { getNativeWallet } from "./getNativeWallet";
 
 import { mustGetCosmosNetwork } from "@/networks";
+// eslint-disable-next-line no-restricted-imports
 import { StoreWallet } from "@/store/slices/wallets";
 
 export const getNativeSigner = async (selectedWallet: StoreWallet) => {
   const network = mustGetCosmosNetwork(selectedWallet.networkId);
 
-  const wallet = await getNativeWallet("tori", selectedWallet.index);
+  const wallet = await getNativeWallet(network.idPrefix, selectedWallet.index);
   const rpcEndpoint = network.rpcEndpoint;
   const client = await SigningCosmWasmClient.connectWithSigner(
     rpcEndpoint,
