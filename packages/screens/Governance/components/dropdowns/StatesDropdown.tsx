9342c214c2d076839e658397237c3276b0a71865
ChiragPansuriya-iView <134677881+ChiragPansuriya-iView@users.noreply.github.com>
2024-02-23 13:48:34 +0530

    feat: governance proposal list ui v2 (#980)
    * fix: governance proposal list ui updated as per figma
    
    * fix: lint error fixed
    
    * fix: refactoring governance box
    
    * fix: color and style change
    
    * fix: search functionality added
    
    * fix: remove second dropdown and updated UI as per figma
    
    * fix: revert victory package changes
    
    * fix: removed victory package
    
    * fix: dropdown click issue fixed
    
    * chore: rename option
    
    * fix: replace getKeys to objectKeys in Statesdropdown
    
    ---------
    
    Co-authored-by: n0izn0iz <n0izn0iz@users.noreply.github.com>

---
+++ packages/screens/Governance/components/dropdowns/StatesDropdown.tsx
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import { TouchableOpacity, View, ViewStyle } from "react-native";
+
+import chevronDownSVG from "./../../../../../assets/icons/chevron-down.svg";
+import chevronUpSVG from "./../../../../../assets/icons/chevron-up.svg";
+
+import { BrandText } from "@/components/BrandText";
+import { SVG } from "@/components/SVG";
+import { Separator } from "@/components/separators/Separator";
+import { SpacerColumn } from "@/components/spacer";
+import { useDropdowns } from "@/hooks/useDropdowns";
+import { neutral33, neutral55, secondaryColor } from "@/utils/style/colors";
+import { fontMedium14, fontSemibold14 } from "@/utils/style/fonts";
+import { layout } from "@/utils/style/layout";
+import { ProposalStatus } from "@/utils/types/gov";
+import { objectKeys } from "@/utils/typescript";
+
+const def = {
+  all: {
+    name: "All states",
+    filter: undefined,
+  },
+  voting: {
+    name: "Voting",
+    filter: "PROPOSAL_STATUS_VOTING",
+  },
+  passed: {
+    name: "Passed",
+    filter: "PROPOSAL_STATUS_PASSED",
+  },
+  rejected: {
+    name: "Rejected",
+    filter: "PROPOSAL_STATUS_REJECTED",
+  },
+};
+
+interface StatesDropdownGovProps {
+  style?: ViewStyle;
+  onChange: (filter?: ProposalStatus) => void;
+}
+
+export const StatesDropdown = ({ style, onChange }: StatesDropdownGovProps) => {
+  const [isDropdownOpen, setDropdownState, ref] = useDropdowns();
+  const [selected, setSelected] = useState<keyof typeof def>("all");
+
+  return (
+    <View
+      style={[
+        {
+          zIndex: 1,
+          width: 148,
+        },
+        style,
+      ]}
+      ref={ref}
+    >
+      <View
+        style={{
+          backgroundColor: neutral33,
+          borderWidth: 1,
+          borderRadius: 8,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            paddingHorizontal: layout.spacing_x1_5,
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: "100%",
+              height: 40,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flex: 1,
+            }}
+            activeOpacity={1}
+            onPress={() =>
+              isDropdownOpen ? setDropdownState(false) : setDropdownState(true)
+            }
+          >
+            <BrandText
+              style={[
+                fontMedium14,
+                {
+                  marginRight: layout.spacing_x1,
+                  color: secondaryColor,
+                },
+              ]}
+            >
+              {def[selected].name}
+            </BrandText>
+            <SVG
+              source={isDropdownOpen ? chevronUpSVG : chevronDownSVG}
+              width={16}
+              height={16}
+              color={secondaryColor}
+            />
+          </TouchableOpacity>
+          {isDropdownOpen && (
+            <View
+              style={{
+                position: "absolute",
+                top: 35,
+                right: 0,
+                width: "100%",
+                paddingHorizontal: layout.spacing_x1_5,
+                paddingBottom: layout.spacing_x1,
+                backgroundColor: neutral33,
+                borderBottomEndRadius: 8,
+                borderBottomLeftRadius: 8,
+                alignItems: "flex-start",
+                zIndex: 1,
+              }}
+            >
+              <Separator
+                color={neutral55}
+                style={{ marginTop: layout.spacing_x1 }}
+              />
+
+              {objectKeys(def).map((key, index) => {
+                return (
+                  <TouchableOpacity
+                    onPress={() => {
+                      onChange(def[key].filter as any);
+                      setSelected(key);
+                      setDropdownState(false);
+                    }}
+                    style={{
+                      paddingTop: layout.spacing_x1_5,
+                      width: "100%",
+                    }}
+                  >
+                    <BrandText
+                      style={[fontSemibold14, { color: secondaryColor }]}
+                    >
+                      {def[key].name}
+                    </BrandText>
+
+                    {objectKeys(def).length - 1 !== index && (
+                      <>
+                        <SpacerColumn size={1} />
+                        <Separator color={neutral55} />
+                      </>
+                    )}
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          )}
+        </View>
+      </View>
+    </View>
+  );
+};
