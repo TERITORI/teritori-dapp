dfb0431ceab889d0fada3799c335444a00c538c4
sachin-into <154645503+sachin-into@users.noreply.github.com>
2024-03-06 02:06:26 +0545

    feat: mini create post (#1026)
    * fix: replaced create post normal input by mini input
    
    * navigate to feed on post add
    
    * chore: fix import
    
    ---------
    
    Co-authored-by: n0izn0iz <n0izn0iz@users.noreply.github.com>

--- packages/components/navigation/getMiniModeScreens.tsx
+++ packages/components/navigation/getMiniModeScreens.tsx
@@ -125,7 +125,6 @@ export const getMiniModeScreens = () => {
         options={{
           header: () => null,
           title: "",
-          presentation: "transparentModal",
         }}
       />
 
