55a9d9d19b557cb472b7ef5ddb823563a3e78427
Sachin <sachin@intosoft.com>
2024-03-11 17:38:08 +0545

    fix:emoji reaction on feeds

--- packages/hooks/feed/useSocialReactions.ts
+++ packages/hooks/feed/useSocialReactions.ts
@@ -63,6 +63,20 @@ export const useSocialReactions = ({
         up: true,
       },
     });
+
+    //Update reactions on post
+    const reactions = [...post.reactions];
+    const currentReactionIdx = reactions.findIndex((r) => r.icon === emoji);
+    if (currentReactionIdx > -1) {
+      reactions[currentReactionIdx].count++;
+    } else {
+      reactions.push({
+        icon: emoji,
+        count: 1,
+        ownState: true,
+      });
+    }
+    setPost({ ...post, reactions });
   };
   const gnoReaction = async (emoji: string, rpcEndpoint: string) => {
     const gnoNetwork = mustGetGnoNetwork(selectedNetworkId);
