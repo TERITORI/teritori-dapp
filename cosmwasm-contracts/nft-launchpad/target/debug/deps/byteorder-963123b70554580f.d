1a1a83c4d1d6b70ec0f742129fc3e3d46f2ed13f
hthieu1110 <tronghieu.ha@gmail.com>
2024-03-13 18:06:04 +0700

    chore: remove rust target builds

--- cosmwasm-contracts/nft-launchpad/target/debug/deps/byteorder-963123b70554580f.d
+++
@@ -1,7 +0,0 @@
-/Users/hieu.ha/Projects/teritori-dapp/rust/contracts/nft-launchpad/target/debug/deps/libbyteorder-963123b70554580f.rmeta: /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/Users/hieu.ha/Projects/teritori-dapp/rust/contracts/nft-launchpad/target/debug/deps/libbyteorder-963123b70554580f.rlib: /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/Users/hieu.ha/Projects/teritori-dapp/rust/contracts/nft-launchpad/target/debug/deps/byteorder-963123b70554580f.d: /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs:
--- cosmwasm-contracts/nft-launchpad/target/debug/deps/hmac-ce9fc5dff988888f.d
+++
@@ -1,9 +0,0 @@
-/Users/hieu.ha/Projects/teritori-dapp/rust/contracts/nft-launchpad/target/debug/deps/libhmac-ce9fc5dff988888f.rmeta: /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/lib.rs /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/optim.rs /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/simple.rs
-
-/Users/hieu.ha/Projects/teritori-dapp/rust/contracts/nft-launchpad/target/debug/deps/libhmac-ce9fc5dff988888f.rlib: /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/lib.rs /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/optim.rs /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/simple.rs
-
-/Users/hieu.ha/Projects/teritori-dapp/rust/contracts/nft-launchpad/target/debug/deps/hmac-ce9fc5dff988888f.d: /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/lib.rs /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/optim.rs /Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/simple.rs
-
-/Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/lib.rs:
-/Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/optim.rs:
-/Users/hieu.ha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hmac-0.12.1/src/simple.rs:
