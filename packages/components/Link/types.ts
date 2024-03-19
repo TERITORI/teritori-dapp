473943830330bdbd4c1b1dac3cc1ace380225d83
sujal-into <155436948+sujal-into@users.noreply.github.com>
2024-03-19 19:35:48 +0545

    fix: home banner mobile layout (#1100)
    * fix: custompressable to wrap the image
    
    * fix: Custom Pressable added to the hub landing
    
    * fix: HubLanding import link removed
    
    * fix: image getting bigger with new approach max height

--- packages/components/Link/types.ts
+++ packages/components/Link/types.ts
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
 
 export type LinkProps = {
   to?: string;
   children: ReactNode;
+  style?: StyleProp<ViewStyle | TextStyle>;
 };
