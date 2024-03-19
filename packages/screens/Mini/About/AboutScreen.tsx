64368c8eb1fc9b1c49d1cb9d4d2c536a5e4acbcb
ChiragPansuriya-iView <134677881+ChiragPansuriya-iView@users.noreply.github.com>
2024-03-19 16:24:10 +0530

    fix: all link added exclude help and support  link

--- packages/screens/Mini/About/AboutScreen.tsx
+++ packages/screens/Mini/About/AboutScreen.tsx
@@ -1,15 +1,20 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { FlatList, View } from "react-native";
+import { useState } from "react";
+import { FlatList, SafeAreaView, View } from "react-native";
 
 import externalLinkSVG from "../../../../assets/icons/external-grey.svg";
 import teritoriSVG from "../../../../assets/icons/teritori-white.svg";
 import ListView from "../components/ListView";
 import { BlurScreenContainer } from "../layout/BlurScreenContainer";
 
+import BackSVG from "@/assets/icons/back.svg";
 import { BrandText } from "@/components/BrandText";
+import { EmbeddedWeb } from "@/components/EmbeddedWeb";
 import { SVG } from "@/components/SVG";
 import { TertiaryBadge } from "@/components/badges/TertiaryBadge";
+import { CustomPressable } from "@/components/buttons/CustomPressable";
 import { SpacerColumn } from "@/components/spacer";
+import { useIsMobile } from "@/hooks/useIsMobile";
 import { RootStackParamList } from "@/utils/navigation";
 import { neutral77 } from "@/utils/style/colors";
 import { fontBold16 } from "@/utils/style/fonts";
@@ -22,27 +27,27 @@ type AboutScreenProps = {
 const links = [
   {
     label: "Privacy Policy",
-    url: "https:ggogle.com",
+    url: "https://teritori.notion.site/Privacy-Policy-16e2332744d346db9b78909a91cb44e3",
   },
   {
     label: "Terms of Use",
-    url: "https:ggogle.com",
+    url: "https://teritori.notion.site/Terms-Conditions-6007160a0c74472cbb75a07bdfdd5f73",
   },
   {
     label: "Help & Support",
-    url: "https:ggogle.com",
+    url: "https://teritori.notion.site/Privacy-Policy-16e2332744d346db9b78909a91cb44e3",
   },
   {
     label: "GitHub",
-    url: "https:ggogle.com",
+    url: "https://github.com/TERITORI",
   },
   {
     label: "Developer Docs",
-    url: "https:ggogle.com",
+    url: "https://github.com/TERITORI/documentation",
   },
   {
     label: "Website",
-    url: "https:ggogle.com",
+    url: "https://teritori.com/",
   },
 ];
 
@@ -52,53 +57,85 @@ export default function AboutScreen({ navigation }: AboutScreenProps) {
       ? navigation.goBack()
       : navigation.replace("MiniTabs", { screen: "MiniChats" });
 
-  return (
-    <BlurScreenContainer title="About Teritori" onGoBack={onClose}>
-      <View
-        style={{
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <SpacerColumn size={3} />
-        <SVG source={teritoriSVG} fontSize={64} />
-        <SpacerColumn size={1} />
-        <BrandText style={fontBold16}>TERITORI</BrandText>
-        <SpacerColumn size={1} />
-        <TertiaryBadge label="v1.25" textColor={neutral77} />
-        <SpacerColumn size={6} />
-      </View>
+  const isMobile = useIsMobile();
+  const seasonVideoHeight = isMobile ? 214 : 293;
+  const seasonVideoWidth = isMobile ? 400 : 516;
 
-      <View
-        style={{
-          justifyContent: "space-between",
-          backgroundColor: "rgba(0,0,0,0.8)",
-          padding: layout.spacing_x2,
-          flex: 1,
-        }}
-      >
-        <FlatList
-          inverted
-          data={links}
-          keyExtractor={(item) => item.label}
-          renderItem={({ item }) => (
-            <ListView
-              onPress={() => {}}
-              style={{
-                paddingHorizontal: layout.spacing_x1_5,
-              }}
-              options={{
-                label: item?.label,
-                iconEnabled: true,
-                iconOptions: {
-                  icon: externalLinkSVG,
-                },
-              }}
+  const [url, setUrl] = useState("");
+
+  return (
+    <>
+      {url ? (
+        <SafeAreaView style={{ flex: 1 }}>
+          <CustomPressable
+            onPress={() => {
+              setUrl("");
+            }}
+            style={{
+              marginLeft: 15,
+              backgroundColor: "#000",
+              paddingVertical: 8,
+            }}
+          >
+            <SVG source={BackSVG} height={28} width={28} />
+          </CustomPressable>
+          <EmbeddedWeb
+            uri={url}
+            width={seasonVideoWidth - 2}
+            height={seasonVideoHeight}
+          />
+        </SafeAreaView>
+      ) : (
+        <BlurScreenContainer title="About Teritori" onGoBack={onClose}>
+          <View
+            style={{
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <SpacerColumn size={3} />
+            <SVG source={teritoriSVG} fontSize={64} />
+            <SpacerColumn size={1} />
+            <BrandText style={fontBold16}>TERITORI</BrandText>
+            <SpacerColumn size={1} />
+            <TertiaryBadge label="v1.25" textColor={neutral77} />
+            <SpacerColumn size={6} />
+          </View>
+
+          <View
+            style={{
+              justifyContent: "space-between",
+              backgroundColor: "rgba(0,0,0,0.8)",
+              padding: layout.spacing_x2,
+              flex: 1,
+            }}
+          >
+            <FlatList
+              inverted
+              data={links}
+              keyExtractor={(item) => item.label}
+              renderItem={({ item }) => (
+                <ListView
+                  onPress={() => {
+                    setUrl(item?.url);
+                  }}
+                  style={{
+                    paddingHorizontal: layout.spacing_x1_5,
+                  }}
+                  options={{
+                    label: item?.label,
+                    iconEnabled: true,
+                    iconOptions: {
+                      icon: externalLinkSVG,
+                    },
+                  }}
+                />
+              )}
             />
-          )}
-        />
-      </View>
-    </BlurScreenContainer>
+          </View>
+        </BlurScreenContainer>
+      )}
+    </>
   );
 }
