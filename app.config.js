9e039647d5f0c7cf8b568efabc07d72be7d95584
Sujal Lama <sujal@intosoft.com>
2024-03-20 10:13:07 +0545

    fix: api access reasons provided

--- app.config.js
+++ app.config.js
@@ -27,6 +27,14 @@ const config = {
         NSPhotoLibraryUsageDescription:
           "Access to your photo library is required for image upload functionality.",
         ITSAppUsesNonExemptEncryption: false,
+        NSPrivacyAccessedAPICategoryDiskSpace:
+          "To efficiently manage local storage and cache files for improved app performance.",
+        NSPrivacyAccessedAPICategoryFileTimestamp:
+          "To track file modification dates for app data synchronization and integrity checks.",
+        NSPrivacyAccessedAPICategoryUserDefaults:
+          "To store user preferences and settings locally for a customized user experience.",
+        NSPrivacyAccessedAPICategorySystemBootTime:
+          "To optimize app launch times and resource utilization based on system boot information.",
       },
     },
     android: {
