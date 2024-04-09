b23b7bc4ad3f875ad18b6f1b703f2c03d3a83d7b
hthieu1110 <tronghieu.ha@gmail.com>
2024-04-09 21:14:54 +0700

    chore: merge + update proto

--- rust/apipb/src/launchpad.v1.rs
+++ rust/apipb/src/launchpad.v1.rs
@@ -53,24 +53,17 @@ pub struct TokenMetadataResponse {
     #[prost(string, repeated, tag="3")]
     pub merkle_proof: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
 }
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct UpdateCollectionWhitelistsRequest {
-    #[prost(string, tag="1")]
-    pub sender: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
-    pub network_id: ::prost::alloc::string::String,
-    #[prost(uint32, tag="3")]
-    pub project_id: u32,
-    #[prost(message, repeated, tag="4")]
-    pub whitelist_mint_infos: ::prost::alloc::vec::Vec<WhitelistMintInfo>,
-}
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct UpdateCollectionWhitelistsResponse {
-    #[prost(string, repeated, tag="1")]
-    pub merkle_roots: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-}
+// message UpdateCollectionWhitelistsRequest {
+//    string sender = 1;
+//    string network_id = 2;
+//    uint32 project_id = 3;
+//    repeated WhitelistMintInfo whitelist_mint_infos = 4; 
+// }
+
+// message UpdateCollectionWhitelistsResponse {
+//    repeated string merkle_roots = 1;
+// }
+
 // message WhitelistedAddressMerkleInfoRequest {
 //    string sender = 1;
 //    string network_id = 2;
@@ -119,93 +112,5 @@ pub struct Trait {
     pub trait_type: ::prost::alloc::string::String,
     #[prost(string, tag="3")]
     pub value: ::prost::alloc::string::String,
-}
-// message ProjectInfo {
-//    // Collection info ----------------------------
-//    string name = 1;
-//    string desc = 2;
-//    string symbol = 3;
-//    string cover_img_uri = 4;
-//    string target_network = 5;
-//    string external_link = 6;
-
-//    // Collection details ----------------------------
-//    string website_link = 7;
-
-//    string twitter_profile = 8;
-//    uint64 twitter_followers_count = 9;
-
-//    string contact_discord_name = 10;
-//    string contact_email = 11;
-
-//    bool is_project_derivative = 12;
-
-//    string project_type = 13;
-//    string project_desc = 14;
-
-//    bool is_applied_previously = 15;
-
-//    // Team info --------------------------------------
-//    string team_desc = 16;
-//    string team_link = 17;
-
-//    string partners = 18;
-
-//    string investment_desc = 19;
-//    string investment_link = 20;
-
-//    string whitepaper_link = 21;
-//    string roadmap_link = 22;
-
-//    // Additional info ----------------------------
-//    string artwork_desc = 23;
-
-//    bool is_ready_for_mint = 24;
-
-//    uint32 expected_supply = 25;
-//    uint64 expected_public_mint_price = 26;
-//    uint64 expected_mint_date = 27;
-
-//    uint64 escrow_mint_proceeds_period = 28;
-//    bool is_dox = 29;
-
-//    uint32 dao_whitelist_count = 30;
-
-//    // Minting details ----------------------------
-//    uint32 tokens_count = 31;
-//    uint64 unit_price = 32;
-//    uint32 limit_per_address = 33;
-//    uint64 start_time = 34;
-
-//    // Whitelist minting --------------------------
-//    // repeated WhitelistMinting whitelist_mintings = 35;
-
-//    // Royalty --------------------------
-//    string royalty_address = 35;
-//    uint32 royalty_percentage = 36;
-
-//    // Extend info --------------------------
-//    string base_token_uri = 37;
-//    string merkle_root = 38;
-//    string deployed_address = 39;
-// }
-
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct WhitelistMintInfo {
-    #[prost(string, repeated, tag="1")]
-    pub addresses: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(uint64, tag="2")]
-    pub unit_price: u64,
-    #[prost(string, tag="3")]
-    pub denom: ::prost::alloc::string::String,
-    #[prost(uint32, tag="4")]
-    pub limit_per_address: u32,
-    #[prost(uint32, tag="5")]
-    pub addresses_count: u32,
-    #[prost(uint64, tag="6")]
-    pub start_time: u64,
-    #[prost(uint64, tag="7")]
-    pub end_time: u64,
 }
 // @@protoc_insertion_point(module)
