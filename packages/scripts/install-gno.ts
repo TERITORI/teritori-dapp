ed33d9eb230adfa9e93e8665e3508e280f4c7b66
Norman Meier <norman@samourai.coop>
2024-05-28 01:08:31 +0200

    chore: use https remote
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- packages/scripts/install-gno.ts
+++ packages/scripts/install-gno.ts
@@ -3,7 +3,7 @@ import child_process from "child_process";
 import sqh from "./sqh";
 
 const ref = "project-manager-atomic";
-const remote = "git@github.com:TERITORI/gno.git";
+const remote = "https://github.com/TERITORI/gno.git";
 
 const main = async () => {
   const cmd = `rm -fr gno && git clone ${sqh(remote)} && cd gno && git checkout ${sqh(ref)} && make install`;
