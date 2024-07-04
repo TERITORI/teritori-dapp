d4b622e12e85d7b32ca6ff9394aca5c6ead9a570
Norman Meier <norman@samourai.coop>
2024-07-04 14:36:38 +0200

    chore: rename preload helper test
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- cypress/e2e/0_preload.cy.ts
+++ cypress/e2e/0_preload.cy.ts
@@ -1,4 +1,6 @@
-describe("The Home Page", () => {
+// this helps to get accurate test times by running first and preloading the app
+
+describe("Preload", () => {
   it("successfully loads", () => {
     cy.visit("http://localhost:8081", {
       timeout: 300000,
