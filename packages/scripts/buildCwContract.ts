937a2e4a94027b941fe8eb29ba2ea0d1c49bd879
hthieu1110 <tronghieu.ha@gmail.com>
2024-08-16 19:38:06 +0700

    feat: add scripts to build cosmwasm contract with optimizer

---
+++ packages/scripts/buildCwContract.ts
@@ -0,0 +1,17 @@
+import child_process from "child_process";
+import { program } from "commander";
+
+const main = async () => {
+  program.argument("<pkgname>", "Name of contract package to build");
+  program.parse();
+  const [pkgname] = program.args as [string];
+
+  const buildCmd = `docker run --rm -v ./:/code \
+  --mount type=volume,source="${pkgname}_cache",target=/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/optimizer:0.16.0 ./rust/cw-contracts/${pkgname}`;
+  
+  child_process.execSync(buildCmd, { stdio: "inherit" });
+};
+
+main();
