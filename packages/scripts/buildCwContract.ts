da49e98bcdf181820722a505659eb72e97697135
Norman Meier <norman@samourai.coop>
2024-08-16 15:38:50 +0200

    fix: force platform
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- packages/scripts/buildCwContract.ts
+++ packages/scripts/buildCwContract.ts
@@ -6,11 +6,11 @@ const main = async () => {
   program.parse();
   const [pkgname] = program.args as [string];
 
-  const buildCmd = `docker run --rm -v ./:/code \
+  const buildCmd = `docker run -p linux/amd64 --rm -v ./:/code \
   --mount type=volume,source="${pkgname}_cache",target=/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   cosmwasm/optimizer:0.16.0 ./rust/cw-contracts/${pkgname}`;
-  
+
   child_process.execSync(buildCmd, { stdio: "inherit" });
 };
 
