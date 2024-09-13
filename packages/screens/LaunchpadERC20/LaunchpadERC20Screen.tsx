0888072b1117b4e6e0a4095dc2ac97fcdb6f58b0
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-09-13 13:49:25 +0200

    feat(launchpad-grc20): add launchpadERC20 banner & description text

--- packages/screens/LaunchpadERC20/LaunchpadERC20Screen.tsx
+++ packages/screens/LaunchpadERC20/LaunchpadERC20Screen.tsx
@@ -1,14 +1,46 @@
-import React from "react";
-
 import { BrandText } from "@/components/BrandText";
 import { ScreenContainer } from "@/components/ScreenContainer";
+import { SpacerColumn } from "@/components/spacer";
 import { ScreenFC } from "@/utils/navigation";
-import { fontSemibold28 } from "@/utils/style/fonts";
+import { neutral77 } from "@/utils/style/colors";
+import { fontSemibold14, fontSemibold28 } from "@/utils/style/fonts";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { LaunchpadERC20Banner } from "./component/LaunchpadERC20Banner";
+
 
 export const LaunchpadERC20Screen: ScreenFC<"LaunchpadERC20"> = () => {
     return (
         <ScreenContainer>
-            <BrandText style={fontSemibold28}>Welcome</BrandText>
+            <LaunchpadERC20Banner />
+            <SpacerColumn size={2} />
+            <BrandText style={fontSemibold28}>Welcome on the ERC20 Launchpad</BrandText>
+            <SpacerColumn size={2} />
+            <BrandText style={styles.descriptionText}>
+                Looking to create your own ERC20 token? Look no further.
+            </BrandText>
+            <SpacerColumn size={2} />
+            <BrandText style={styles.descriptionText}>
+                Teritori provide a simple and efficient way to create your own ERC20 token.
+            </BrandText>
+            <BrandText style={styles.descriptionText}>
+                In few clicks, you can create your own tokens, airdrops and sales to engage with your community.
+            </BrandText>
         </ScreenContainer>
     );
 };
+
+// FIXME: remove StyleSheet.create
+// eslint-disable-next-line no-restricted-syntax
+const styles = StyleSheet.create({
+    descriptionText: StyleSheet.flatten([
+        fontSemibold14,
+        {
+            color: neutral77,
+        },
+    ]),
+    buttonsContainer: {
+        flexDirection: "row",
+        flex: 1,
+    },
+});
---
+++ packages/screens/LaunchpadERC20/component/LaunchpadERC20Banner.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Image, View } from "react-native";
+
+import LaunchpadERC20BannerImage from "../../../../assets/banners/launchpadERC20.jpg";
+import LogoSimpleSvg from "../../../../assets/icons/logo-simple.svg";
+
+import { BrandText } from "@/components/BrandText";
+import { SVG } from "@/components/SVG";
+import { SpacerColumn } from "@/components/spacer";
+import { useMaxResolution } from "@/hooks/useMaxResolution";
+import { fontSemibold16, fontSemibold28 } from "@/utils/style/fonts";
+
+export const LaunchpadERC20Banner: React.FC = () => {
+    const { width } = useMaxResolution();
+    const isSmallScreen = width < 500;
+    const logoSize = isSmallScreen ? 44 : 88;
+    const font = isSmallScreen ? fontSemibold16 : fontSemibold28;
+
+    return (
+        <View>
+            <Image
+                source={LaunchpadERC20BannerImage}
+                style={{
+                    width,
+                    height: width / 2.5,
+                }}
+            />
+            <View
+                style={{
+                    position: "absolute",
+                    height: "100%",
+                    width: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <SVG source={LogoSimpleSvg} width={logoSize} height={logoSize} />
+                <SpacerColumn size={1} />
+                <BrandText style={font}>Launchpad ERC20 Platform</BrandText>
+            </View>
+        </View>
+    );
+};
