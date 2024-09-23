8e21a48ffee6c49b6de186c5df3a44f75869b168
Mikael VALLENET <mikael.vallenetpro@gmail.com>
2024-09-23 23:11:16 +0200

    Update packages/screens/LaunchpadERC20/LaunchpadERCAirdrops/LaunchpadERC20AirdropsScreen.tsx
    Co-authored-by: WaDadidou <50441633+WaDadidou@users.noreply.github.com>

--- packages/screens/LaunchpadERC20/LaunchpadERCAirdrops/LaunchpadERC20AirdropsScreen.tsx
+++ packages/screens/LaunchpadERC20/LaunchpadERCAirdrops/LaunchpadERC20AirdropsScreen.tsx
@@ -32,6 +32,7 @@ export const LaunchpadERC20AirdropsScreen: ScreenFC<
       forceNetworkKind={NetworkKind.Gno}
       isLarge
       responsive
+      onBackPress={() => navigation.navigate("LaunchpadERC20")}
     >
       <View
         style={{
