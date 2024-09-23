b19fbf24ca9bd3cfa5952b856c7d4145160d743f
Mikael VALLENET <mikael.vallenetpro@gmail.com>
2024-09-23 23:11:01 +0200

    Update packages/screens/LaunchpadERC20/LaunchpadERC20Tokens/LaunchpadERC20TokensScreen.tsx
    Co-authored-by: WaDadidou <50441633+WaDadidou@users.noreply.github.com>

--- packages/screens/LaunchpadERC20/LaunchpadERC20Tokens/LaunchpadERC20TokensScreen.tsx
+++ packages/screens/LaunchpadERC20/LaunchpadERC20Tokens/LaunchpadERC20TokensScreen.tsx
@@ -32,6 +32,7 @@ export const LaunchpadERC20TokensScreen: ScreenFC<"LaunchpadERC20Tokens"> = ({
       forceNetworkKind={NetworkKind.Gno}
       isLarge
       responsive
+      onBackPress={() => navigation.navigate("LaunchpadERC20")}
     >
       <View
         style={{
