a933aaefec09a5e3eac1d83dd338ce151731e20e
hthieu1110 <tronghieu.ha@gmail.com>
2024-10-08 23:02:55 +0700

    fix: fallback to retry to add chain when unable to get eth provider

--- packages/networks/polygon/currencies.ts
+++ packages/networks/polygon/currencies.ts
@@ -4,10 +4,10 @@ import { CurrencyInfo } from "../types";
 export const polygonCurrencies: CurrencyInfo[] = [
   {
     denom: "0x0000000000000000000000000000000000000000", // native currency: wei
-    displayName: "MATIC",
+    displayName: "POL",
     decimals: 18,
     variant: "ethereum",
-    coingeckoId: "matic",
+    coingeckoId: "pol-ex-matic",
     icon: "polygon.svg",
     kind: "native",
     color: currencyETHcolor,
