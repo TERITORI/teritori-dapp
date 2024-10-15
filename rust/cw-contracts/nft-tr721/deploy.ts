c801467865c521981e9c28f90b750d424cb75bff
WaDadidou <wadadidou@gmail.com>
2024-10-15 12:53:10 -0400

    chore: restore rust TS scripts

--- rust/cw-contracts/nft-tr721/deploy.ts
+++ rust/cw-contracts/nft-tr721/deploy.ts
@@ -1,13 +1,10 @@
-// ERROR: import is restricted from being used by a pattern. Scripts imports are only allowed in scripts  no-restricted-imports
-// =====> So I comment this file
+import { deploy } from "../../utils/scripts";
 
-// import { deploy } from "../../scripts/utils";
-//
-// const WASM_FILE = "nft_tr721-aarch64.wasm";
-//
-// const main = async () => {
-//   const codeId = await deploy(WASM_FILE);
-//   console.log("CodeId:", codeId);
-// };
-//
-// main();
+const WASM_FILE = "nft_tr721-aarch64.wasm";
+
+const main = async () => {
+  const codeId = await deploy(WASM_FILE);
+  console.log("CodeId:", codeId);
+};
+
+main();
