8f6362ba912e82b362536522ea490fee7d4842fb
WaDadidou <wadadidou@gmail.com>
2024-10-18 14:06:26 -0400

    feat: add nft-launchpad contract and deps

---
+++ rust/cw-contracts/nft-tr721/deploy.ts
@@ -0,0 +1,9 @@
+import { deploy } from "../../utils/scripts";
+const WASM_FILE = "nft_tr721.wasm";
+
+const main = async () => {
+  const codeId = await deploy(WASM_FILE);
+  console.log("CodeId:", codeId);
+};
+
+main();
