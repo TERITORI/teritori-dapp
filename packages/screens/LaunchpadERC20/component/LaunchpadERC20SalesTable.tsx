284e2c9bbb6c17b7e7ef845cec4f7028c7dc4882
WaDadidou <50441633+WaDadidou@users.noreply.github.com>
2024-11-02 11:09:05 -0400

    fix: set a prop as optional and remove usage (#1326)
    * fix: make paginationProps to avoid calling TableWrapper with it
    
    * chore: remove useless prop usage

--- packages/screens/LaunchpadERC20/component/LaunchpadERC20SalesTable.tsx
+++ packages/screens/LaunchpadERC20/component/LaunchpadERC20SalesTable.tsx
@@ -75,17 +75,7 @@ export const SalesTable: React.FC<SalesTableProps> = ({ networkId }) => {
     >
       <BrandText>Latest ERC20 Sales Created</BrandText>
       <SpacerColumn size={2} />
-      <TableWrapper
-        paginationProps={{
-          currentPage: 0,
-          maxPage: 1,
-          itemsPerPage: 10,
-          nbItemsOptions: [],
-          setItemsPerPage: () => {},
-          onChangePage: () => {},
-        }}
-        horizontalScrollBreakpoint={breakpointM}
-      >
+      <TableWrapper horizontalScrollBreakpoint={breakpointM}>
         <TableHeader columns={columns} />
         {sales && (
           <FlatList
