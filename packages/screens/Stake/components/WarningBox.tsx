c3328516019f968c006cf6c2375510631e7a81b0
WaDadidou <50441633+WaDadidou@users.noreply.github.com>
2024-11-05 12:17:21 -0500

    feat(launchpad): make a svg color dynamic (#1330)
    * feat: make an svg color synamic, remove unused svg
    
    * chore: simplify import
    
    * yarn lint-fix

--- packages/screens/Stake/components/WarningBox.tsx
+++ packages/screens/Stake/components/WarningBox.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { StyleSheet, View, useWindowDimensions } from "react-native";
+import { View, useWindowDimensions } from "react-native";
 
-import warningTriangleSVG from "../../../../assets/icons/warning-triangle.svg";
-
+import warningTriangleSVG from "@/assets/icons/warning-triangle.svg";
 import { BrandText } from "@/components/BrandText";
 import { SVG } from "@/components/SVG";
 import { SpacerColumn, SpacerRow } from "@/components/spacer";
@@ -17,11 +16,22 @@ export const WarningBox: React.FC<{
   const { width: windowWidth } = useWindowDimensions();
   const maxWidth = windowWidth - 20 >= 490 ? 350 : 300;
   return (
-    <View style={[styles.stakeWarningContainer]}>
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: errorColor,
+        borderRadius: layout.borderRadius * 0.65,
+        paddingVertical: layout.spacing_x1_5,
+        paddingHorizontal: layout.spacing_x3,
+      }}
+    >
       <SVG
         width={24}
         height={24}
         source={warningTriangleSVG}
+        color={errorColor}
         style={{ flexShrink: 0 }}
       />
       <SpacerRow size={3} />
@@ -30,28 +40,10 @@ export const WarningBox: React.FC<{
           {title}
         </BrandText>
         <SpacerColumn size={0.5} />
-        <BrandText style={[styles.alternateText, { maxWidth }]}>
+        <BrandText style={[fontSemibold12, { color: neutral77, maxWidth }]}>
           {description}
         </BrandText>
       </View>
     </View>
   );
 };
-
-// FIXME: remove StyleSheet.create
-// eslint-disable-next-line no-restricted-syntax
-const styles = StyleSheet.create({
-  stakeWarningContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: 1,
-    borderColor: errorColor,
-    borderRadius: layout.borderRadius * 0.65,
-    paddingVertical: layout.spacing_x1_5,
-    paddingHorizontal: layout.spacing_x3,
-  },
-  alternateText: {
-    ...StyleSheet.flatten(fontSemibold12),
-    color: neutral77,
-  },
-});
