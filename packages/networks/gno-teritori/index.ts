ec2e57f425b171c0316ed575b9e7112f017f38e4
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-11-12 15:39:38 +0100

    feat(gno/dao): add dao_roles_group into network objects

--- packages/networks/gno-teritori/index.ts
+++ packages/networks/gno-teritori/index.ts
@@ -40,7 +40,7 @@ export const gnoTeritoriNetwork: GnoNetworkInfo = {
   modboardsPkgPath: "gno.land/r/teritori/modboards_v4",
   groupsPkgPath: "gno.land/r/teritori/groups_v4",
   votingGroupPkgPath: "gno.land/p/teritori/dao_voting_group_v2",
-  rolesGroupPkgPath: "gno.land/r/teritori/dao_roles_group",
+  rolesGroupPkgPath: "gno.land/p/teritori/dao_roles_group",
   daoProposalSinglePkgPath: "gno.land/p/teritori/dao_proposal_single_v4",
   daoInterfacesPkgPath: "gno.land/p/teritori/dao_interfaces_v5",
   daoUtilsPkgPath: "", //TODO: fill with the correct path
