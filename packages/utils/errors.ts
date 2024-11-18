7a2dc3875071501c8834f6e5927b0117cd16871b
WaDadidou <wadadidou@gmail.com>
2024-11-18 17:53:05 -0500

    chore(multisig): remove unused code

--- packages/utils/errors.ts
+++ packages/utils/errors.ts
@@ -1,8 +1,6 @@
 export const DEFAULT_FORM_ERRORS = {
   required: "This field is required",
   datetime: "Invalid",
-  address: "Invalid address",
-  emptyArray: "At least one item is required",
   onlyLetters: "Only letters are allowed",
   onlyNumbers: "Only numbers are allowed",
   onlyUrl: "Only URL is allowed",
