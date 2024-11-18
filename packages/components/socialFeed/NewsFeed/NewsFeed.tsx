3b40061bd52d1c0b3d57c967fffdd4b4dc581725
WaDadidou <wadadidou@gmail.com>
2024-11-18 13:11:54 -0500

    chore(feed): remvoe console.log

--- packages/components/socialFeed/NewsFeed/NewsFeed.tsx
+++ packages/components/socialFeed/NewsFeed/NewsFeed.tsx
@@ -171,11 +171,6 @@ export const NewsFeed: React.FC<NewsFeedProps> = ({
     );
   }, [mobileMode]);
 
-  console.log(
-    "==============",
-    posts.find((p) => p.id === "testori-caecb9c3-1a35-429b-be30-54c0578c8ec4"),
-  );
-
   const RenderItem = useCallback(
     (post: Post) => {
       // NOTE: if you edit this, make sure that this is not too CPU expensive
