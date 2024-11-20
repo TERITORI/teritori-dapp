13a2ed42422e9ff3b5c3e04c41d8b2bd7bf8012f
sujal-into <sujalintosoft@gmail.com>
2024-11-20 14:03:46 +0545

    fix: updating the value of default map posiiton

--- packages/utils/feed/map.ts
+++ packages/utils/feed/map.ts
@@ -12,8 +12,8 @@ import { CustomLatLngExpression, PostCategory } from "@/utils/types/feed";
 
 export const MAP_LAYER_URL = `https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=${process.env.EXPO_PUBLIC_LEAFLET_MAP_TOKEN}`;
 
-// Paris baguette
-export const DEFAULT_MAP_POSITION: CustomLatLngExpression = [20, 0];
+// center of the map
+export const DEFAULT_MAP_POSITION: CustomLatLngExpression = [0, 0];
 
 const musicPostSvgString = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
   <g filter="url(#filter0_di_22136_173395)">
