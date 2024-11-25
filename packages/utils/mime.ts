b3443b66030ad096ec4f204075aac4a5d9d1a9a7
sujal-into <sujalintosoft@gmail.com>
2024-11-25 17:04:03 +0545

    feat: added the possibility to upload wav audio

--- packages/utils/mime.ts
+++ packages/utils/mime.ts
@@ -1,4 +1,4 @@
-export const AUDIO_MIME_TYPES = ["audio/aac", "audio/mpeg"];
+export const AUDIO_MIME_TYPES = ["audio/aac", "audio/mpeg", "audio/wav"];
 export const VIDEO_MIME_TYPES = ["video/mp4", "video/quicktime"];
 export const GIF_MIME_TYPE = "image/gif";
 export const IMAGE_MIME_TYPES = [
