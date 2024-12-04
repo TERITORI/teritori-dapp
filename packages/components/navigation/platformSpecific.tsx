e23f00fbd2131bc1f363938e6085910f0578a95d
Norman Meier <norman@samourai.coop>
2024-12-04 17:37:04 +0100

    chore: add comment explaining why getPlatformsScreens can't be a component
    Signed-off-by: Norman Meier <norman@samourai.coop>

--- packages/components/navigation/platformSpecific.tsx
+++ packages/components/navigation/platformSpecific.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// this can't be a FC because using an intermediary component makes the Navigator crash
 export const getPlatformScreens: (
   screenTitle: (title: string) => string,
 ) => JSX.Element = () => <></>;
--- packages/components/navigation/platformSpecific.web.tsx
+++ packages/components/navigation/platformSpecific.web.tsx
@@ -6,6 +6,7 @@ import { RiotGameBridgeScreen } from "@/screens/RiotGame/RiotGameBridgeScreen";
 
 const { Nav } = getNav("normal");
 
+// this can't be a FC because using an intermediary component makes the Navigator crash
 export const getPlatformScreens: (
   screenTitle: (title: string) => string,
 ) => JSX.Element = (screenTitle) => (
