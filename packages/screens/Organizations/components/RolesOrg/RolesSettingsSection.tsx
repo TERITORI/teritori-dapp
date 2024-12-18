10d39a73722567f1372aa3e6412018909bd8f8b4
MikaelVallenet <mikael.vallenetpro@gmail.com>
2024-12-18 16:39:20 +0100

    feat: add roles tables

--- packages/screens/Organizations/components/RolesOrg/RolesSettingsSection.tsx
+++ packages/screens/Organizations/components/RolesOrg/RolesSettingsSection.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Pressable, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 
 import { RolesModalCreateRole } from "./RolesModalCreateRole";
 import trashSVG from "../../../../../assets/icons/trash.svg";
@@ -10,11 +10,16 @@ import { BrandText } from "@/components/BrandText";
 import { SVG } from "@/components/SVG";
 import { PrimaryButton } from "@/components/buttons/PrimaryButton";
 import { SecondaryButton } from "@/components/buttons/SecondaryButton";
-import { TextInputCustom } from "@/components/inputs/TextInputCustom";
-import { SpacerColumn, SpacerRow } from "@/components/spacer";
+import { SpacerColumn } from "@/components/spacer";
+import { TableCell } from "@/components/table/TableCell";
+import { TableHeader } from "@/components/table/TableHeader";
+import { TableRow } from "@/components/table/TableRow";
+import { TableTextCell } from "@/components/table/TableTextCell";
+import { TableWrapper } from "@/components/table/TableWrapper";
+import { TableColumns } from "@/components/table/utils";
 import { neutral33 } from "@/utils/style/colors";
 import { fontSemibold28 } from "@/utils/style/fonts";
-import { layout } from "@/utils/style/layout";
+import { layout, screenContentMaxWidthLarge } from "@/utils/style/layout";
 import {
   ROLES_BASED_ORGANIZATION_STEPS,
   RolesSettingFormType,
@@ -27,8 +32,15 @@ interface RolesSettingsSectionProps {
 export const RolesSettingsSection: React.FC<RolesSettingsSectionProps> = ({
   onSubmit,
 }) => {
-  const { handleSubmit, control, unregister, register, setValue, resetField } =
-    useForm<RolesSettingFormType>();
+  const {
+    handleSubmit,
+    control,
+    unregister,
+    register,
+    setValue,
+    resetField,
+    getValues,
+  } = useForm<RolesSettingFormType>();
   const [modalVisible, setModalVisible] = useState(false);
   const [rolesIndexes, setRolesIndexes] = useState<number[]>([]);
   const [resources, setResources] =
@@ -100,48 +112,45 @@ export const RolesSettingsSection: React.FC<RolesSettingsSectionProps> = ({
         <BrandText style={fontSemibold28}>Roles</BrandText>
         <SpacerColumn size={2.5} />
 
-        {rolesIndexes.map((id, index) => (
-          <View
-            style={{
-              flexDirection: "row",
-              marginBottom: layout.spacing_x2,
-            }}
-            key={id.toString()}
-          >
-            <View style={{ flex: 4 }}>
-              <TextInputCustom<RolesSettingFormType>
-                control={control}
-                noBrokenCorners
-                name={`roles.${index}.name`}
-                label="Role name"
-                placeholder="Role name"
-                rules={{ required: true }}
-                placeHolder="Role name"
-              />
-            </View>
-            <SpacerRow size={2.5} />
-            <View style={{ flex: 2 }}>
-              <TextInputCustom<RolesSettingFormType>
-                control={control}
-                noBrokenCorners
-                name={`roles.${index}.color`}
-                label="Role color"
-                placeholder="Role color"
-                placeHolder="Role color"
-              />
-            </View>
-            <SpacerRow size={2.5} />
-            <View style={{ flex: 1, alignSelf: "center" }}>
-              <Pressable
-                onPress={() => {
-                  removeRoleField(id, index);
-                }}
-              >
-                <SVG source={trashSVG} width={32} height={32} style={{}} />
-              </Pressable>
-            </View>
-          </View>
-        ))}
+        <View
+          style={{
+            width: "100%",
+            maxWidth: screenContentMaxWidthLarge,
+          }}
+        >
+          <BrandText>Roles table</BrandText>
+          <SpacerColumn size={2} />
+          <TableWrapper horizontalScrollBreakpoint={800}>
+            <TableHeader columns={columns} />
+            <FlatList
+              data={rolesIndexes}
+              renderItem={({ item, index }) => {
+                const role = getValues(`roles.${index}`);
+
+                if (!role) {
+                  return null;
+                }
+
+                return (
+                  <View key={item} style={{ flex: 1 }}>
+                    <RoleTableRow
+                      role={{
+                        name: role.name || "undefined",
+                        color: role.color || "#FFFFFF",
+                        resources: role.resources,
+                      }}
+                      removeRoleField={removeRoleField}
+                      id={item}
+                      index={index}
+                    />
+                    <SpacerColumn size={1} />
+                  </View>
+                );
+              }}
+              keyExtractor={(item) => item.toString()}
+            />
+          </TableWrapper>
+        </View>
         <SecondaryButton size="SM" text="Add New Role" onPress={onOpenModal} />
       </ScrollView>
       <View
@@ -163,6 +172,81 @@ export const RolesSettingsSection: React.FC<RolesSettingsSectionProps> = ({
       </View>
     </View>
   );
+};
+
+const RoleTableRow: React.FC<{
+  role: { name: string; color: string; resources: string[] | undefined };
+  removeRoleField: (id: number, index: number) => void;
+  id: number;
+  index: number;
+}> = ({ role, removeRoleField, id, index }) => {
+  return (
+    <TableRow>
+      <TableTextCell
+        style={{
+          minWidth: columns.name.minWidth,
+          flex: columns.name.flex,
+        }}
+      >
+        {role.name}
+      </TableTextCell>
+      <TableTextCell
+        style={{
+          minWidth: columns.color.minWidth,
+          flex: columns.color.flex,
+        }}
+      >
+        {role.color}
+      </TableTextCell>
+      <TableTextCell
+        style={{
+          minWidth: columns.resources.minWidth,
+          flex: columns.resources.flex,
+        }}
+      >
+        {role.resources?.join(", ") || "No resources defined"}
+      </TableTextCell>
+      <TableCell
+        style={{
+          minWidth: columns.delete.minWidth,
+          flex: columns.delete.flex,
+        }}
+      >
+        <View>
+          <Pressable
+            onPress={() => {
+              removeRoleField(id, index);
+            }}
+          >
+            <SVG source={trashSVG} width={32} height={32} style={{}} />
+          </Pressable>
+        </View>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+const columns: TableColumns = {
+  name: {
+    label: "Name",
+    flex: 1,
+    minWidth: 120,
+  },
+  color: {
+    label: "Color",
+    flex: 1,
+    minWidth: 60,
+  },
+  resources: {
+    label: "Resources",
+    flex: 1.5,
+    minWidth: 150,
+  },
+  delete: {
+    label: "Delete",
+    flex: 0.25,
+    minWidth: 30,
+  },
 };
 
 // TODO: Create a hook to get all the resources
