eb8745e3b31a2408e8611ac4104a87f8b235b2e8
clegirar <33428384+clegirar@users.noreply.github.com>
2025-01-13 18:24:38 +0100

    fix: fontWeight and fullWidth for Wallet manager screen (#1473)
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- packages/utils/style/fonts.ts
+++ packages/utils/style/fonts.ts
@@ -243,6 +243,13 @@ export const fontRegular20: TextStyle = {
   fontFamily: "Exo_400Regular",
   fontWeight: "400",
 };
+export const fontRegular18: TextStyle = {
+  fontSize: 18,
+  letterSpacing: -(18 * 0.02),
+  lineHeight: 20,
+  fontFamily: "Exo_400Regular",
+  fontWeight: "400",
+};
 export const fontRegular16: TextStyle = {
   fontSize: 16,
   letterSpacing: -(16 * 0.02),
