8ececeef0e1144197a973ae036c981bf55a9ab2f
clegirar <33428384+clegirar@users.noreply.github.com>
2025-01-15 12:05:30 +0100

    fix: font weight multisig screen (#1493)
    Signed-off-by: clegirar <clemntgirard@gmail.com>

--- packages/components/EmptyList.tsx
+++ packages/components/EmptyList.tsx
@@ -6,7 +6,7 @@ import { AnimationFadeIn } from "./animations/AnimationFadeIn";
 import { SpacerColumn } from "./spacer";
 import emptyListSVG from "../../assets/icons/empty-list.svg";
 import { neutral77 } from "../utils/style/colors";
-import { fontSemibold16 } from "../utils/style/fonts";
+import { fontRegular16 } from "../utils/style/fonts";
 
 interface EmptyListProps {
   text: string;
@@ -24,7 +24,7 @@ export const EmptyList: React.FC<EmptyListProps> = ({ text, size = 250 }) => {
     >
       <SVG source={emptyListSVG} width={size} height={size} />
       <SpacerColumn size={2} />
-      <BrandText style={[fontSemibold16, { color: neutral77 }]}>
+      <BrandText style={[fontRegular16, { color: neutral77 }]}>
         {text}
       </BrandText>
     </AnimationFadeIn>
