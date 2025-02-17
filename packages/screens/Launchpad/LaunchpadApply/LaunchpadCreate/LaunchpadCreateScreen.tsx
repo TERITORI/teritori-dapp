704fa0c0533130c6788d0956401d61234a80a092
WaDadidou <wadadidou@gmail.com>
2025-02-16 23:26:46 -0500

    fix(launchpad): Key in Next button

--- packages/screens/Launchpad/LaunchpadApply/LaunchpadCreate/LaunchpadCreateScreen.tsx
+++ packages/screens/Launchpad/LaunchpadApply/LaunchpadCreate/LaunchpadCreateScreen.tsx
@@ -161,7 +161,7 @@ export const LaunchpadCreateScreen: ScreenFC<"LaunchpadCreate"> = () => {
               />
             )}
 
-            {selectedStepKey === 6 ? (
+            {selectedStepKey === 5 ? (
               <PrimaryButton
                 width={160}
                 size="M"
